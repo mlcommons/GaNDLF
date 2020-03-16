d2e665f5a066924a3e6dd8d7ca0c32c1e57b1dc9
meghbhalerao <megh.bhalerao@gmail.com>
2020-03-16 10:35:12 -0400

    more changes to indexing

--- data.py
+++ data.py
@@ -46,15 +46,15 @@ class TumorSegmentationDataset(Dataset):
     def rcrop(self,image,gt,psize):
         imshape = image.shape
         if imshape[1]>psize[0]:
-            xshift = random.randint(0,imshape[0]-psize[0])
+            xshift = random.randint(0,imshape[1]-psize[0])
             image = image[:,xshift:xshift+psize[0],:,:]
             gt = gt[xshift:xshift+psize[0],:,:]
         if imshape[2]>psize[1]:
-            yshift = random.randint(0,imshape[1]-psize[1])
+            yshift = random.randint(0,imshape[2]-psize[1])
             image = image[:,:,yshift:yshift+psize[1],:]
             gt = gt[:,yshift:yshift+psize[1],:]
         if imshape[3]>psize[2]:
-            zshift = random.randint(0,imshape[2]-psize[2])
+            zshift = random.randint(0,imshape[3]-psize[2])
             image = image[:,:,:,zshift:zshift+psize[2]]
             gt = gt[:,:,zshift:zshift+psize[2]]
         return image,gt
