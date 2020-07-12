61012b1db8fdd90506c5242ab6cfbe470976b2cc
sarthakpati <sarthak.pati@hotmail.com>
2020-07-12 09:05:05 -0400

    IncDownsamplingModule separated

---
+++ models/seg_modules/IncDownsamplingModule.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class IncDownsamplingModule(nn.Module):
+    def __init__(self, input_channels, output_channels, leakiness=1e-2, kernel_size=1, conv_bias=True, 
+                 inst_norm_affine=True, lrelu_inplace=True):
+        nn.Module.__init__(self)
+        self.output_channels = output_channels 
+        self.input_channels = input_channels
+        self.leakiness = leakiness
+        self.conv_bias = conv_bias 
+        self.lrelu_inplace = lrelu_inplace
+        self.inst_norm_affine = inst_norm_affine
+        self.inst_norm = nn.InstanceNorm3d(output_channels,affine = self.inst_norm_affine, track_running_stats = True)
+        self.down = nn.Conv3d(input_channels,output_channels,kernel_size = 1, stride = 2, padding = 0, bias = self.conv_bias)
+    
+    def forward(self,x):
+        x = F.leaky_relu(self.inst_norm(self.down(x)),negative_slope = self.leakiness, inplace = self.lrelu_inplace)
+        return x
+  
