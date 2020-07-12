9c141354ba9f24463a623c43803508067bb87115
sarthakpati <sarthak.pati@hotmail.com>
2020-07-12 09:01:13 -0400

    Interpolate separated

--- models/seg_modules/seg_modules.py
+++ models/seg_modules/seg_modules.py
@@ -1,20 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-class Interpolate(nn.Module):
-    def __init__(self, size=None, scale_factor=None, mode='nearest', 
-                 align_corners=True):
-        super(Interpolate, self).__init__()
-        self.align_corners = align_corners
-        self.mode = mode
-        self.scale_factor = scale_factor
-        self.size = size
-        
-    def forward(self, x):
-        return nn.functional.interpolate(x, size=self.size, scale_factor=self.scale_factor, 
-                             mode=self.mode, align_corners=self.align_corners)
-    
+  
 class UpsamplingModule(nn.Module): 
     def __init__(self, input_channels, output_channels, leakiness=1e-2, 
         lrelu_inplace=True, kernel_size=3, scale_factor=2,
