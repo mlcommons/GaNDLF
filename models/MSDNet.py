25e36acff89c79175d4fc7efe925ed38e3b3f75e
sarthakpati <sarthak.pati@hotmail.com>
2020-07-12 10:52:34 -0400

    updated seg_module import

--- models/MSDNet.py
+++ models/MSDNet.py
@@ -1,9 +1,7 @@
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
-from seg_modules import in_conv, DownsamplingModule, EncodingModule, InceptionModule, ResNetModule
-from seg_modules import UpsamplingModule, DecodingModule,IncDownsamplingModule,IncConv
-from seg_modules import out_conv, FCNUpsamplingModule, IncDropout,IncUpsamplingModule,add_conv_block
+from seg_modules import *
 
 
 class MSDNet(nn.Module):
--- models/fcn.py
+++ models/fcn.py
@@ -1,9 +1,7 @@
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
-from seg_modules import in_conv, DownsamplingModule, EncodingModule, InceptionModule, ResNetModule
-from seg_modules import UpsamplingModule, DecodingModule,IncDownsamplingModule,IncConv
-from seg_modules import out_conv, FCNUpsamplingModule, IncDropout,IncUpsamplingModule
+from seg_modules import *
 
 class fcn(nn.Module):
     """
