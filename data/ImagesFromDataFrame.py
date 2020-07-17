dc81b2a07328f0ead3dcdbd2e986b4ccae6efe93
sarthakpati <sarthak.pati@hotmail.com>
2020-07-17 12:50:45 -0400

    added a default

--- data/ImagesFromDataFrame.py
+++ data/ImagesFromDataFrame.py
@@ -6,7 +6,7 @@ class ImagesFromDataFrame():
   """
   This class takes a pandas dataframe as input and structures it into a data structure that can be passed to Torch
   """
-  def __init__(self, dataframe, augmentations):
+  def __init__(self, dataframe, augmentations = None):
     self.input_df = dataframe
     self.augmentations = augmentations
 
