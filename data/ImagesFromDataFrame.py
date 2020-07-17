46676a8197af4fcad898582bc18c46a5b1c721e2
sarthakpati <sarthak.pati@hotmail.com>
2020-07-17 08:05:09 -0400

    added documentation

--- data/ImagesFromDataFrame.py
+++ data/ImagesFromDataFrame.py
@@ -4,7 +4,7 @@ from torchio.transforms import RandomAffine, RandomElasticDeformation, Compose
 
 class ImagesFromDataFrame():
   """
-  Documentation for the class goes here
+  This class takes a pandas dataframe as input and structures it into a data structure that can be passed to Torch
   """
   def __init__(self, dataFrame, augmentations):
     self.input_df = dataFrame
