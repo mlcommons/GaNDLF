e0ab82e4c408231902fe7bb71458f8e8ef07db1b
sarthakpati <sarthak.pati@hotmail.com>
2020-07-20 16:51:01 -0400

    we want the configuration file to present for every fold

--- trainer.py
+++ trainer.py
@@ -188,7 +188,7 @@ for train_index, test_index in kf.split(training_indeces_full):
     validationData = trainingData_full.iloc[test_index]
 
     # save the current model configuration as a sanity check
-    copyfile(model_parameters, os.path.join(model_path,'model.cfg'))
+    copyfile(model_parameters, os.path.join(currentOutputFolder,'model.cfg'))
 
     # pickle the data
     currentTrainingDataPickle = os.path.join(currentOutputFolder, 'train.pkl')
