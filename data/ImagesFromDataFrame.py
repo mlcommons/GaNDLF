46ed034cb0bf00a6d3eedc7b48ce431923a215a5
sarthakpati <sarthak.pati@hotmail.com>
2020-07-19 20:06:50 -0400

    the subject dict should be controlled by the csv header, not the complete path

--- data/ImagesFromDataFrame.py
+++ data/ImagesFromDataFrame.py
@@ -59,7 +59,7 @@ def ImagesFromDataFrame(dataframe, psize, augmentations = None):
         for channel in channelHeaderIndeces:
             channel_path = str(dataframe[channel][patient])
             # assigining the dict key to the channel
-            subject_dict[channel_path] = Image(channel_path,type = torchio.INTENSITY)
+            subject_dict[channel] = Image(channel_path,type = torchio.INTENSITY)
         subject_dict['mask'] = Image(str(dataframe[labelHeader][patient]),type = torchio.LABEL)
         # Initializing the subject object using the dict
         subject = Subject(subject_dict) 
