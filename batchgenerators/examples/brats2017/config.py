12e8310038a5a40068f0eff2db67396eec12f851
sarthakpati <sarthak.pati@hotmail.com>
2020-07-24 08:54:20 -0400

    removed all augmentation code (using torchio instead)

--- batchgenerators/examples/brats2017/config.py
+++
@@ -1,3 +0,0 @@
-brats_preprocessed_folder = "/media/fabian/DeepLearningData/BraTS2017_preprocessed"
-brats_folder_with_downloaded_train_data = "/media/fabian/DeepLearningData/Brats17TrainingData"
-num_threads_for_brats_example = 8
