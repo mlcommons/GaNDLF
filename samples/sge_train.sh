f65f099fdc4dedad2ab1b58130212c79395d89d3
sarthakpati <sarthak.pati@hotmail.com>
2020-07-27 14:29:07 -0400

    syntactic corrections

--- samples/sge_train.sh
+++ samples/sge_train.sh
@@ -9,9 +9,11 @@
 #$ -m s #### send mail when job is suspended
 #$ -l h_vmem=32G
 #$ -l gpu
+#$ -o ./output_dir/log_\$JOB_ID.stdout 
+#$ -e ./output_dir/log_\$JOB_ID.stderr
 ############################## END OF DEFAULT EMBEDDED SGE COMMANDS #######################
 # place this in the `experiment` folder, along with the model configuration and data list
 CUDA_VISIBLE_DEVICES=`get_CUDA_VISIBLE_DEVICES` || exit
 export CUDA_VISIBLE_DEVICES 
 source activate ../venv/
-python ../deep_sage -config ./model.cfg -data ./train.csv -output ./output_dir/ -train 1 -device ${CUDA_VISIBLE_DEVICES}
+python ../deep_sage -config ./model.cfg -data ./train.csv -output ./output_dir/ -train 1 -device \$CUDA_VISIBLE_DEVICES
