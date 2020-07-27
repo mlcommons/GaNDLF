7ac0766dffb5f13c2e3be586d5c92a1ac9455dee
sarthakpati <sarthak.pati@hotmail.com>
2020-07-27 13:37:05 -0400

    this should work for multi-class segmentation

--- DeepSAGE/utils.py
+++ DeepSAGE/utils.py
@@ -3,14 +3,13 @@ def one_hot(segmask_array):
     '''
     Encodes the mask voxels in a stacked numpy array for segmentation
     '''
-    # list = []
-    # tum_mask = np.array(list)
+    list = []
+    tum_mask = np.array(list)
     
-    # # iterate over unique values in segmentation and stack them
-    # for j in range(np.unique(segmask_array)):  # or range(len(theta))
-        # tum_mask = [tum_mask, (segmask_array == j).astype(np.uint8)]
-    tum_mask = (segmask_array>0.5).astype(np.uint8)
+    # iterate over unique values in segmentation and stack them
+    for j in range(np.unique(segmask_array)):
+        tum_mask = [tum_mask, (segmask_array == j).astype(np.uint8)]
+    
     bag_mask = (segmask_array == 0).astype(np.uint8)
     onehot_stack = [tum_mask, bag_mask]
     return np.array(onehot_stack)
-   
