6430e364b790e34d2c59a5fc94511fb5f4d44bf3
sarthakpati <sarthak.pati@hotmail.com>
2020-07-27 12:53:26 -0400

    comment added and commented code for multi-segmentation support (untested)

--- DeepSAGE/utils.py
+++ DeepSAGE/utils.py
@@ -1,5 +1,14 @@
 import numpy as np
 def one_hot(segmask_array):
+    '''
+    Encodes the mask voxels in a stacked numpy array for segmentation
+    '''
+    # list = []
+    # tum_mask = np.array(list)
+    
+    # # iterate over unique values in segmentation and stack them
+    # for j in range(np.unique(segmask_array)):  # or range(len(theta))
+        # tum_mask = [tum_mask, (segmask_array == j).astype(np.uint8)]
     tum_mask = (segmask_array>0.5).astype(np.uint8)
     bag_mask = (segmask_array == 0).astype(np.uint8)
     onehot_stack = [tum_mask, bag_mask]
