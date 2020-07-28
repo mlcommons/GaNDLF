364eebbf44fc6ee74627166585141b145d4e1253
sarthakpati <sarthak.pati@hotmail.com>
2020-07-28 11:13:12 -0400

    changing syntax

--- DeepSAGE/utils.py
+++ DeepSAGE/utils.py
@@ -13,5 +13,5 @@ def one_hot(segmask_array, largest_class):
             tum_mask = [tum_mask, (segmask_array == j).astype(np.uint8)]
     
     bag_mask = (segmask_array == 0).astype(np.uint8)
-    onehot_stack = [tum_mask, bag_mask]
-    return np.array(onehot_stack)
+    onehot_stack = np.array([tum_mask, bag_mask])
+    return onehot_stack
