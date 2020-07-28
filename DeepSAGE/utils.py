6b8f61ee9ed673d718365e1efdbe8ecdb33f699c
Megh <33202960+meghbhalerao@users.noreply.github.com>
2020-07-28 22:37:50 +0530

    updated - works but dim issue

--- DeepSAGE/utils.py
+++ DeepSAGE/utils.py
@@ -3,19 +3,15 @@ def one_hot(segmask_array, largest_class):
     '''
     Encodes the mask voxels in a stacked numpy array for segmentation
     '''
-    list = []
-    tum_mask = np.array(list)
+    tum_mask = []
     
     # create a range from 0 to largest class and find if it is present in current mask
     # do not consider background
     for idx, j in enumerate(np.arange(largest_class)):
         if j != 0:
-            mask_to_append = (segmask_array == j).astype(np.float32)
-            if tum_mask.size == 0:
-                tum_mask = mask_to_append
-            else:
-                tum_mask = np.concatenate((tum_mask, mask_to_append), axis=1)
-    
-    bag_mask = (segmask_array == 0).astype(np.float32)
-    onehot_stack = np.concatenate((tum_mask, bag_mask), axis=1)
+            tum_mask.append((segmask_array == j).astype(np.uint8))
+
+    bag_mask = (segmask_array == 0).astype(np.uint8)
+    tum_mask.append(bag_mask)
+    onehot_stack = np.asarray(tum_mask)
     return onehot_stack
