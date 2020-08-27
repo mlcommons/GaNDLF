8d51861f625d084bfe96a3bd72045436fcf7ec23
Megh Bhalerao Megh.Bhalerao@Pennmedicine.upenn.edu <megh.bhalerao@gmail.com>
2020-08-27 07:53:35 -0400

    more options for LR rate scheduling

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -149,6 +149,9 @@ def trainingLoop(trainingDataFromPickle, validataionDataFromPickle,
     scheduler_lr = torch.optim.lr_scheduler.StepLR(optimizer, step_size, gamma=0.1, last_epoch=-1, verbose=False)
   if scheduler == "reduce-on-plateau":
     scheduler_lr = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08, verbose=False)
+  if scheduler = "triangular":
+    torch.optim.lr_scheduler.CyclicLR(optimizer, learning_rate * 0.001, learning_rate, step_size_up=4*batch_size*len(train_loader.dataset), step_size_down=None, mode='triangular', gamma=1.0, scale_fn=None, scale_mode='cycle', cycle_momentum=True, base_momentum=0.8, max_momentum=0.9, last_epoch=-1, verbose=False)
+
 
 
   print(scheduler)
