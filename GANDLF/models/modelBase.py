263ffc24a1ec8646554754b624ef162fc78ba57d
sarthakpati <sarthak.pati@hotmail.com>
2020-09-09 12:51:24 -0400

    added initializer

--- GANDLF/models/modelBase.py
+++ GANDLF/models/modelBase.py
@@ -1,5 +1,14 @@
 
 
 class ModelBase:
-    def __init__(self, final_convolution_layer):
-        super().__init__()
+    '''
+    This is the base model class that all other architectures will need to derive from
+    '''
+    def __init__(self, n_channels, n_classes, base_filters, final_convolution_layer):
+        '''
+        This defines all defaults that the model base uses
+        '''
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.base_filters = base_filters
+        self.final_convolution_layer = final_convolution_layer
