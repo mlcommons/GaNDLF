1ad8e41b5010bef82ff0cdb0f1147268da8a4a2f
sarthakpati <sarthak.pati@hotmail.com>
2020-09-10 11:32:24 -0400

    typo fixed and extra scheduler code removed

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -151,7 +151,7 @@ def trainingLoop(trainingDataFromPickle, validataionDataFromPickle, channelHeade
         print("Current Device : ", torch.cuda.current_device())
         print("Device Count on Machine : ", torch.cuda.device_count())
         print("Device Name : ", torch.cuda.get_device_name(device))
-        print("Cuda Availibility : ", torch.cuda.is_available())
+        print("Cuda Availability : ", torch.cuda.is_available())
         print('Memory Usage : ')
         print('Allocated : ', round(torch.cuda.memory_allocated(0)/1024**3, 1),'GB')
         print('Cached: ', round(torch.cuda.memory_reserved(0)/1024**3, 1), 'GB')
@@ -190,25 +190,6 @@ def trainingLoop(trainingDataFromPickle, validataionDataFromPickle, channelHeade
     else:
         print('WARNING: Could not find the requested Learning Rate scheduler \'' + scheduler + '\' in the impementation, using exp, instead', file=sys.stderr)
         scheduler_lr = scheduler_lr = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.1, last_epoch=-1)
-
-    model = model.to(dev)
-    # Checking for the learning rate scheduler
-    if scheduler == "triangle":
-      step_size = 4*batch_size*len(train_loader.dataset)
-      clr = cyclical_lr(step_size, min_lr = 10**-3, max_lr=1)
-      scheduler_lr = torch.optim.lr_scheduler.LambdaLR(optimizer, [clr])
-      print("Starting Learning rate is:", learning_rate)
-    elif scheduler == "exp":
-      scheduler_lr = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.1, last_epoch=-1)
-    elif scheduler == "step":
-      scheduler_lr = torch.optim.lr_scheduler.StepLR(optimizer, step_size, gamma=0.1, last_epoch=-1)
-    elif scheduler == "reduce-on-plateau":
-      scheduler_lr = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08, verbose=False)
-    elif scheduler == "triangular":
-      scheduler_lr = torch.optim.lr_scheduler.CyclicLR(optimizer, learning_rate * 0.001, learning_rate, step_size_up=4*batch_size*len(train_loader.dataset), step_size_down=None, mode='triangular', gamma=1.0, scale_fn=None, scale_mode='cycle', cycle_momentum=True, base_momentum=0.8, max_momentum=0.9, last_epoch=-1)
-    else:
-      print('WARNING: Could not find the requested Learning Rate scheduler \'' + scheduler + '\' in the impementation, using exp, instead', file = sys.stderr)
-      scheduler_lr = scheduler_lr = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.1, last_epoch=-1)
 
     sys.stdout.flush()
     ############## STORING THE HISTORY OF THE LOSSES #################
