d4106ea841e444b93dc7f329b22382c0a924a250
SIddhesh Thakur <sid.cre8er@gmail.com>
2020-10-03 00:54:42 +0530

    none selection is now easier

--- GANDLF/models/modelBase.py
+++ GANDLF/models/modelBase.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+All Models in GANDLF are to be derived from this base class code
+"""
 
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -8,18 +11,21 @@ class ModelBase(nn.Module):
     This is the base model class that all other architectures will need to derive from
     '''
     def __init__(self, n_channels, n_classes, base_filters, final_convolution_layer):
-        super(ModelBase, self).__init__()
-        '''
+        """
         This defines all defaults that the model base uses
-        '''
+        """
+        super(ModelBase, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.base_filters = base_filters
+        none_list = ['none', None, 'None', 'regression']
 
         if final_convolution_layer == 'sigmoid':
             self.final_convolution_layer = F.sigmoid
+
         elif final_convolution_layer == 'softmax':
             self.final_convolution_layer = F.softmax
-        elif (final_convolution_layer == 'none') or (final_convolution_layer == None) or (final_convolution_layer == 'None') or (final_convolution_layer == 'regression'):
+
+        elif final_convolution_layer in none_list:
             self.final_convolution_layer = None
         
