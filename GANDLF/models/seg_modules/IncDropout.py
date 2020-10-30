1e057f1afa506751afd64b75259b335400b6902e
sarthakpati <sarthak.pati@hotmail.com>
2020-10-30 13:09:05 -0400

    updated for n-d case

--- GANDLF/models/seg_modules/IncDropout.py
+++ GANDLF/models/seg_modules/IncDropout.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class IncDropout(nn.Module):
-    def __init__(self,input_channels,output_channels,dropout_p=0.3,leakiness=1e-2,conv_bias=True,inst_norm_affine=True, res = False, lrelu_inplace = True):
+    def __init__(self,input_channels, output_channels, Conv, Dropout, InstanceNorm, 
+    dropout_p=0.3, leakiness=1e-2, conv_bias=True, inst_norm_affine=True, res = False, lrelu_inplace = True):
         nn.Module.__init__(self)
         self.input_channels = input_channels
         self.output_channels = output_channels
@@ -13,8 +14,8 @@ class IncDropout(nn.Module):
         self.inst_norm_affine = inst_norm_affine
         self.res = res
         self.lrelu_inplace = lrelu_inplace 
-        self.dropout = nn.Dropout3d(dropout_p)
-        self.conv = nn.Conv3d(input_channels,output_channels,kernel_size=1,stride=1,padding=0,bias=self.conv_bias)
+        self.dropout = Dropout(dropout_p)
+        self.conv = Conv(input_channels,output_channels,kernel_size=1,stride=1,padding=0,bias=self.conv_bias)
     def forward(self,x):
         x = self.dropout(x)
         x = self.conv(x)
