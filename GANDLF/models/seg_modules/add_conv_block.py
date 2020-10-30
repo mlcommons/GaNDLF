e987ea999accd8c8a7bb7931a895a84ef6686542
sarthakpati <sarthak.pati@hotmail.com>
2020-10-30 12:48:35 -0400

    updated to take new apis

--- GANDLF/models/seg_modules/add_conv_block.py
+++ GANDLF/models/seg_modules/add_conv_block.py
@@ -4,16 +4,10 @@ Add Conv Block for MSDNet
 """
 import torch.nn as nn
 
-def add_conv_block(in_ch=1, out_ch=1, kernel_size=3, dilate=1, last=False, volumetric=True):
+def add_conv_block(Conv, BatchNorm, in_ch=1, out_ch=1, kernel_size=3, dilate=1, last=False):
     """
     Helper function
     """
-    if volumetric:
-        Conv = nn.Conv3d
-        BatchNorm = nn.BatchNorm3d
-    else:
-        Conv = nn.Conv2d
-        BatchNorm = nn.BatchNorm2d
     pad = dilate if not last else 0
     conv_1 = Conv(in_ch, out_ch, kernel_size, padding=pad, dilation=dilate)
     bn_1 = BatchNorm(out_ch)
