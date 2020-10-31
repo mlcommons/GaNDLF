a0c704a6fef007c98a6a507f6b67c62dabb69d70
sarthakpati <sarthak.pati@hotmail.com>
2020-10-31 08:48:39 -0400

    reverting

--- GANDLF/models/modelBase.py
+++ GANDLF/models/modelBase.py
@@ -25,15 +25,15 @@ class ModelBase(nn.Module):
         # convolution, batch_norm, instancenorm, dropout
 
         if self.n_dimensions == 2:
-            self.Conv = nn.Conv3d
-            self.ConvTranspose = nn.ConvTranspose3d
-            self.InstanceNorm = nn.InstanceNorm3d
-            self.Dropout = nn.Dropout3d
-            self.BatchNorm = nn.BatchNorm3d
-            self.MaxPool = nn.MaxPool3d
-            self.AvgPool = nn.AvgPool3d
-            self.AdaptiveAvgPool = nn.AdaptiveAvgPool3d
-            self.AdaptiveMaxPool = nn.AdaptiveMaxPool3d
+            self.Conv = nn.Conv2d
+            self.ConvTranspose = nn.ConvTranspose2d
+            self.InstanceNorm = nn.InstanceNorm2d
+            self.Dropout = nn.Dropout2d
+            self.BatchNorm = nn.BatchNorm2d
+            self.MaxPool = nn.MaxPool2d
+            self.AvgPool = nn.AvgPool2d
+            self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d
+            self.AdaptiveMaxPool = nn.AdaptiveMaxPool2d
         elif self.n_dimensions == 3:
             self.Conv = nn.Conv3d
             self.ConvTranspose = nn.ConvTranspose3d
