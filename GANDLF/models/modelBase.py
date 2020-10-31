ad2262c6bbfa3b692847e6f974af50da782388f3
sarthakpati <sarthak.pati@hotmail.com>
2020-10-31 08:44:42 -0400

    trying another thing

--- GANDLF/models/modelBase.py
+++ GANDLF/models/modelBase.py
@@ -26,14 +26,14 @@ class ModelBase(nn.Module):
 
         if self.n_dimensions == 2:
             self.Conv = nn.Conv3d
-            self.ConvTranspose = nn.ConvTranspose2d
-            self.InstanceNorm = nn.InstanceNorm2d
-            self.Dropout = nn.Dropout2d
-            self.BatchNorm = nn.BatchNorm2d
-            self.MaxPool = nn.MaxPool2d
-            self.AvgPool = nn.AvgPool2d
-            self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d
-            self.AdaptiveMaxPool = nn.AdaptiveMaxPool2d
+            self.ConvTranspose = nn.ConvTranspose3d
+            self.InstanceNorm = nn.InstanceNorm3d
+            self.Dropout = nn.Dropout3d
+            self.BatchNorm = nn.BatchNorm3d
+            self.MaxPool = nn.MaxPool3d
+            self.AvgPool = nn.AvgPool3d
+            self.AdaptiveAvgPool = nn.AdaptiveAvgPool3d
+            self.AdaptiveMaxPool = nn.AdaptiveMaxPool3d
         elif self.n_dimensions == 3:
             self.Conv = nn.Conv3d
             self.ConvTranspose = nn.ConvTranspose3d
