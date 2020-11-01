f217f7422a06a9049fd2bbdea604b39dd2939452
sarthakpati <sarthak.pati@hotmail.com>
2020-11-01 10:51:37 -0500

    correct definition

--- GANDLF/utils.py
+++ GANDLF/utils.py
@@ -51,9 +51,8 @@ def get_stats(model, loader, psize, channel_keys, class_list, loss_fn, weights =
     # if no weights are specified, use 1
     if weights is None:
         weights = [1]
-        if class_list > 1:
-            for i in range(class_list - 1):
-                weights.append(1)
+        for i in range(len(class_list) - 1):
+            weights.append(1)
 
     model.eval()
     with torch.no_grad():
