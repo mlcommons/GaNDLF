0100b0ba0c8b29ed882415a99f442811c86f27c2
Megh <33202960+meghbhalerao@users.noreply.github.com>
2020-11-06 20:56:47 +0530

    more code cleanup

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -178,10 +178,7 @@ def trainingLoop(trainingDataFromPickle, validationDataFromPickle, headers, devi
             print('Device finally used: ', dev)
             device = torch.device('cuda:' + dev)
             model = model.to(int(dev))
-            print('Memory Total : ', round(torch.cuda.get_device_properties(int(dev)).total_memory/1024**3, 1), 'GB')
-            print('Memory Usage : ')
-            print('Allocated : ', round(torch.cuda.memory_allocated(int(dev))/1024**3, 1),'GB')
-            print('Cached: ', round(torch.cuda.memory_reserved(int(dev))/1024**3, 1), 'GB')
+            print('Memory Total : ', round(torch.cuda.get_device_properties(int(dev)).total_memory/1024**3, 1), 'GB, Allocated: ', round(torch.cuda.memory_allocated(int(dev))/1024**3, 1),'GB, Cached: ',round(torch.cuda.memory_reserved(int(dev))/1024**3, 1), 'GB' )
         
         print("Device - Current: %s Count: %d Name: %s Availability: %s"%(torch.cuda.current_device(), torch.cuda.device_count(), torch.cuda.get_device_name(device), torch.cuda.is_available()))
      
