017379c8b3a1dfd21d0154500eec26b2a95ab0a4
sarthakpati <sarthak.pati@hotmail.com>
2020-11-07 07:58:35 -0500

    pre-processing works

--- GANDLF/data/ImagesFromDataFrame.py
+++ GANDLF/data/ImagesFromDataFrame.py
@@ -46,12 +46,6 @@ def spatial_transform(patch_size = None, p=1):
         max_displacement = 7.5
     return OneOf({RandomAffine(): 0.8, RandomElasticDeformation(max_displacement = max_displacement): 0.2}, p=p)
 
-def threshold_transform(min, max, p=1):
-    return Lambda(lambda x: threshold_intensities(x, min, max))
-
-def clip_transform(min, max, p=1):
-    return Lambda(lambda x: clip_intensities(x, min, max))
-
 def bias(patch_size = None, p=1):
     return RandomBiasField(coefficients=0.5, order=3, p=p, seed=None)
 
@@ -64,11 +58,22 @@ def noise(patch_size = None, p=1):
 def swap(patch_size = 15, p=1):
     return RandomSwap(patch_size=patch_size, num_iterations=100, p=p, seed=None)
 
-# Defining a dictionary - key is the string and the value is the augmentation object
-global_augs_dict = {
+## lambdas for pre-processing
+def threshold_transform(min, max, p=1):
+    return Lambda(lambda x: threshold_intensities(x, min, max))
+
+def clip_transform(min, max, p=1):
+    return Lambda(lambda x: clip_intensities(x, min, max))
+
+# defining dict for pre-processing - key is the string and the value is the transform object
+global_preprocessing_dict = {
     'threshold' : threshold_transform,
     'clip' : clip_transform,
-    'normalize' : ZNormalization(),
+    'normalize' : ZNormalization()
+}
+
+# Defining a dictionary for augmentations - key is the string and the value is the augmentation object
+global_augs_dict = {
     'spatial' : spatial_transform,
     'kspace' : mri_artifact,
     'bias' : bias,
@@ -79,7 +84,7 @@ global_augs_dict = {
 
 # This function takes in a dataframe, with some other parameters and returns the dataloader
 def ImagesFromDataFrame(dataframe, psize, headers, q_max_length, q_samples_per_volume,
-                        q_num_workers, q_verbose, train=True, augmentations=None, resize=None):
+                        q_num_workers, q_verbose, train=True, augmentations=None, preprocessing=None, resize=None):
     # Finding the dimension of the dataframe for computational purposes later
     num_row, num_col = dataframe.shape
     # num_channels = num_col - 1 # for non-segmentation tasks, this might be different
@@ -142,27 +147,25 @@ def ImagesFromDataFrame(dataframe, psize, headers, q_max_length, q_samples_per_v
 
     # first, we want to do thresholding, followed by clipping, if it is present - required for inference as well
     for key in ['threshold','clip']:
-        augmentation_list.append(global_augs_dict[key](min=augmentations[key]['min'], max=augmentations[key]['max']))
+        augmentation_list.append(global_preprocessing_dict[key](min=preprocessing[key]['min'], max=preprocessing[key]['max']))
         
     # first, we want to do the resampling, if it is present - required for inference as well
-    if 'resample' in augmentations:
-        if 'resolution' in augmentations['resample']:
+    if 'resample' in preprocessing:
+        if 'resolution' in preprocessing['resample']:
             # resample_split = str(aug).split(':')
-            resample_values = tuple(np.array(augmentations['resample']['resolution']).astype(np.float))
+            resample_values = tuple(np.array(preprocessing['resample']['resolution']).astype(np.float))
             augmentation_list.append(Resample(resample_values))
 
     # next, we want to do the intensity normalize - required for inference as well
-    if 'normalize' in augmentations:
-        augmentation_list.append(global_augs_dict['normalize'])
+    if 'normalize' in preprocessing:
+        augmentation_list.append(global_preprocessing_dict['normalize'])
 
     # other augmentations should only happen for training - and also setting the probabilities
     # for the augmentations
-    if train:
+    if train and not(augmentations == None):
         for aug in augmentations:
-            # resample and normalize should always have probability=1
-            if (aug != 'normalize') and (aug != 'resample'):
-                actual_function = global_augs_dict[aug](patch_size=augmentation_patchAxesPoints, p=augmentations[aug]['probability'])
-                augmentation_list.append(actual_function)
+            actual_function = global_augs_dict[aug](patch_size=augmentation_patchAxesPoints, p=augmentations[aug]['probability'])
+            augmentation_list.append(actual_function)
 
     transform = Compose(augmentation_list)
 
