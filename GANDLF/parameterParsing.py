9da808120f3947b12a5da1aad7496813619dfe2f
sarthakpati <sarthak.pati@hotmail.com>
2020-11-09 07:03:48 -0500

    added file for common parsing

---
+++ GANDLF/parameterParsing.py
@@ -0,0 +1,6 @@
+from GANDLF.schd import *
+from GANDLF.models.fcn import fcn
+from GANDLF.models.unet import unet
+from GANDLF.models.uinc import uinc
+from GANDLF.losses import *
+from GANDLF.utils import *
