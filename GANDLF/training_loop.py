3ae661283d18f0fa57f4ae4fe23ebd386ff3e563
sarthakpati <sarthak.pati@hotmail.com>
2020-11-09 07:20:44 -0500

    moving all model-related check to single location

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -30,6 +30,7 @@ from GANDLF.models.unet import unet
 from GANDLF.models.uinc import uinc
 from GANDLF.losses import *
 from GANDLF.utils import *
+from .parameterParsing import *
 
 def trainingLoop(trainingDataFromPickle, validationDataFromPickle, headers, device, parameters, outputDir, holdoutDataFromPickle = None):
     '''
@@ -76,39 +77,8 @@ def trainingLoop(trainingDataFromPickle, validationDataFromPickle, headers, devi
     if n_channels == 0:
         sys.exit('The number of input channels cannot be zero, please check training CSV')
 
-    divisibilityCheck_patch = True
-    divisibilityCheck_baseFilter = True
-
-    divisibilityCheck_denom_patch = 16 # for unet/resunet/uinc
-    divisibilityCheck_denom_baseFilter = 4 # for uinc
-    
-    # Defining our model here according to parameters mentioned in the configuration file : 
-    if which_model == 'resunet':
-        model = unet(parameters['dimension'], n_channels, n_classList, base_filters, final_convolution_layer = parameters['model']['final_layer'], residualConnections=True)
-        divisibilityCheck_baseFilter = False
-    elif which_model == 'unet':
-        model = unet(parameters['dimension'], n_channels, n_classList, base_filters, final_convolution_layer = parameters['model']['final_layer'])
-        divisibilityCheck_baseFilter = False
-    elif which_model == 'fcn':
-        model = fcn(parameters['dimension'], n_channels, n_classList, base_filters, final_convolution_layer = parameters['model']['final_layer'])
-        # not enough information to perform checking for this, yet
-        divisibilityCheck_patch = False 
-        divisibilityCheck_baseFilter = False
-    elif which_model == 'uinc':
-        model = uinc(parameters['dimension'], n_channels, n_classList, base_filters, final_convolution_layer = parameters['model']['final_layer'])
-    else:
-        print('WARNING: Could not find the requested model \'' + which_model + '\' in the implementation, using ResUNet, instead', file = sys.stderr)
-        which_model = 'resunet'
-        model = unet(parameters['dimension'], n_channels, n_classList, base_filters, final_convolution_layer = parameters['model']['final_layer'], residualConnections=True)
-
-    # check divisibility
-    if divisibilityCheck_patch:
-        if not checkPatchDivisibility(psize, divisibilityCheck_denom_patch):
-        sys.exit('The \'patch_size\' should be divisible by \'' + str(divisibilityCheck_denom_patch) + '\' for the \'' + which_model + '\' architecture')
-    if divisibilityCheck_baseFilter:
-        if not checkPatchDivisibility(base_filters, divisibilityCheck_denom_baseFilter):
-        sys.exit('The \'base_filters\' should be divisible by \'' + str(divisibilityCheck_denom_baseFilter) + '\' for the \'' + which_model + '\' architecture')
-    
+    # Defining our model here according to parameters mentioned in the configuration file
+    model = get_model(which_model, parameters['dimension'], n_channels, n_classList, base_filters, final_convolution_layer = parameters['model']['final_layer'], residualConnections=True)
     # setting optimizer
     if opt == 'sgd':
         optimizer = optim.SGD(model.parameters(),
