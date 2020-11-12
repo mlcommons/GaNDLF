43010b845358e41e5c5337af22993538f72d3f70
sarthakpati <sarthak.pati@hotmail.com>
2020-11-12 17:55:29 -0500

    minor syntax fixes

--- GANDLF/preprocessing.py
+++ GANDLF/preprocessing.py
@@ -6,17 +6,16 @@ def threshold_intensities(input_tensor, min_val, max_val):
     '''
     This function takes an input tensor and 2 thresholds, lower & upper and thresholds between them, basically making intensity values outside this range '0'
     '''
-    l1_tensor = torch.where(input_tensor < float(min_val), input_tensor, 0)
-    l2_tensor = torch.where(l1_tensor > float(max_val), l1_tensor, 0)
+    C = torch.zeros(input_tensor.size())
+    l1_tensor = torch.where(input_tensor < max_val, input_tensor, C)
+    l2_tensor = torch.where(l1_tensor > min_val, l1_tensor, C)
     return l2_tensor
 
 def clip_intensities(input_tensor, min_val, max_val):
     '''
     This function takes an input tensor and 2 thresholds, lower and upper and clips between them, basically making the lowest value as 'min_val' and largest values as 'max_val'
     '''
-    l1_tensor = torch.where(input_tensor < float(min_val), input_tensor, float(min_val))
-    l2_tensor = torch.where(l1_tensor > float(max_val), l1_tensor, float(max_val))
-    return l2_tensor
+    return torch.clamp(input_tensor, min_val, max_val)
 
 def resize_image_resolution(input_image, output_size):
     '''
