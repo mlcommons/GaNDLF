54cf6cb17cc1c6678331e8f27a75c565a4f0e58a
sarthakpati <sarthak.pati@hotmail.com>
2020-11-12 06:35:14 -0500

    using new api

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -59,11 +59,11 @@ def trainingLoop(trainingDataFromPickle, validationDataFromPickle, headers, devi
         psize.append(1) # ensuring same size during torchio processing
 
     trainingDataForTorch = ImagesFromDataFrame(trainingDataFromPickle, psize, headers, q_max_length, q_samples_per_volume,
-                                               q_num_workers, q_verbose, train=True, augmentations=augmentations, preprocessing = preprocessing, resize = parameters['data_preprocessing']['resize'])
+                                               q_num_workers, q_verbose, train=True, augmentations=augmentations, preprocessing = preprocessing)
     validationDataForTorch = ImagesFromDataFrame(validationDataFromPickle, psize, headers, q_max_length, q_samples_per_volume,
-                                               q_num_workers, q_verbose, train=False, augmentations=augmentations, preprocessing = preprocessing, resize = parameters['data_preprocessing']['resize']) # may or may not need to add augmentations here
+                                               q_num_workers, q_verbose, train=False, augmentations=augmentations, preprocessing = preprocessing) # may or may not need to add augmentations here
     inferenceDataForTorch = ImagesFromDataFrame(holdoutDataFromPickle, psize, headers, q_max_length, q_samples_per_volume,
-                                            q_num_workers, q_verbose, train=False, augmentations=augmentations, preprocessing = preprocessing, resize = parameters['data_preprocessing']['resize'])
+                                            q_num_workers, q_verbose, train=False, augmentations=augmentations, preprocessing = preprocessing)
     
     
     train_loader = DataLoader(trainingDataForTorch, batch_size=batch_size, shuffle=True)
