76f616023da2805898c7f605b81eb4864291892f
Siddhesh Thakur <sid.cre8er@gmail.com>
2020-11-20 19:25:52 +0530

    Apply suggestions from code review
    Co-authored-by: Sarthak Pati <sarthak.pati@uphs.upenn.edu>

--- GANDLF/inference_manager.py
+++ GANDLF/inference_manager.py
@@ -35,8 +35,8 @@ def InferenceManager(dataframe, headers, outputDir, parameters, device):
     if parameters['modality'] == 'rad':
 	    inferenceLoopRad(inferenceDataFromPickle=inferenceData_full, headers=headers, outputDir=outputDir,
 	                     device=device, parameters=parameters)
-	elif parameters['modality'] == 'path':
-		inferenceLoopPath(inferenceDataFromPickle=inferenceData_full, headers=headers, outputDir=outputDir,
+    elif parameters['modality'] == 'path':
+	    inferenceLoopPath(inferenceDataFromPickle=inferenceData_full, headers=headers, outputDir=outputDir,
 	                      device=device, parameters=parameters)
     else:
     	print('Modality should be on of rad or path. Please set the correct on in the config file.')
