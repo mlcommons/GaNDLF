6c3ba365ea365547e3ed06ed71a85d6f5ae997ef
sarthakpati <sarthak.pati@hotmail.com>
2020-12-03 16:09:22 -0500

    added comments and example to run multiple types of tests

--- testing/test_full.py
+++ testing/test_full.py
@@ -1,6 +1,17 @@
 import math
 import sys
 
+'''
+steps to follow to write tests:
+1. download sample data
+2. for each dir (application type) and sub-dir (image dimension), run training for a single epoch on cpu
+3. for each dir (application type) and sub-dir (image dimension), run inference for a single trained model per testing/validation split for a single subject on cpu
+4. hopefully the various sys.exit messages throughout the code will catch issues
+'''
+
 def test_first():
   test = 1
-  sys.exit('test')
+  print('started 2d segmentation')
+  print('passed')
+  print('started 3d segmentation')
+  sys.exit('boo!')
