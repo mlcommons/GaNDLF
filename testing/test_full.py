19849aa691c4669af2db60f7231eb46f45908933
sarthakpati <sarthak.pati@hotmail.com>
2020-12-03 17:40:26 -0500

    updated comments

--- testing/test_full.py
+++ testing/test_full.py
@@ -5,22 +5,23 @@ import requests, zipfile, io, os
 
 '''
 steps to follow to write tests:
-1. download sample data
-2. construct the training csv
-2. for each dir (application type) and sub-dir (image dimension), run training for a single epoch on cpu
-3. for each dir (application type) and sub-dir (image dimension), run inference for a single trained model per testing/validation split for a single subject on cpu
+[x] download sample data
+[ ] construct the training csv
+[ ] for each dir (application type) and sub-dir (image dimension), run training for a single epoch on cpu
+[ ] for each dir (application type) and sub-dir (image dimension), run inference for a single trained model per testing/validation split for a single subject on cpu
 4. hopefully the various sys.exit messages throughout the code will catch issues
 '''
 
 def test_download_data():
+  '''
+  This function downloads the sample data, which is the first step towards getting everything ready
+  '''
   urlToDownload = 'https://github.com/sarthakpati/tempDownloads/raw/main/data.zip'
   if not Path(os.getcwd() + '/testing/data/test/3d_rad_segmentation/001/image.nii.gz').exists():
       print('Downloading and extracting sample data')
       r = requests.get(urlToDownload)
       z = zipfile.ZipFile(io.BytesIO(r.content))
       z.extractall('./testing')
-
-  test = 1
 
 def test_full():
   test = 1
