f820155ba69213c79c4916be54162ec5eb279722
sarthakpati <sarthak.pati@hotmail.com>
2020-12-03 16:26:41 -0500

    added a note

--- testing/test_full.py
+++ testing/test_full.py
@@ -4,6 +4,7 @@ import sys
 '''
 steps to follow to write tests:
 1. download sample data
+2. construct the training csv
 2. for each dir (application type) and sub-dir (image dimension), run training for a single epoch on cpu
 3. for each dir (application type) and sub-dir (image dimension), run inference for a single trained model per testing/validation split for a single subject on cpu
 4. hopefully the various sys.exit messages throughout the code will catch issues
