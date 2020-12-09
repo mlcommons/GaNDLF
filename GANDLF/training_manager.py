74c675d7b625cfad5fa6607bccaef033a54e87f6
sarthakpati <sarthak.pati@hotmail.com>
2020-12-09 17:12:21 -0500

    fix testin dir

--- GANDLF/training_manager.py
+++ GANDLF/training_manager.py
@@ -161,7 +161,7 @@ def TrainingManager(dataframe, headers, outputDir, parameters, device):
                     ' -val_loader_pickle ' + currentValidationDataPickle + \
                     ' -parameter_pickle ' + currentModelConfigPickle + \
                     ' -headers_pickle ' + headersPickle + \
-                    ' -device ' + str(device) + ' -outputDir ' + currentValOutputFolder + ' -testing_loader_pickle ' + currentTestingDataPickle
+                    ' -device ' + str(device) + ' -outputDir ' + currentValOutputFolder + ' -testing_loader_pickle '
                 
                 if noTestingData:
                     command = command + 'None'
