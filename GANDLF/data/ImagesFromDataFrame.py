de12dacdd5baaec39da7f4c5f23c51b79a92d276
sarthakpati <sarthak.pati@hotmail.com>
2020-12-22 08:14:17 -0500

    comment added

--- GANDLF/data/ImagesFromDataFrame.py
+++ GANDLF/data/ImagesFromDataFrame.py
@@ -156,7 +156,7 @@ def ImagesFromDataFrame(dataframe, psize, headers, q_max_length, q_samples_per_v
         # padding image, but only for label sampler, because we don't want to pad for uniform
         if 'label' in sampler:
             psize_pad = list(np.round(np.divide(psize,2)))
-            padder = Pad(psize_pad, padding_mode = 'symmetric')
+            padder = Pad(psize_pad, padding_mode = 'symmetric') # for modes: https://numpy.org/doc/stable/reference/generated/numpy.pad.html
             subject = padder(subject)
 
         # Appending this subject to the list of subjects
