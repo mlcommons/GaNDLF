d51a82156fd725a329da80c361fcb3270ddb7a7d
sarthakpati <sarthak.pati@hotmail.com>
2021-01-20 15:35:00 -0500

    trying something else

--- GANDLF/preprocessing.py
+++ GANDLF/preprocessing.py
@@ -2,6 +2,9 @@ import torch
 import numpy as np
 import SimpleITK as sitk
 
+from torchio.data.subject import Subject
+from torchio.transforms.preprocessing.intensity.normalization_transform import NormalizationTransform, TypeMaskingMethod
+
 def threshold_intensities(input_tensor, min_val, max_val):
     '''
     This function takes an input tensor and 2 thresholds, lower & upper and thresholds between them, basically making intensity values outside this range '0'
@@ -27,14 +30,80 @@ def resize_image_resolution(input_image, output_size):
         outputSpacing[i] = outputSpacing[i] * (inputSize[i] / output_size[i])
     return outputSpacing
 
-class NonZeroNormalize:
-    def __call__(self, x):
-        tensor = x.clone().float()
-        mask = tensor > 0
-        values = x > tensor.masked_select(mask)
+class NonZeroNormalize(NormalizationTransform):
+    """Subtract mean and divide by standard deviation.
+
+    Args:
+        masking_method: See
+            :class:`~torchio.transforms.preprocessing.intensity.NormalizationTransform`.
+        **kwargs: See :class:`~torchio.transforms.Transform` for additional keyword arguments.
+    """
+    def __init__(
+            self,
+            **kwargs
+            ):
+        super().__init__(masking_method=lambda x: x > 0, **kwargs)
+        self.args_names = ('masking_method',)
+
+    def apply_normalization(
+            self,
+            subject: Subject,
+            image_name: str,
+            mask: torch.Tensor,
+            ) -> None:
+        image = subject[image_name]
+        standardized = self.znorm(
+            image.data,
+            mask,
+        )
+        if standardized is None:
+            message = (
+                'Standard deviation is 0 for masked values'
+                f' in image "{image_name}" ({image.path})'
+            )
+            raise RuntimeError(message)
+        image.data = standardized
+
+    @staticmethod
+    def znorm(tensor: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        tensor = tensor.clone().float()
+        values = tensor.masked_select(mask)
         mean, std = values.mean(), values.std()
         if std == 0:
             return None
         tensor -= mean
         tensor /= std
         return tensor
+
+# class NonZeroNormalize:
+#     '''
+#     This class performs non-zero z-score normalize
+#     '''
+#     def __call__(self, x):
+#         tensor = x.clone().float()
+#         mask = tensor > 0
+#         values = x > tensor.masked_select(mask)
+#         mean, std = values.mean(), values.std()
+#         if std == 0:
+#             return None
+#         tensor -= mean
+#         tensor /= std
+#         return tensor
+
+class ThresholdIntensities:
+    def __call__(self, x, min_val, max_val):
+        input_tensor = x.clone().float()
+        C = torch.zeros(input_tensor.size())
+        l1_tensor = torch.where(input_tensor < max_val, input_tensor, C)
+        l2_tensor = torch.where(l1_tensor > min_val, l1_tensor, C)
+        return l2_tensor
+
+class ClipIntensities:
+    def __call__(self, x, min_val, max_val):
+        input_tensor = x.clone().float()
+        return torch.clamp(input_tensor, min_val, max_val)
+
+class ResizeImageResolution:
+    def __call__(self, x, min_val, max_val):
+        input_tensor = x.clone().float()
+        return torch.clamp(input_tensor, min_val, max_val)
