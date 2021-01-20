9a23e4ded26c2ca5bc632ccffd9c6b29e020a32f
sarthakpati <sarthak.pati@hotmail.com>
2021-01-20 15:19:29 -0500

    trying out a new solution for nonzero norm

--- GANDLF/data/ImagesFromDataFrame.py
+++ GANDLF/data/ImagesFromDataFrame.py
@@ -198,7 +198,7 @@ def ImagesFromDataFrame(dataframe, psize, headers, q_max_length = 10, q_samples_
         if 'normalize' in preprocessing:
             augmentation_list.append(global_preprocessing_dict['normalize'])
         elif 'normalize_nonZero' in preprocessing:
-            augmentation_list.append(global_preprocessing_dict['normalize_nonZero'])
+            augmentation_list.append(NonZeroNormalize())
 
     # other augmentations should only happen for training - and also setting the probabilities
     # for the augmentations
