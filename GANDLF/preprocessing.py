95d70fe7659444cc222e333f0a0e4f18fe56e1d3
Edwards <edwardsb@spr-gpu01.jf.intel.com>
2021-01-22 15:42:15 -0800

    cleaning up

--- GANDLF/preprocessing.py
+++ GANDLF/preprocessing.py
@@ -38,13 +38,10 @@ def resize_image_resolution(input_image, output_size):
 def crop_image_outside_zeros(array, patch_size):
     dimensions = len(array.shape)
     if dimensions != 4:
-        raise ValueError("Array expected to be 4D but got {} dimensions.".format(dimensions))
-    if (len(patch_size) != 4) or (patch_size[0] != 1):
-        raise ValueError("The length of patch_size is expected as 4 with a 1 at index 0 as we do not crop the channels.") 
+        raise ValueError("Array expected to be 4D but got {} dimensions.".format(dimensions)) 
     
     # collapse to single channel and get the mask of non-zero voxels
     mask = array.sum(axis=0) > 0
-    patch_size = patch_size[1:]
 
     # get the small and large corners
   
@@ -82,9 +79,8 @@ def crop_image_outside_zeros(array, patch_size):
 # adapted from https://github.com/fepegar/torchio/blob/master/torchio/transforms/preprocessing/spatial/crop.py
 class  CropExternalZeroplanes(SpatialTransform):
     """
-    Transformation class to enable taking the whole stack (inluding all scan
-    modalities as well as a channel for the segmentation label) and removing all 
-    external zero planes (zero across all channels).
+    Transformation class to enable taking the whole image stack (including segmentation) and removing 
+    (starting from edges) physical-coordinate planes with all zero voxels until you reach a non-zero voxel.
 
     Args:
         psize: patch size (has a channel axis dimension which should be made 1) used to ensure we do not crop to smaller size than this)
@@ -97,30 +93,30 @@ class  CropExternalZeroplanes(SpatialTransform):
         self.patch_size = patch_size
     
     def apply_transform(self, subject):
+        
+        # make sure shapes are consistent across images, and get this shape
         subject.check_consistent_spatial_shape()
-        # create stack of all images
+        example_image_affine = list(images_dict.values())[0].affine
+        
+        # create stack of all images (including segmentation)
         images_dict = subject.get_images_dict(intensity_only=False)
         numpy_stack_list = []
         names_list = []
-
         for name, image in images_dict.items():
             numpy_stack_list.append(image.data.numpy().copy())
             names_list.append(name)
         numpy_stack = np.concatenate(numpy_stack_list, axis=0)
 
-        # crop away the zero planes on the whole stack
+        # crop away the external zero-planes on the whole stack
         new_corner_idxs, new_stack = crop_image_outside_zeros(array=numpy_stack, patch_size=self.patch_size)
 
-        # recompute origin of affine matrix using first image in dictionary
-        # first line in this method checked that all are the same
-        example_image_affine = list(images_dict.values())[0].affine
+        # recompute origin of affine matrix using initial image shape
         new_origin = nib.affines.apply_affine(example_image_affine, new_corner_idxs)
         new_affine = example_image_affine.copy()
         new_affine[:3, 3] = new_origin
 
-        # repopulate the subject data with the stack slices
+        # repopulate the subject data and shape
         for idx, array in enumerate(new_stack):
-
             images_dict[names_list[idx]]['data'] = torch.tensor(np.expand_dims(array, axis=0))
             images_dict[names_list[idx]]['affine'] = new_affine
 
