175eff6281166feb236cb2711ba351a0111c0696
sarthakpati <sarthak.pati@hotmail.com>
2021-01-29 12:54:34 -0500

    made everything into classes that depend on torchio bases

--- GANDLF/preprocessing.py
+++ GANDLF/preprocessing.py
@@ -170,66 +170,69 @@ class  ClipIntensities(NormalizationTransform):
     def is_invertible(self):
         return False
 
-def tensor_rotate_90(input_image, axis):
-    # with 'axis' axis of rotation, rotate 90 degrees
-    # tensor image is expected to be of shape (1, a, b, c)
-    if axis not in [1, 2, 3]:
-        raise ValueError("Axes must be in [1, 2, 3], but was provided as: ", axis)
-    relevant_axes = set([1, 2, 3])
-    affected_axes = list(relevant_axes - set([axis]) )
-    return torch.transpose(input_image, affected_axes[0], affected_axes[1]).flip(affected_axes[1])
+# def tensor_rotate_90(input_image, axis):
+#     # with 'axis' axis of rotation, rotate 90 degrees
+#     # tensor image is expected to be of shape (1, a, b, c)
+#     if axis not in [1, 2, 3]:
+#         raise ValueError("Axes must be in [1, 2, 3], but was provided as: ", axis)
+#     relevant_axes = set([1, 2, 3])
+#     affected_axes = list(relevant_axes - set([axis]) )
+#     return torch.transpose(input_image, affected_axes[0], affected_axes[1]).flip(affected_axes[1])
 
 
-def tensor_rotate_180(input_image, axis):
-    # with 'axis' axis of rotation, rotate 180 degrees
-    # tensor image is expected to be of shape (1, a, b, c)
-    if axis not in [1, 2, 3]:
-        raise ValueError("Axes must be in [1, 2, 3], but was provided as: ", axis)
-    relevant_axes = set([1, 2, 3])
-    affected_axes = list(relevant_axes - set([axis]) )
-    return input_image.flip(affected_axes[0]).flip(affected_axes[1]) 
+# def tensor_rotate_180(input_image, axis):
+#     # with 'axis' axis of rotation, rotate 180 degrees
+#     # tensor image is expected to be of shape (1, a, b, c)
+#     if axis not in [1, 2, 3]:
+#         raise ValueError("Axes must be in [1, 2, 3], but was provided as: ", axis)
+#     relevant_axes = set([1, 2, 3])
+#     affected_axes = list(relevant_axes - set([axis]) )
+#     return input_image.flip(affected_axes[0]).flip(affected_axes[1]) 
 
-# adapted from https://codereview.stackexchange.com/questions/132914/crop-black-border-of-image-using-numpy/132933#132933
-def crop_image_outside_zeros(array, psize):
-    dimensions = len(array.shape)
-    if dimensions != 4:
-        raise ValueError("Array expected to be 4D but got {} dimensions.".format(dimensions)) 
-    
-    # collapse to single channel and get the mask of non-zero voxels
-    mask = array.sum(axis=0) > 0
+# adapted from https://github.com/fepegar/torchio/blob/master/torchio/transforms/preprocessing/spatial/crop.py
+class  Rotate(SpatialTransform):
+    """
+    Rotation by 90 or 180 augmentation
 
-    # get the small and large corners
-  
-    m0 = mask.any(1).any(1)
-    m1 = mask.any(0)
-    m2 = m1.any(0)
-    m1 = m1.any(1)
-    
-    small = [m0.argmax(), m1.argmax(), m2.argmax()]
-    large = [m0[::-1].argmax(), m1[::-1].argmax(), m2[::-1].argmax()]
-    large = [m - l for m, l in zip(mask.shape, large)]
-    
-    # ensure we have a full patch
-    # for each axis
-    for i in range(3):
-        # if less than patch size, extend the small corner out
-        if large[i] - small[i] < psize[i]:
-            small[i] = large[i] - psize[i]
+    Args:
+        psize: patch size (used to ensure we do not crop to smaller size than this)
+        **kwargs: See :class:`~torchio.transforms.Transform` for additional
+            keyword arguments.
+    """
 
-        # if bottom fell off array, extend the large corner and set small to 0
-        if small[i] < 0:
-            small[i] = 0
-            large[i] = psize[i]
+    def __init__(self, degree, axis, **kwargs):
+        super().__init__(**kwargs)
+        self.degree = degree
+        if axis not in [1, 2, 3]:
+            raise ValueError("Axes must be in [1, 2, 3], but was provided as: ", axis)
+        self.axis = axis
 
-    # calculate pixel location of new bounding box corner (will use to update the reference of the image to physical space)
-    new_corner_idxs = np.array([small[0], small[1], small[2]])
-    # Get the contents of the bounding box from the array
-    new_array = array[:,
-                      small[0]:large[0],
-                      small[1]:large[1],
-                      small[2]:large[2]]
-    
-    return new_corner_idxs, new_array
+    def apply_transform(self, subject):
+        # get dictionary of images
+        images_dict = subject.get_images_dict(intensity_only=False)
+
+        # make sure shapes are consistent across images, and get this shape
+        subject.check_consistent_spatial_shape()
+        
+        relevant_axes = set([1, 2, 3])
+        affected_axes = list(relevant_axes - set([self.axis]))
+        
+        allDefined = True
+        for name, image in images_dict.items():
+            if self.degree == 90:
+                images_dict[name]['data'] = torch.transpose(images_dict[name]['data'], affected_axes[0], affected_axes[1]).flip(affected_axes[1])
+            elif self.degree == 180:
+                images_dict[name]['data'] = images_dict[name]['data'].flip(affected_axes[0]).flip(affected_axes[1]) 
+            else:
+                allDefined = False
+        
+        if not allDefined:    
+            degree_str = str(self.degree)
+            message = ('Rotation of ' + degree_str + ' is not defined')
+            raise RuntimeError(message)
+
+        return subject
+
 
 
 # adapted from https://github.com/fepegar/torchio/blob/master/torchio/transforms/preprocessing/spatial/crop.py
@@ -266,7 +269,7 @@ class  CropExternalZeroplanes(SpatialTransform):
         numpy_stack = np.concatenate(numpy_stack_list, axis=0)
 
         # crop away the external zero-planes on the whole stack
-        new_corner_idxs, new_stack = crop_image_outside_zeros(array=numpy_stack, psize=self.psize)
+        new_corner_idxs, new_stack = self.crop_image_outside_zeros(array=numpy_stack, psize=self.psize)
 
         # recompute origin of affine matrix using initial image shape
         new_origin = nib.affines.apply_affine(example_image_affine, new_corner_idxs)
@@ -279,6 +282,48 @@ class  CropExternalZeroplanes(SpatialTransform):
             images_dict[names_list[idx]]['affine'] = new_affine
 
         return subject
+
+    # adapted from https://codereview.stackexchange.com/questions/132914/crop-black-border-of-image-using-numpy/132933#132933
+    def crop_image_outside_zeros(array, psize):
+        dimensions = len(array.shape)
+        if dimensions != 4:
+            raise ValueError("Array expected to be 4D but got {} dimensions.".format(dimensions)) 
+        
+        # collapse to single channel and get the mask of non-zero voxels
+        mask = array.sum(axis=0) > 0
+
+        # get the small and large corners
+    
+        m0 = mask.any(1).any(1)
+        m1 = mask.any(0)
+        m2 = m1.any(0)
+        m1 = m1.any(1)
+        
+        small = [m0.argmax(), m1.argmax(), m2.argmax()]
+        large = [m0[::-1].argmax(), m1[::-1].argmax(), m2[::-1].argmax()]
+        large = [m - l for m, l in zip(mask.shape, large)]
+        
+        # ensure we have a full patch
+        # for each axis
+        for i in range(3):
+            # if less than patch size, extend the small corner out
+            if large[i] - small[i] < psize[i]:
+                small[i] = large[i] - psize[i]
+
+            # if bottom fell off array, extend the large corner and set small to 0
+            if small[i] < 0:
+                small[i] = 0
+                large[i] = psize[i]
+
+        # calculate pixel location of new bounding box corner (will use to update the reference of the image to physical space)
+        new_corner_idxs = np.array([small[0], small[1], small[2]])
+        # Get the contents of the bounding box from the array
+        new_array = array[:,
+                        small[0]:large[0],
+                        small[1]:large[1],
+                        small[2]:large[2]]
+        
+        return new_corner_idxs, new_array
 
     def is_invertible(self):
         return False
