2c151ef70a8989075e53215fc0b2de56ae02bf4c
sarthakpati <sarthak.pati@hotmail.com>
2021-01-29 13:10:39 -0500

    minor revert for crop

--- GANDLF/preprocessing.py
+++ GANDLF/preprocessing.py
@@ -234,6 +234,48 @@ class  Rotate(SpatialTransform):
         return subject
 
 
+# adapted from https://codereview.stackexchange.com/questions/132914/crop-black-border-of-image-using-numpy/132933#132933
+def crop_image_outside_zeros(array, psize):
+    dimensions = len(array.shape)
+    if dimensions != 4:
+        raise ValueError("Array expected to be 4D but got {} dimensions.".format(dimensions)) 
+    
+    # collapse to single channel and get the mask of non-zero voxels
+    mask = array.sum(axis=0) > 0
+
+    # get the small and large corners
+
+    m0 = mask.any(1).any(1)
+    m1 = mask.any(0)
+    m2 = m1.any(0)
+    m1 = m1.any(1)
+    
+    small = [m0.argmax(), m1.argmax(), m2.argmax()]
+    large = [m0[::-1].argmax(), m1[::-1].argmax(), m2[::-1].argmax()]
+    large = [m - l for m, l in zip(mask.shape, large)]
+    
+    # ensure we have a full patch
+    # for each axis
+    for i in range(3):
+        # if less than patch size, extend the small corner out
+        if large[i] - small[i] < psize[i]:
+            small[i] = large[i] - psize[i]
+
+        # if bottom fell off array, extend the large corner and set small to 0
+        if small[i] < 0:
+            small[i] = 0
+            large[i] = psize[i]
+
+    # calculate pixel location of new bounding box corner (will use to update the reference of the image to physical space)
+    new_corner_idxs = np.array([small[0], small[1], small[2]])
+    # Get the contents of the bounding box from the array
+    new_array = array[:,
+                    small[0]:large[0],
+                    small[1]:large[1],
+                    small[2]:large[2]]
+    
+    return new_corner_idxs, new_array
+
 
 # adapted from https://github.com/fepegar/torchio/blob/master/torchio/transforms/preprocessing/spatial/crop.py
 class  CropExternalZeroplanes(SpatialTransform):
@@ -269,7 +311,7 @@ class  CropExternalZeroplanes(SpatialTransform):
         numpy_stack = np.concatenate(numpy_stack_list, axis=0)
 
         # crop away the external zero-planes on the whole stack
-        new_corner_idxs, new_stack = self.crop_image_outside_zeros(array=numpy_stack, psize=self.psize)
+        new_corner_idxs, new_stack = crop_image_outside_zeros(array=numpy_stack, psize=self.psize)
 
         # recompute origin of affine matrix using initial image shape
         new_origin = nib.affines.apply_affine(example_image_affine, new_corner_idxs)
@@ -282,48 +324,6 @@ class  CropExternalZeroplanes(SpatialTransform):
             images_dict[names_list[idx]]['affine'] = new_affine
 
         return subject
-
-    # adapted from https://codereview.stackexchange.com/questions/132914/crop-black-border-of-image-using-numpy/132933#132933
-    def crop_image_outside_zeros(array, psize):
-        dimensions = len(array.shape)
-        if dimensions != 4:
-            raise ValueError("Array expected to be 4D but got {} dimensions.".format(dimensions)) 
-        
-        # collapse to single channel and get the mask of non-zero voxels
-        mask = array.sum(axis=0) > 0
-
-        # get the small and large corners
-    
-        m0 = mask.any(1).any(1)
-        m1 = mask.any(0)
-        m2 = m1.any(0)
-        m1 = m1.any(1)
-        
-        small = [m0.argmax(), m1.argmax(), m2.argmax()]
-        large = [m0[::-1].argmax(), m1[::-1].argmax(), m2[::-1].argmax()]
-        large = [m - l for m, l in zip(mask.shape, large)]
-        
-        # ensure we have a full patch
-        # for each axis
-        for i in range(3):
-            # if less than patch size, extend the small corner out
-            if large[i] - small[i] < psize[i]:
-                small[i] = large[i] - psize[i]
-
-            # if bottom fell off array, extend the large corner and set small to 0
-            if small[i] < 0:
-                small[i] = 0
-                large[i] = psize[i]
-
-        # calculate pixel location of new bounding box corner (will use to update the reference of the image to physical space)
-        new_corner_idxs = np.array([small[0], small[1], small[2]])
-        # Get the contents of the bounding box from the array
-        new_array = array[:,
-                        small[0]:large[0],
-                        small[1]:large[1],
-                        small[2]:large[2]]
-        
-        return new_corner_idxs, new_array
 
     def is_invertible(self):
         return False
