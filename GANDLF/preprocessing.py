aa4a919b804f77765fc5f983b8f21855587d3b0c
sarthakpati <sarthak.pati@hotmail.com>
2021-02-01 15:31:07 -0500

    minor syntax fix

--- GANDLF/preprocessing.py
+++ GANDLF/preprocessing.py
@@ -11,21 +11,21 @@ import nibabel as nib
 from torchio.data.subject import Subject
 from torchio.transforms.preprocessing.intensity.normalization_transform import NormalizationTransform, TypeMaskingMethod
 
-def threshold_intensities(input_tensor, min_val, max_val):
+def threshold_intensities(input_tensor, min, max):
     '''
     This function takes an input tensor and 2 thresholds, lower & upper and thresholds between them, basically making intensity values outside this range '0'
     '''
     C = torch.zeros(input_tensor.size())
-    l1_tensor = torch.where(input_tensor < max_val, input_tensor, C)
-    l2_tensor = torch.where(l1_tensor > min_val, l1_tensor, C)
+    l1_tensor = torch.where(input_tensor < max, input_tensor, C)
+    l2_tensor = torch.where(l1_tensor > min, l1_tensor, C)
     return l2_tensor
 
 
-def clip_intensities(input_tensor, min_val, max_val):
+def clip_intensities(input_tensor, min, max):
     '''
-    This function takes an input tensor and 2 thresholds, lower and upper and clips between them, basically making the lowest value as 'min_val' and largest values as 'max_val'
+    This function takes an input tensor and 2 thresholds, lower and upper and clips between them, basically making the lowest value as 'min' and largest values as 'max'
     '''
-    return torch.clamp(input_tensor, min_val, max_val)
+    return torch.clamp(input_tensor, min, max)
 
 
 def resize_image_resolution(input_image, output_size):
