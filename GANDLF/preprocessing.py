f64ac75871c55a8a5ff434fc66ea0a7a9594bb8f
Edwards <edwardsb@spr-gpu02.jf.intel.com>
2021-02-08 15:39:53 -0800

    now trying to write in nomalized image through subject.get_image_dict[image_name]

--- GANDLF/preprocessing.py
+++ GANDLF/preprocessing.py
@@ -93,7 +93,7 @@ class NonZeroNormalizeOnMaskedRegion(NormalizationTransform):
                 f' in image "{image_name}" ({image.path})'
             )
             raise RuntimeError(message)
-        subject[image_name] = standardized
+        subject.get_images_dict(intensity_only=True)[image_name] = standardized
 
     @staticmethod
     def znorm(tensor: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
