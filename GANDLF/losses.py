3a6445f83fab733527d3a04f1042e4bf4998514b
Siddhesh Thakur <sid.cre8er@gmail.com>
2021-02-14 23:21:12 +0530

    MSE_loss should consider the '0'th tensor

--- GANDLF/losses.py
+++ GANDLF/losses.py
@@ -123,7 +123,7 @@ def MSE(inp, target, reduction = 'mean'):
 
 def MSE_loss(inp, target, num_classes, reduction = 'mean'):
     acc_mse_loss = 0
-    for i in range(1, num_classes):
+    for i in range(0, num_classes):
         acc_mse_loss += MSE(inp[:,i,:,:,:], target[:,i,:,:,:], reduction = reduction)
     acc_mse_loss/=num_classes
     return acc_mse_loss
