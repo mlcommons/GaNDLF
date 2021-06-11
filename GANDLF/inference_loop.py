ef4b021440b08f9f044865a678e453ed318c95e0
sarthakpati <sarthak.pati@hotmail.com>
2021-06-11 09:48:00 -0400

    unnecessary imports removed

--- GANDLF/inference_loop.py
+++ GANDLF/inference_loop.py
@@ -2,32 +2,13 @@ from GANDLF.training_loop import validate_network
 import os
 os.environ['TORCHIO_HIDE_CITATION_PROMPT'] = '1' # hides torchio citation request, see https://github.com/fepegar/torchio/issues/235
 
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-import torch
-from torch.utils.data.dataset import Dataset
-import torch.optim as optim
-from torch.autograd import Variable
-import torch.nn as nn
+import pickle, argparse, torch
 import numpy as np
 import pandas as pd
 from torch.utils.data import DataLoader
-import random
-import torchio
-from torchio import Image, Subject
 from torchio.transforms import *
-from torchio import Image, Subject
-from sklearn.model_selection import KFold
 from skimage.io import imsave
-from shutil import copyfile
 from tqdm import tqdm
-import time
-import sys
-import ast 
-import pickle
-from pathlib import Path
-import argparse
-import datetime
-import SimpleITK as sitk
 from torch.cuda.amp import autocast
 from GANDLF.data.ImagesFromDataFrame import ImagesFromDataFrame
 from GANDLF.schd import *
