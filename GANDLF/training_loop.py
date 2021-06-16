9f43c2d4ab61107fb1bb2e0e27e69fb1f49a25e8
sarthakpati <sarthak.pati@hotmail.com>
2021-06-15 23:10:07 -0400

    tqdm added for dataloader iteration

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -10,6 +10,7 @@ import torch
 import time
 import torchio
 import psutil
+from tqdm import tqdm
 from torch.utils.data import DataLoader
 from GANDLF.logger import Logger
 from GANDLF.losses import one_hot
@@ -142,7 +143,7 @@ def train_network(model, train_dataloader, optimizer, params):
 
     # Set the model to train
     model.train()
-    for batch_idx, (subject) in enumerate(train_dataloader):
+    for batch_idx, (subject) in enumerate(tqdm(train_dataloader)):
         optimizer.zero_grad()
         image = (
             torch.cat(
@@ -263,7 +264,7 @@ def validate_network(model, valid_dataloader, scheduler, params, mode="validatio
         model.enable_medcam()
         params["medcam_enabled"] = True
 
-    for batch_idx, (subject) in enumerate(valid_dataloader):
+    for batch_idx, (subject) in enumerate(tqdm(valid_dataloader)):
         if params["verbose"]:
             print("== Current subject:", subject["subject_id"], flush=True)
 
