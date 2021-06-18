4f620427d460da985d08db7cf90a5a1644fb2810
Siddhesh Thakur <sid.cre8er@gmail.com>
2021-06-18 21:16:11 +0530

    removed unused parameters from UpsamplingModule.py

--- GANDLF/models/seg_modules/UpsamplingModule.py
+++ GANDLF/models/seg_modules/UpsamplingModule.py
@@ -11,13 +11,9 @@ class UpsamplingModule(nn.Module):
         output_channels,
         Conv,
         Dropout,
-        InstanceNorm,
-        leakiness=1e-2,
-        lrelu_inplace=True,
         kernel_size=3,
         scale_factor=2,
         conv_bias=True,
-        inst_norm_affine=True,
     ):
         """[summary]
 
@@ -28,18 +24,12 @@ class UpsamplingModule(nn.Module):
             output_channels {[type]} -- [description]
 
         Keyword Arguments:
-            leakiness {number} -- [description] (default: {1e-2})
-            lrelu_inplace {bool} -- [description] (default: {True})
             kernel_size {number} -- [description] (default: {3})
             scale_factor {number} -- [description] (default: {2})
             conv_bias {bool} -- [description] (default: {True})
-            inst_norm_affine {bool} -- [description] (default: {True})
         """
         nn.Module.__init__(self)
-        self.lrelu_inplace = lrelu_inplace
-        self.inst_norm_affine = inst_norm_affine
         self.conv_bias = conv_bias
-        self.leakiness = leakiness
         self.scale_factor = scale_factor
         if Conv == nn.Conv3d:
             mode = "trilinear"
@@ -65,5 +55,4 @@ class UpsamplingModule(nn.Module):
         Extends:
         """
         x = self.conv0(self.interpolate(x))
-        # print(x.shape)
         return x
