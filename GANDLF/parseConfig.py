8dffe15956dfebf85819732e5de850654ca29839
sarthakpati <sarthak.pati@hotmail.com>
2021-06-21 15:51:43 -0400

    revert

--- GANDLF/parseConfig.py
+++ GANDLF/parseConfig.py
@@ -21,7 +21,6 @@ parameter_defaults = {
     "learning_rate": 0.001,  # default learning rate
     "clip_grad": None,  # clip_gradient value
     "clip_mode": None,  # default clip mode
-    "log_memory": False,  # logging memory usage
 }
 
 ## dictionary to define string defaults for appropriate options
