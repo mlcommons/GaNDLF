068f401b1b29f415cc91976c69002cf2e6ddb5f5
sarthakpati <sarthak.pati@hotmail.com>
2021-06-21 16:16:37 -0400

    re-added the training_loop cli for gpu-farmed jobs

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -811,3 +811,38 @@ def training_loop(
         " mins",
         flush=True,
     )
+
+if __name__ == "__main__":
+
+    import argparse, pickle, pandas
+
+    torch.multiprocessing.freeze_support()
+    # parse the cli arguments here
+    parser = argparse.ArgumentParser(description = "Training Loop of GANDLF")
+    parser.add_argument('-train_loader_pickle', type=str, help = 'Train loader pickle', required=True)
+    parser.add_argument('-val_loader_pickle', type=str, help = 'Validation loader pickle', required=True)
+    parser.add_argument('-testing_loader_pickle', type=str, help = 'Testing loader pickle', required=True)
+    parser.add_argument('-parameter_pickle', type=str, help = 'Parameters pickle', required=True)
+    parser.add_argument('-outputDir', type=str, help = 'Output directory', required=True)
+    parser.add_argument('-device', type=str, help = 'Device to train on', required=True)
+    
+    args = parser.parse_args()
+
+    # # write parameters to pickle - this should not change for the different folds, so keeping is independent
+    parameters = pickle.load(open(args.parameter_pickle,"rb"))
+    trainingDataFromPickle = pandas.read_pickle(args.train_loader_pickle)
+    validationDataFromPickle = pandas.read_pickle(args.val_loader_pickle)
+    testingData_str = args.testing_loader_pickle
+    if testingData_str == 'None':
+        testingDataFromPickle = None
+    else:
+        testingDataFromPickle = pandas.read_pickle(testingData_str)
+
+    training_loop(
+        training_data=trainingDataFromPickle,
+        validation_data=validationDataFromPickle,
+        output_dir=args.outputDir,
+        device=args.device,
+        params=parameters,
+        testing_data=testingDataFromPickle,
+    )
