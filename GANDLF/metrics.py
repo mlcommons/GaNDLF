86b74649b18bf402d7a0d6330e2156ab6bd716e3
sarthakpati <sarthak.pati@hotmail.com>
2021-06-25 15:58:48 -0400

    added hausdorff implementation #109

--- GANDLF/metrics.py
+++ GANDLF/metrics.py
@@ -1,9 +1,16 @@
 """
 All the metrics are to be called from here
 """
-import torch
+import torch, numpy
 from .losses import MSE, MSE_loss
 from .utils import one_hot
+import SimpleITK as sitk
+from scipy.ndimage import _ni_support
+from scipy.ndimage.morphology import (
+    distance_transform_edt,
+    binary_erosion,
+    generate_binary_structure,
+)
 
 
 # Dice scores and dice losses
@@ -168,3 +175,96 @@ def fetch_metric(metric_name):
         print("Metric was undefined")
         metric_function = identity
     return metric_function
+
+
+def __surface_distances(result, reference, voxelspacing=None, connectivity=1):
+    """
+    The distances between the surface voxel of binary objects in result and their
+    nearest partner surface voxel of a binary object in reference.
+
+    Adopted from https://github.com/loli/medpy/blob/39131b94f0ab5328ab14a874229320efc2f74d98/medpy/metric/binary.py#L1195
+    """
+    result = numpy.atleast_1d(result.astype(numpy.bool))
+    reference = numpy.atleast_1d(reference.astype(numpy.bool))
+    if voxelspacing is not None:
+        voxelspacing = _ni_support._normalize_sequence(voxelspacing, result.ndim)
+        voxelspacing = numpy.asarray(voxelspacing, dtype=numpy.float64)
+        if not voxelspacing.flags.contiguous:
+            voxelspacing = voxelspacing.copy()
+
+    # binary structure
+    footprint = generate_binary_structure(result.ndim, connectivity)
+
+    # test for emptiness
+    if 0 == numpy.count_nonzero(result):
+        raise RuntimeError(
+            "The first supplied array does not contain any binary object."
+        )
+    if 0 == numpy.count_nonzero(reference):
+        raise RuntimeError(
+            "The second supplied array does not contain any binary object."
+        )
+
+    # extract only 1-pixel border line of objects
+    result_border = result ^ binary_erosion(result, structure=footprint, iterations=1)
+    reference_border = reference ^ binary_erosion(
+        reference, structure=footprint, iterations=1
+    )
+
+    # compute average surface distance
+    # Note: scipys distance transform is calculated only inside the borders of the
+    #       foreground objects, therefore the input has to be reversed
+    dt = distance_transform_edt(~reference_border, sampling=voxelspacing)
+    sds = dt[result_border]
+
+    return sds
+
+
+def hd95(result, reference, connectivity=1):
+    """
+    95th percentile of the Hausdorff Distance.
+    Computes the 95th percentile of the (symmetric) Hausdorff Distance (HD) between the binary objects in two
+    images. Compared to the Hausdorff Distance, this metric is slightly more stable to small outliers and is
+    commonly used in Biomedical Segmentation challenges.
+    Parameters
+    ----------
+    result : SimpleITK.Image
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    reference : SimpleITK.Image
+        Input data containing objects. Can be any type but will be converted
+        into binary: background where 0, object everywhere else.
+    voxelspacing : float or sequence of floats, optional
+        The voxelspacing in a distance unit i.e. spacing of elements
+        along each dimension. If a sequence, must be of length equal to
+        the input rank; if a single number, this is used for all axes. If
+        not specified, a grid spacing of unity is implied.
+    connectivity : int
+        The neighbourhood/connectivity considered when determining the surface
+        of the binary objects. This value is passed to
+        `scipy.ndimage.morphology.generate_binary_structure` and should usually be :math:`> 1`.
+        Note that the connectivity influences the result in the case of the Hausdorff distance.
+    Returns
+    -------
+    hd : float
+        The symmetric Hausdorff Distance between the object(s) in ```result``` and the
+        object(s) in ```reference```. The distance unit is the same as for the spacing of
+        elements along each dimension, which is usually given in mm.
+    See also
+    --------
+    :func:`hd`
+    Notes
+    -----
+    This is a real metric. The binary images can therefore be supplied in any order.
+    """
+    result_array = sitk.GetArrayFromImage(result)
+    reference_array = sitk.GetArrayFromImage(reference)
+    voxelspacing = result.GetSpacing()
+    if result.GetSpacing() != reference.GetSpacing():
+        raise ValueError("The reference and result images should have same resolutions")
+    if result.GetSize() != reference.GetSize():
+        raise ValueError("The reference and result images should have same shape")
+        
+    hd1 = __surface_distances(result_array, reference_array, voxelspacing, connectivity)
+    hd2 = __surface_distances(reference_array, result_array, voxelspacing, connectivity)
+    return numpy.percentile(numpy.hstack((hd1, hd2)), 95)
