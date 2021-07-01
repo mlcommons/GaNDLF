a17bae6387f5a640371a42f8645a68d261b3c8b2
sarthakpati <sarthak.pati@hotmail.com>
2021-07-01 14:38:52 -0400

    commenting fixes

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -638,7 +638,17 @@ def training_loop(
     output_dir,
     testing_data=None,
 ):
+    """
+    The main training loop.
 
+    Args:
+        training_data (pandas.DataFrame): The data to use for training.
+        validation_data (pandas.DataFrame): The data to use for validation.
+        device (str): The device to perform computations on.
+        params (dict): The parameters dictionary.
+        output_dir (str): The output directory.
+        testing_data (pandas.DataFrame): The data to use for testing.
+    """
     # Some autodetermined factors
     epochs = params["num_epochs"]
     params["device"] = device
