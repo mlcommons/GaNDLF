2fe71bbe25b335417046d54051b584cfa46b30de
Orhun <orhunguley@gmail.com>
2021-07-01 23:40:51 +0200

    testing 3d classification fixed.

--- GANDLF/utils.py
+++ GANDLF/utils.py
@@ -319,11 +319,14 @@ def populate_header_in_parameters(parameters, headers):
 
     if len(headers["predictionHeaders"]) > 0:
         parameters["model"]["num_classes"] = len(headers["predictionHeaders"])
-    is_regression, _, _ = find_problem_type(parameters["headers"], parameters["model"]["final_layer"])
-    
+    is_regression, _, _ = find_problem_type(
+        parameters["headers"], parameters["model"]["final_layer"]
+    )
+
     # if the problem type is classification/segmentation, ensure the number of classes are picked from the configuration
     if not is_regression:
         parameters["model"]["num_classes"] = len(parameters["model"]["class_list"])
+
     # initialize number of channels for processing
     if not ("num_channels" in parameters["model"]):
         parameters["model"]["num_channels"] = len(headers["channelHeaders"])
