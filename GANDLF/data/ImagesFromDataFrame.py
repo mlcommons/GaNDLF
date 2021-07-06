8c96ed691cc2f20b2f87783eefa1ab12dbfc52d5
sarthakpati <sarthak.pati@hotmail.com>
2021-07-06 17:19:28 -0400

    using new api

--- GANDLF/data/ImagesFromDataFrame.py
+++ GANDLF/data/ImagesFromDataFrame.py
@@ -344,7 +344,7 @@ def ImagesFromDataFrame(dataframe, parameters, train):
             # https://github.com/fepegar/torchio/discussions/587#discussioncomment-928834
             # this is causing memory usage to explode, see https://github.com/CBICA/GaNDLF/issues/128
             print("Checking consistency of images in subject '" + subject["subject_id"] + "'")
-            perform_sanity_check_on_subject(subject)
+            perform_sanity_check_on_subject(subject, parameters)
 
             # # padding image, but only for label sampler, because we don't want to pad for uniform
             if "label" in sampler or "weight" in sampler:
