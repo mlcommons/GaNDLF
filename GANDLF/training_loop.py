b6797d2cf54626827e761e358f25c1981972ec47
sarthakpati <sarthak.pati@hotmail.com>
2021-07-06 18:04:37 -0400

    black.

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -384,9 +384,7 @@ def validate_network(
             )
 
         # regression/classification problem AND label is present
-        if (
-            "value_keys" in params
-        ) and label_present:  
+        if ("value_keys" in params) and label_present:
             sampler = torchio.data.LabelSampler(params["patch_size"])
             tio_subject = torchio.Subject(subject_dict)
             generator = sampler(tio_subject, num_patches=params["q_samples_per_volume"])
