e3668efca4c92b290af43652f52ef43317b6556f
Sarthak Pati <sarthak.pati@pennmedicine.upenn.edu>
2021-07-28 22:56:25 -0400

    cuda fixes

--- GANDLF/losses.py
+++ GANDLF/losses.py
@@ -18,13 +18,14 @@ def cel(out, target, params):
     if len(target.shape) > 1 and target.shape[-1] == 1:
         target = torch.squeeze(target, -1)
 
+    class_weights = None
     if params["class_weights"]:
         class_weights = torch.FloatTensor(list(params["class_weights"].values()))
 
         # more examples you have in the training data, the smaller the weight you have in the loss
         class_weights = 1.0 / class_weights
 
-        class_weights .float().to(params["device"])
+        class_weights = class_weights.float().to(params["device"])
     
     cel = CrossEntropyLoss(weight=class_weights)
     return cel(out, target)
