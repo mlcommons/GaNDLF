7dd03cb35ba8b0d3ee626e6346fc050a4f9b6972
sarthakpati <sarthak.pati@hotmail.com>
2021-08-03 13:25:42 -0400

    ensure subject-specific spacing is getting stored in params

--- GANDLF/training_loop.py
+++ GANDLF/training_loop.py
@@ -358,6 +358,12 @@ def validate_network(
         if params["verbose"]:
             print("== Current subject:", subject["subject_id"], flush=True)
 
+        # ensure spacing is always present in params and is always subject-specific
+        if "subject_spacing" in subject:
+            params["subject_spacing"] = subject["spacing"]
+        else:
+            params["subject_spacing"] = None
+            
         # constructing a new dict because torchio.GridSampler requires torchio.Subject, which requires torchio.Image to be present in initial dict, which the loader does not provide
         subject_dict = {}
         label_ground_truth = None
