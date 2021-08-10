df5790459cce74573f5fe3bad2198e251ea0c1d9
sarthakpati <sarthak.pati@hotmail.com>
2021-08-10 10:48:28 -0400

    converted to lambdas for consistency

--- GANDLF/data/preprocessing/normalize_rgb.py
+++ GANDLF/data/preprocessing/normalize_rgb.py
@@ -1,6 +1,11 @@
-from torchvision.transforms import Normalize
+from functools import partial
+from torchio.transforms import (
+    Normalize,
+    Lambda,
+)
 
 
+## define the functions that need to wrapped into lambdas
 def normalize_by_val(input_tensor, mean, std):
     """
     This function returns the tensor normalized by these particular values
@@ -9,26 +14,32 @@ def normalize_by_val(input_tensor, mean, std):
     return normalizer(input_tensor)
 
 
-def normalize_imagenet(input_tensor):
-    """
-    This function returns the tensor normalized by standard imagenet values
-    """
-    return normalize_by_val(
-        input_tensor, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+# the "_transform" functions return lambdas that can be used to wrap into a Compose class
+def normalize_by_val_transform(mean, std, p=1):
+    return Lambda(
+        function=partial(normalize_by_val, mean=mean, std=std),
+        p=p,
     )
 
 
-def normalize_standardize(input_tensor):
-    """
-    This function returns the tensor normalized by subtracting 128 and dividing by 128
-    image = (image - 128)/128
-    """
-    return normalize_by_val(input_tensor, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+def normalize_imagenet_transform(p=1):
+    return Lambda(
+        function=partial(
+            normalize_by_val, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+        ),
+        p=p,
+    )
 
 
-def normalize_div_by_255(input_tensor):
-    """
-    This function divides all values of the input tensor by 255 on all channels
-    image = image/255
-    """
-    return normalize_by_val(input_tensor, mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0])
+def normalize_standardize_transform(p=1):
+    return Lambda(
+        function=partial(normalize_by_val, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        p=p,
+    )
+
+
+def normalize_div_by_255_transform(p=1):
+    return Lambda(
+        function=partial(normalize_by_val, mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0]),
+        p=p,
+    )
