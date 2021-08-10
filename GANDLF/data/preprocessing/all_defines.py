99e1bbea5156d30245ce6f9901f967539cea1f2c
sarthakpati <sarthak.pati@hotmail.com>
2021-08-10 11:38:11 -0400

    consistency

--- GANDLF/data/preprocessing/all_defines.py
+++ GANDLF/data/preprocessing/all_defines.py
@@ -1,4 +1,4 @@
-from .crop_zero_planes import crop_external_zero_planes
+from .crop_zero_planes import crop_external_zero_planes_transform
 from .non_zero_normalize import NonZeroNormalizeOnMaskedRegion
 from .threshold_and_clip import (
     threshold_transform,
@@ -28,13 +28,13 @@ def nonzero_voxel_mask(image):
 global_preprocessing_dict = {
     "threshold": threshold_transform,
     "clip": clip_transform,
-    "normalize": ZNormalization(),
-    "normalize_positive": ZNormalization(masking_method=positive_voxel_mask),
-    "normalize_nonZero": ZNormalization(masking_method=nonzero_voxel_mask),
-    "normalize_nonZero_masked": NonZeroNormalizeOnMaskedRegion(),
-    "crop_external_zero_planes": crop_external_zero_planes,
+    "crop_external_zero_planes": crop_external_zero_planes_transform,
     "normalize_by_val": normalize_by_val_transform,
     "normalize_imagenet": normalize_imagenet_transform,
     "normalize_standardize": normalize_standardize_transform,
     "normalize_div_by_255": normalize_div_by_255_transform,
+    "normalize": ZNormalization(),
+    "normalize_positive": ZNormalization(masking_method=positive_voxel_mask),
+    "normalize_nonZero": ZNormalization(masking_method=nonzero_voxel_mask),
+    "normalize_nonZero_masked": NonZeroNormalizeOnMaskedRegion(),
 }
