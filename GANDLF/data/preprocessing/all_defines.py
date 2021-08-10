e344a5b21baaecd441eb87865b8ede44b19537db
sarthakpati <sarthak.pati@hotmail.com>
2021-08-10 11:13:35 -0400

    added the transform signature in the same place as algorithm, for consistency

--- GANDLF/data/preprocessing/all_defines.py
+++ GANDLF/data/preprocessing/all_defines.py
@@ -1,12 +1,4 @@
-from functools import partial
-
-import numpy as np
-import torch
-import SimpleITK as sitk
-from torchvision.transforms import Normalize
-
-from GANDLF.utils import resample_image
-from .crop_zero_planes import CropExternalZeroplanes
+from .crop_zero_planes import crop_external_zero_planes
 from .non_zero_normalize import NonZeroNormalizeOnMaskedRegion
 from .threshold_and_clip import (
     threshold_transform,
@@ -30,57 +22,6 @@ def positive_voxel_mask(image):
 
 def nonzero_voxel_mask(image):
     return image != 0
-
-
-def crop_external_zero_planes(patch_size, p=1):
-    # p is only accepted as a parameter to capture when values other than one are attempted
-    if p != 1:
-        raise ValueError(
-            "crop_external_zero_planes cannot be performed with non-1 probability."
-        )
-    return CropExternalZeroplanes(patch_size=patch_size)
-
-
-## lambdas for pre-processing
-
-
-def resize_image_resolution(input_image, output_size):
-    """
-    This function gets the output image spacing based on the input image and output size
-    """
-    inputSize = input_image.GetSize()
-    outputSpacing = np.array(input_image.GetSpacing())
-    for i, n in enumerate(output_size):
-        outputSpacing[i] = outputSpacing[i] * (inputSize[i] / n)
-    return outputSpacing
-
-
-def apply_resize(input_image, preprocessing_params, interpolator=sitk.sitkLinear):
-    """
-    This function resizes the input image based on the output size and interpolator
-    """
-    return resample_image(
-        input_image,
-        resize_image_resolution(input_image, preprocessing_params["resize"]),
-        interpolator=interpolator,
-    )
-
-
-def get_tensor_for_dataloader(input_sitk_image):
-    """
-    This function obtains the tensor to load into the data loader
-    """
-    temp_array = sitk.GetArrayFromImage(input_sitk_image)
-    if (
-        temp_array.dtype == np.uint16
-    ):  # this is a contingency, because torch cannot convert this
-        temp_array = temp_array.astype(np.int32)
-    input_image_tensor = torch.from_numpy(temp_array).unsqueeze(
-        0
-    )  # single unsqueeze is always needed
-    if len(input_image_tensor.shape) == 3:  # this is for 2D images
-        input_image_tensor = input_image_tensor.unsqueeze(0)
-    return input_image_tensor
 
 
 # defining dict for pre-processing - key is the string and the value is the transform object
