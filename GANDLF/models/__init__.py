8fbdf54a52fb98abaeae4bdde603a45ca22de2a4
sarthakpati <sarthak.pati@hotmail.com>
2021-08-28 23:18:56 -0400

    added all models

--- GANDLF/models/__init__.py
+++ GANDLF/models/__init__.py
@@ -1,9 +1,21 @@
-from .unet import unet
+from .unet import unet, resunet
+from .uinc import uinc
+from .fcn import fcn
+from .vgg import vgg11, vgg13, vgg16, vgg19
+from .sdnet import SDNet
 
 global_models_dict = {
     "unet": unet,
-    "resunet": unet,
+    "resunet": resunet,
     "residualunet": unet,
+    "fcn": fcn,
+    "uinc": uinc,
+    "sdnet": SDNet,
+    "vgg": vgg19,
+    "vgg11": vgg11,
+    "vgg13": vgg13,
+    "vgg16": vgg16,
+    "vgg19": vgg19,
 }
 
 def get_model(parameters):
