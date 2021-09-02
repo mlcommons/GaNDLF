50e30fd9e3f26db650e4c4afe7c5a8b9c3b8b35c
sarthakpati <sarthak.pati@hotmail.com>
2021-09-02 14:15:27 -0400

    same fix for rgb normalize

--- GANDLF/data/preprocessing/normalize_rgb.py
+++ GANDLF/data/preprocessing/normalize_rgb.py
@@ -1,45 +1,56 @@
-from functools import partial
-from torchio.transforms import (
-    Lambda,
-)
+import torch
+
+from torchio.transforms.intensity_transform import IntensityTransform
+from torchio.data.subject import Subject
 from torchvision.transforms import Normalize
 
+# adapted from GANDLF's ThresholdOrClipTransform class
+class ThresholdRGBTransform(IntensityTransform):
+    """
+    Threshold the intensities of the RGB images in the subject.
 
-## define the functions that need to wrapped into lambdas
-def normalize_by_val(input_tensor, mean, std):
+        **kwargs: See :class:`~torchio.transforms.Transform` for additional keyword arguments.
     """
-    This function returns the tensor normalized by these particular values
-    """
-    normalizer = Normalize(mean, std)
-    return normalizer(input_tensor)
+
+    def __init__(self, mean=None, std=None, p=1, **kwargs):
+        super().__init__(p, mean, std, **kwargs)
+        self.mean = mean
+        self.std = std
+        self.probability = p
+
+    def apply_transform(self, subject: Subject) -> Subject:
+        for image_name, _ in self.get_images_dict(subject).items():
+            self.apply_normalize(subject, image_name)
+        return subject
+
+    def apply_normalize(
+        self,
+        subject: Subject,
+        image_name: str,
+    ) -> None:
+        image = subject[image_name].data
+        normalizer = Normalize(self.mean, self.std)
+        output = normalizer(image)
+        if output is None:
+            message = (
+                "Resultantant image is 0" f' in image "{image_name}" ({image.path})'
+            )
+            raise RuntimeError(message)
+        subject.get_images_dict(intensity_only=True)[image_name]["data"] = output
 
 
 # the "_transform" functions return lambdas that can be used to wrap into a Compose class
-def normalize_by_val_transform(mean, std, p=1):
-    return Lambda(
-        function=partial(normalize_by_val, mean=mean, std=std),
-        p=p,
-    )
+def normalize_by_val_transform(mean, std):
+    return ThresholdRGBTransform(p=1, mean=mean, std=std)
 
 
-def normalize_imagenet_transform(p=1):
-    return Lambda(
-        function=partial(
-            normalize_by_val, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-        ),
-        p=p,
-    )
+def normalize_imagenet_transform():
+    return ThresholdRGBTransform(p=1, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 
-def normalize_standardize_transform(p=1):
-    return Lambda(
-        function=partial(normalize_by_val, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        p=p,
-    )
+def normalize_standardize_transform():
+    return ThresholdRGBTransform(p=1, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 
 
-def normalize_div_by_255_transform(p=1):
-    return Lambda(
-        function=partial(normalize_by_val, mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0]),
-        p=p,
-    )
+def normalize_div_by_255_transform():
+    return ThresholdRGBTransform(p=1, mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0])
