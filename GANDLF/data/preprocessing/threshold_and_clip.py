1885a2c8ae9013fadc6ebcada2ec1171cbdba981
sarthakpati <sarthak.pati@hotmail.com>
2021-09-02 13:29:17 -0400

    fixing threshold by adding a new class

--- GANDLF/data/preprocessing/threshold_and_clip.py
+++ GANDLF/data/preprocessing/threshold_and_clip.py
@@ -26,14 +26,15 @@ def clip_intensities(input_tensor, min_thresh, max_thresh):
 
 # the "_transform" functions return lambdas that can be used to wrap into a Compose class
 def threshold_transform(parameters):
-    return Lambda(
-        function=partial(
-            threshold_intensities,
-            min_thresh=parameters["min"],
-            max_thresh=parameters["max"],
-        ),
-        p=1,
-    )
+    return ThresholdTransform(min_threshold=parameters["min"], max_threshold=parameters["max"])
+    # return Lambda(
+    #     function=partial(
+    #         threshold_intensities,
+    #         min_thresh=parameters["min"],
+    #         max_thresh=parameters["max"],
+    #     ),
+    #     p=1,
+    # )
 
 
 def clip_transform(parameters):
@@ -43,3 +44,46 @@ def clip_transform(parameters):
         ),
         p=1,
     )
+
+
+import torch
+
+from torchio.data.subject import Subject
+from torchio.transforms.intensity_transform import IntensityTransform
+
+# adapted from https://github.com/fepegar/torchio/blob/master/torchio/transforms/preprocessing/intensity/z_normalization.py
+class ThresholdTransform(IntensityTransform):
+    """
+    Threshold the intensities of the images in the subject.
+
+        **kwargs: See :class:`~torchio.transforms.Transform` for additional keyword arguments.
+    """
+
+    def __init__(self, min_threshold = None, max_threshold = None, **kwargs):
+        super().__init__(min_threshold, max_threshold, **kwargs)
+        self.min_thresh = min_threshold
+        self.max_thresh = max_threshold
+        self.probability = 1
+
+    def apply_transform(self, subject: Subject) -> Subject:
+        for image_name, _ in self.get_images_dict(subject).items():
+            self.apply_threshold(subject, image_name)
+        return subject
+
+    def apply_threshold(
+        self,
+        subject: Subject,
+        image_name: str,
+    ) -> None:
+        image = subject[image_name].data
+        C = torch.zeros(image.shape, dtype=image.dtype)
+        l1_tensor = torch.where(image < self.max_thresh, image, C)
+        l2_tensor = torch.where(l1_tensor > self.min_thresh, l1_tensor, C)
+        
+        if l2_tensor is None:
+            message = (
+                "Resultantant image is 0"
+                f' in image "{image_name}" ({image.path})'
+            )
+            raise RuntimeError(message)
+        subject.get_images_dict(intensity_only=True)[image_name]["data"] = l2_tensor
