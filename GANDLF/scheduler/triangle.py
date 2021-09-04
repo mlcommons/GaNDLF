7b50f6ef1367d1d66dc41dc315585c337f8173bd
sarthakpati <sarthak.pati@hotmail.com>
2021-09-04 10:20:27 -0400

    added cyclic lr schedulers

--- GANDLF/scheduler/triangle.py
+++ GANDLF/scheduler/triangle.py
@@ -1,4 +1,4 @@
-from torch.optim.lr_scheduler import LambdaLR
+from torch.optim.lr_scheduler import LambdaLR, CyclicLR
 import math
 
 
@@ -40,16 +40,15 @@ def base_triangle(parameters):
     """
     This function parses the parameters from the config file and returns the appropriate object
     """
-    step_size = 4 * parameters["batch_size"] * parameters["training_samples_size"]
 
-    # pick defaults for "min_lr", "max_lr" if not present in parameters
+    # pick defaults
     if not ("min_lr" in parameters["scheduler"]):
         parameters["scheduler"]["min_lr"] = 10 ** -3
     if not ("max_lr" in parameters["scheduler"]):
         parameters["scheduler"]["max_lr"] = 1
 
     clr = cyclical_lr(
-        step_size,
+        parameters["scheduler"]["step_size"],
         min_lr=parameters["scheduler"]["min_lr"],
         max_lr=parameters["scheduler"]["max_lr"],
     )
@@ -57,9 +56,8 @@ def base_triangle(parameters):
 
 
 def triangle_modified(parameters):
-    step_size = parameters["training_samples_size"] / parameters["learning_rate"]
 
-    # pick defaults for "min_lr", "max_lr", "max_lr_multiplier" if not present in parameters
+    # pick defaults
     if not ("min_lr" in parameters["scheduler"]):
         parameters["scheduler"]["min_lr"] = 0.000001
     if not ("max_lr" in parameters["scheduler"]):
@@ -68,9 +66,98 @@ def triangle_modified(parameters):
         parameters["scheduler"]["max_lr_multiplier"] = 1.0
 
     clr = cyclical_lr_modified(
-        step_size,
+        parameters["scheduler"]["step_size"],
         parameters["scheduler"]["min_lr"],
         parameters["scheduler"]["max_lr"],
         parameters["scheduler"]["max_lr_multiplier"],
     )
     return LambdaLR(parameters["optimizer"], [clr])
+
+"""
+elif which_scheduler == "triangular":
+    scheduler_lr = CyclicLR(
+        optimizer,
+        learning_rate * 0.001,
+        learning_rate,
+        step_size_up=step_size,
+        step_size_down=None,
+        mode="triangular",
+        gamma=1.0,
+        scale_fn=None,
+        scale_mode="cycle",
+        cycle_momentum=False,
+        base_momentum=0.8,
+        max_momentum=0.9,
+        last_epoch=-1,
+    )
+elif which_scheduler == "triangular2":
+    scheduler_lr = CyclicLR(
+        optimizer,
+        learning_rate * 0.001,
+        learning_rate,
+        step_size_up=step_size,
+        step_size_down=None,
+        mode="triangular2",
+        gamma=1.0,
+        scale_fn=None,
+        scale_mode="cycle",
+        cycle_momentum=False,
+        base_momentum=0.8,
+        max_momentum=0.9,
+        last_epoch=-1,
+    )
+elif which_scheduler == "exp_range":
+    scheduler_lr = CyclicLR(
+        optimizer,
+        learning_rate * 0.001,
+        learning_rate,
+        step_size_up=step_size,
+        step_size_down=None,
+        mode="exp_range",
+        gamma=1.0,
+        scale_fn=None,
+        scale_mode="cycle",
+        cycle_momentum=False,
+        base_momentum=0.8,
+        max_momentum=0.9,
+        last_epoch=-1,
+    )
+"""
+
+def cyclic_lr_base(parameters, mode="triangular"):
+    # pick defaults for "min_lr", "max_lr", "max_lr_multiplier" if not present in parameters
+    if not ("min_lr" in parameters["scheduler"]):
+        parameters["scheduler"]["min_lr"] = parameters["learning_rate"] * 0.001
+    if not ("max_lr" in parameters["scheduler"]):
+        parameters["scheduler"]["max_lr"] = parameters["learning_rate"]
+    if not ("gamma" in parameters["scheduler"]):
+        parameters["scheduler"]["gamma"] = 1.0
+    if not ("scale_mode" in parameters["scheduler"]):
+        parameters["scheduler"]["scale_mode"] = "cycle"
+    if not ("cycle_momentum" in parameters["scheduler"]):
+        parameters["scheduler"]["cycle_momentum"] = False
+    if not ("base_momentum" in parameters["scheduler"]):
+        parameters["scheduler"]["base_momentum"] = 0.8
+    if not ("max_momentum" in parameters["scheduler"]):
+        parameters["scheduler"]["max_momentum"] = 0.9
+    
+    return CyclicLR(
+        parameters["optimizer"],
+        parameters["learning_rate"] * 0.001,
+        parameters["learning_rate"],
+        step_size_up=parameters["scheduler"]["step_size"],
+        step_size_down=None,
+        mode=mode,
+        gamma=1.0,
+        scale_fn=None,
+        scale_mode=parameters["scheduler"]["scale_mode"],
+        cycle_momentum=parameters["scheduler"]["cycle_momentum"],
+        base_momentum=parameters["scheduler"]["base_momentum"],
+        max_momentum=parameters["scheduler"]["max_momentum"],
+    )
+
+def cyclic_lr_triangular2(parameters):
+    return cyclic_lr_base(parameters, mode="triangular2")
+
+def cyclic_lr_exp_range(parameters):
+    return cyclic_lr_base(parameters, mode="exp_range")
