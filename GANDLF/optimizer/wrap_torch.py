33f87d9c62cdfa2acdae9d904d6e7290ec4fd63c
sarthakpati <sarthak.pati@hotmail.com>
2021-09-04 11:38:27 -0400

    previous implementations of optimizer ready

--- GANDLF/optimizer/wrap_torch.py
+++ GANDLF/optimizer/wrap_torch.py
@@ -23,3 +23,16 @@ def asgd(parameters, model_parameters):
     if not ("weight_decay" in parameters["optimizer"]):
         parameters["optimizer"]["weight_decay"] = 0
     return ASGD(model_parameters, lr=parameters["learning_rate"], alpha=parameters["optimizer"]["alpha"], t0=parameters["optimizer"]["t0"], lambd=parameters["optimizer"]["lambd"], weight_decay=parameters["optimizer"]["weight_decay"])
+
+def adam(parameters, model_parameters):
+    # pick defaults
+    if not ("betas" in parameters["optimizer"]):
+        parameters["optimizer"]["betas"] = (0.9, 0.999)
+    if not ("weight_decay" in parameters["optimizer"]):
+        parameters["optimizer"]["weight_decay"] = 0.00005
+    if not ("eps" in parameters["optimizer"]):
+        parameters["optimizer"]["eps"] = 1e-8
+    if not ("amsgrad" in parameters["optimizer"]):
+        parameters["optimizer"]["amsgrad"] = False
+    return Adam(
+            model_parameters, lr=parameters["learning_rate"], betas=parameters["optimizer"]["betas"], weight_decay=parameters["optimizer"]["weight_decay"], eps=parameters["optimizer"]["eps"], amsgrad=parameters["optimizer"]["amsgrad"])
