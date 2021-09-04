654248cad3e9cad2eb466efcafaaa77b0649c19f
sarthakpati <sarthak.pati@hotmail.com>
2021-09-03 23:45:29 -0400

    init added

---
+++ GANDLF/scheduler/__init__.py
@@ -0,0 +1,7 @@
+from .triangle import base_triangle
+
+
+# defining dict for schedulers - key is the string and the value is the transform object
+global_schedulers_dict = {
+    "triangle": base_triangle
+}
