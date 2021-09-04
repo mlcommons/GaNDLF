5b48bba100cec95f50bc1d63a31164a29b0b4c22
sarthakpati <sarthak.pati@hotmail.com>
2021-09-03 23:49:17 -0400

    added triangle_modified

--- GANDLF/scheduler/__init__.py
+++ GANDLF/scheduler/__init__.py
@@ -1,7 +1,8 @@
-from .triangle import base_triangle
+from .triangle import base_triangle, triangle_modified
 
 
 # defining dict for schedulers - key is the string and the value is the transform object
 global_schedulers_dict = {
-    "triangle": base_triangle
+    "triangle": base_triangle,
+    "triangle_modified": triangle_modified,
 }
