f427276c94b4d72a0a94533fed78c4918b29e746
sarthakpati <sarthak.pati@hotmail.com>
2021-09-04 10:20:38 -0400

    defining in dict

--- GANDLF/scheduler/__init__.py
+++ GANDLF/scheduler/__init__.py
@@ -1,8 +1,11 @@
-from .triangle import base_triangle, triangle_modified
+from .triangle import base_triangle, triangle_modified, cyclic_lr_base, cyclic_lr_triangular2, cyclic_lr_exp_range
 
 
 # defining dict for schedulers - key is the string and the value is the transform object
 global_schedulers_dict = {
     "triangle": base_triangle,
     "triangle_modified": triangle_modified,
+    "triangular": cyclic_lr_base,
+    "triangular2": cyclic_lr_triangular2,
+    "exp_range": cyclic_lr_exp_range,
 }
