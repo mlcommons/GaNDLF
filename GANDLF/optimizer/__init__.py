d5e12861c169b5340c80d1c0f26166c87a643fd3
sarthakpati <sarthak.pati@hotmail.com>
2021-09-04 11:42:52 -0400

    added rprop

--- GANDLF/optimizer/__init__.py
+++ GANDLF/optimizer/__init__.py
@@ -1,7 +1,8 @@
-from .wrap_torch import sgd, asgd, adam
+from .wrap_torch import sgd, asgd, adam, rprop
 
 global_optimizer_dict = {
     'sgd': sgd,
     'asgd': asgd,
     'adam': adam,
+    'rprop': rprop,
 }
