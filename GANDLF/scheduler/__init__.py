e9ab7529c220826ef17c2e15aca312dcfc4d4be2
sarthakpati <sarthak.pati@hotmail.com>
2021-09-04 10:34:46 -0400

    add reduce on plateu

--- GANDLF/scheduler/__init__.py
+++ GANDLF/scheduler/__init__.py
@@ -1,4 +1,4 @@
-from .wrap_torch import base_triangle, triangle_modified, cyclic_lr_base, cyclic_lr_triangular2, cyclic_lr_exp_range, exp, step
+from .wrap_torch import base_triangle, triangle_modified, cyclic_lr_base, cyclic_lr_triangular2, cyclic_lr_exp_range, exp, step, reduce_on_plateau
 
 
 # defining dict for schedulers - key is the string and the value is the transform object
@@ -10,4 +10,8 @@ global_schedulers_dict = {
     "exp_range": cyclic_lr_exp_range,
     "exp": exp,
     "step": step,
+    "reduce_on_plateau": reduce_on_plateau,
+    "reduce-on-plateau": reduce_on_plateau,
+    "plateau": reduce_on_plateau,
+    "reduceonplateau": reduce_on_plateau,
 }
