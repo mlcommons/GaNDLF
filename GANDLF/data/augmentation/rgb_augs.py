1e884770b107ad0930a964b5298a3b00bd81b040
sarthakpati <sarthak.pati@hotmail.com>
2021-10-06 20:01:50 -0400

    updated with new class that only applies jitter to images

--- GANDLF/data/augmentation/rgb_augs.py
+++ GANDLF/data/augmentation/rgb_augs.py
@@ -1,14 +1,56 @@
 from functools import partial
 from torchvision.transforms import ColorJitter
 from torchio.transforms import Lambda
+from collections import defaultdict
+from typing import Tuple, Union, Dict, Sequence
 
-def colorjitter(parameters):
-    return Lambda(
-        function=partial(ColorJitter,
-            brightness=parameters["brightness"],
-            contrast=parameters["contrast"],
-            saturation=parameters["saturation"],
-            hue=parameters["hue"],
-        ),
-        p=parameters["probability"],
-    )
+from torchio.transforms.augmentation import RandomTransform
+from torchio.transforms import IntensityTransform
+from torchio import Subject
+
+class RandomColorJitter(RandomTransform, IntensityTransform):
+    r"""Add color jitter noise with random parameters.
+
+    Add color jitter with random parameters.
+
+    Args:
+        brightness (float or tuple of float (min, max)): How much to jitter brightness.
+            brightness_factor is chosen uniformly from [max(0, 1 - brightness), 1 + brightness]
+            or the given [min, max]. Should be non negative numbers.
+        contrast (float or tuple of float (min, max)): How much to jitter contrast.
+            contrast_factor is chosen uniformly from [max(0, 1 - contrast), 1 + contrast]
+            or the given [min, max]. Should be non negative numbers.
+        saturation (float or tuple of float (min, max)): How much to jitter saturation.
+            saturation_factor is chosen uniformly from [max(0, 1 - saturation), 1 + saturation]
+            or the given [min, max]. Should be non negative numbers.
+        hue (float or tuple of float (min, max)): How much to jitter hue.
+            hue_factor is chosen uniformly from [-hue, hue] or the given [min, max].
+            Should have 0<= hue <= 0.5 or -0.5 <= min <= max <= 0.5.
+        **kwargs: See :class:`~torchio.transforms.Transform` for additional
+            keyword arguments.
+    """
+    def __init__(
+            self,
+            brightness: Union[float, Tuple[float, float]] = 0.1,
+            contrast: Union[float, Tuple[float, float]] = 0,
+            saturation: Union[float, Tuple[float, float]] = 0,
+            hue: Union[float, Tuple[float, float]] = 0.2,
+            **kwargs
+            ):
+        super().__init__(**kwargs)
+        self.brightness_range = self._parse_range(brightness, 'brightness', min_constraint=0)
+        self.contrast_range = self._parse_range(contrast, 'contrast', min_constraint=0)
+        self.saturation_range = self._parse_range(saturation, 'saturation', min_constraint=0)
+        self.hue_range = self._parse_range(hue, 'hue', min_constraint=-0.5, max_constraint=0.5)
+
+    def apply_transform(self, subject: Subject) -> Subject:
+        brightness = self.sample_uniform(*self.brightness_range).item()
+        contrast = self.sample_uniform(*self.contrast_range).item()
+        saturation = self.sample_uniform(*self.saturation_range).item()
+        hue = self.sample_uniform(*self.hue_range).item()
+        transform = ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation, hue=hue)
+        for _, image in self.get_images_dict(subject).items():
+            image.set_data(transform(image.data))
+        
+        return subject
+
