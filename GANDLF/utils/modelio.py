357600cbeb0cb0e309ba78b48088ff615fc305e9
sarthakpati <sarthak.pati@hotmail.com>
2021-12-13 09:28:41 -0500

    added load model

--- GANDLF/utils/modelio.py
+++ GANDLF/utils/modelio.py
@@ -16,6 +16,27 @@ def save_model(model_dict, path):
 
     Args:
         model_dict (dict): Model dictionary to save.
-        path ([type]): [description]
+        path (str): The path to save the model dictionary to.
     """
     torch.save(model_dict, path)
+
+
+def load_model(path):
+    """
+    Load a model dictionary from a file.
+
+    Args:
+        path (str): The path to save the model dictionary to.
+
+    Returns:
+        dict: Model dictionary containing model parameters and metadata.
+    """
+    model_dict = torch.load(path)
+
+    # check if the model dictionary is complete
+    incomplete_keys = [key for key in model_dict_base.keys() if key not in model_dict.keys()]
+    
+    if len(incomplete_keys) > 0:
+        print("Model dictionary is incomplete; the following keys are missing:", incomplete_keys)
+    
+    return model_dict
