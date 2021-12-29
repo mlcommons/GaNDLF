4ef4dbb9d7f3ea23f1496f2d8dd58b3c4aa08829
sarthakpati <sarthak.pati@hotmail.com>
2021-12-29 09:35:46 -0500

    black .

--- GANDLF/metrics/__init__.py
+++ GANDLF/metrics/__init__.py
@@ -2,7 +2,14 @@
 All the metrics are to be called from here
 """
 from GANDLF.losses.regression import MSE_loss, CEL
-from .segmentation import multi_class_dice, multi_class_dice_per_label, hd100, hd100_per_label, hd95, hd95_per_label
+from .segmentation import (
+    multi_class_dice,
+    multi_class_dice_per_label,
+    hd100,
+    hd100_per_label,
+    hd95,
+    hd95_per_label,
+)
 from .regression import accuracy, classification_accuracy, balanced_acc_score
 from .generic import recall_score, precision_score, iou_score, f1_score
 
