6e6090b27db9e710bc61a1dfb29bc233b89924f6
sarthakpati <sarthak.pati@hotmail.com>
2022-01-13 13:34:47 -0500

    implementation updated to use `ModelBase`

--- GANDLF/models/vgg.py
+++ GANDLF/models/vgg.py
@@ -7,69 +7,43 @@ import sys, math
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .modelBase import get_final_layer
-from GANDLF.models.seg_modules.average_pool import (
-    GlobalAveragePooling3D,
-    GlobalAveragePooling2D,
-)
+from .modelBase import ModelBase
 
 
-class VGG(nn.Module):
+class VGG(ModelBase):
     """
     VGG model
     """
 
     def __init__(
         self,
-        parameters,
+        parameters: dict,
         configuration,
     ):
         """
         Initializer function for the VGG model
 
         Args:
-            n_dimensions (int): The number of dimensions in the input data (2 or 3).
             configuration (dict): A dictionary of configuration parameters for the model.
-            features (int): The number of features to extract from the input data.
-            n_outputClasses (int): The number of output classes.
-            final_convolution_layer (str, optional): The final layer of the model. Defaults to "softmax".
+            parameters (dict) - overall parameters dictionary; parameters specific for DenseNet:
         """
-        super(VGG, self).__init__()
-        n_dimensions = parameters["model"]["dimension"]
-        final_convolution_layer = parameters["model"]["final_layer"]
-        n_outputClasses = parameters["model"]["num_classes"]
+        super(VGG, self).__init__(parameters)
 
-        if "batch" in parameters["model"]["norm_type"].lower():
-            batch_norm = True
-        else:
-            batch_norm = False
-        self.features = make_layers(
+        self.features = self.make_layers(
             configuration,
-            n_dimensions,
-            parameters["model"]["num_channels"],
-            batch_norm=batch_norm,
+            self.n_channels,
         )
 
         # amp is not supported for vgg
         parameters["model"]["amp"] = False
 
-        self.final_convolution_layer = get_final_layer(final_convolution_layer)
-        if n_dimensions == 2:
-            self.Conv = nn.Conv2d
-            self.avg_pool = GlobalAveragePooling2D()
-        elif n_dimensions == 3:
-            self.Conv = nn.Conv3d
-            self.avg_pool = GlobalAveragePooling3D()
-        else:
-            sys.exit("Only 2D or 3D convolutions are supported.")
-
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            self.avg_pool,
+            self.GlobalAvgPool(),
             nn.ReLU(True),
             nn.Dropout(),
             # number of input features should be changed later, but works for all vgg right now
-            nn.Linear(512, n_outputClasses),
+            nn.Linear(512, self.n_classes),
         )
         # Initialize weights
         for m in self.modules():
@@ -93,27 +67,19 @@ class VGG(nn.Module):
         return out
 
 
-def make_layers(cfg, n_dimensions, in_channels, batch_norm=False):
-    layers = []
-    if n_dimensions == 2:
-        Conv = nn.Conv2d
-        MaxPool = nn.MaxPool2d
-        BatchNorm = nn.BatchNorm2d
-    elif n_dimensions == 3:
-        Conv = nn.Conv3d
-        MaxPool = nn.MaxPool3d
-        BatchNorm = nn.BatchNorm3d
-    for v in cfg:
-        if v == "M":
-            layers += [MaxPool(kernel_size=2, stride=2)]
-        else:
-            conv = Conv(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv, BatchNorm(v), nn.ReLU(inplace=True)]
+    def make_layers(self, cfg, in_channels):
+        layers = []
+        for v in cfg:
+            if v == "M":
+                layers += [self.MaxPool(kernel_size=2, stride=2)]
             else:
-                layers += [conv, nn.ReLU(inplace=True)]
-            in_channels = v
-    return nn.Sequential(*layers)
+                conv = self.Conv(in_channels, v, kernel_size=3, padding=1)
+                if self.norm_type in ["batch", "instance"]:
+                    layers += [conv, self.Norm(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv, nn.ReLU(inplace=True)]
+                in_channels = v
+        return nn.Sequential(*layers)
 
 
 cfg = {
