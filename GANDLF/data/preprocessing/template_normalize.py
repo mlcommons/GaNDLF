a8ee29f2426da7cb7b1e92da3a29e6cdb15e785c
sarthakpati <sarthak.pati@hotmail.com>
2022-02-05 22:12:01 -0500

    updated to handle no target, which triggers global histogram equalization

--- GANDLF/data/preprocessing/template_normalize.py
+++ GANDLF/data/preprocessing/template_normalize.py
@@ -1,4 +1,5 @@
 import SimpleITK as sitk
+import numpy as np
 
 from torchio.transforms.intensity_transform import IntensityTransform
 from torchio.data.subject import Subject
@@ -42,6 +43,23 @@ class HistogramMatching(TemplateNormalizeBase):
 
     def apply_normalize(self, image: ScalarImage) -> None:
         image_sitk = image.as_sitk()
-        target_sitk = sitk.ReadImage(self.target)
+        if self.target is not None:
+            target_sitk = sitk.ReadImage(self.target)
+        else:
+            # if target is not present, perform global histogram equalization
+            image_sitk_arr = sitk.GetArrayFromImage(image_sitk)
+            x=np.linspace(-1.0,1.0,np.array(image_sitk_arr.shape).prod(), dtype=image_sitk_arr.dtype)            
+            target_sitk = sitk.GetImageFromArray(x.reshape(image_sitk_arr.shape))
         return sitk.HistogramMatching(image_sitk, target_sitk, self.num_hist_level, self.num_match_points)
-    
+
+def histogram_matching(parameters):
+    """
+    This function is a wrapper for histogram matching.
+
+    Args:
+        parameters (dict): Dictionary of parameters.
+    """
+    num_hist_level = parameters.get("num_hist_level", 1024)
+    num_match_points = parameters.get("num_match_points", 16)
+    target = parameters.get("target", None)
+    return HistogramMatching(target=target, num_hist_level=num_hist_level, num_match_points=num_match_points)
