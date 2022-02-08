ef165e9a565c34185a08235e74997c732a1e229e
sarthakpati <sarthak.pati@hotmail.com>
2022-02-08 18:04:48 -0500

    added adaptive histogram equalization

--- GANDLF/parseConfig.py
+++ GANDLF/parseConfig.py
@@ -434,10 +434,13 @@ def parseConfig(config_file_path, version_check_flag=True):
 
                 if key == "histogram_equalization":
                     if params["data_preprocessing"][key] is not False:
-                        if not (isinstance(params["data_preprocessing"][key], dict)):
-                            params["data_preprocessing"][key] = {}
                         # if histogram equalization is enabled, call histogram_matching
                         params["data_preprocessing"]["histogram_matching"] = {}
+
+                if key == "adaptive_histogram_equalization":
+                    if params["data_preprocessing"][key] is not False:
+                        # if histogram equalization is enabled, call histogram_matching
+                        params["data_preprocessing"]["histogram_matching"] = {"target": "adaptive"}
 
     if "model" in params:
 
