2ee27f9c59830374fda4a59cb3791523c359ddd3
sarthakpati <sarthak.pati@hotmail.com>
2022-02-24 20:05:32 -0500

    print best epoch info

--- GANDLF/compute/training_loop.py
+++ GANDLF/compute/training_loop.py
@@ -470,6 +470,7 @@ def training_loop(
                 best_model_path,
             )
             first_model_saved = True
+            print("Best epoch: ", best_train_idx)
 
         if patience > params["patience"]:
             print(
