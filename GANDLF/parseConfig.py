69901109097215c96ca6325fa52131a9c1e606c6
sarthakpati <sarthak.pati@hotmail.com>
2022-03-10 17:10:04 -0500

    parsing resample and resize

--- GANDLF/parseConfig.py
+++ GANDLF/parseConfig.py
@@ -1,4 +1,4 @@
-import sys, yaml, ast, pkg_resources
+import sys, yaml, ast, pkg_resources, warnings
 import numpy as np
 
 from .utils import version_check
@@ -396,13 +396,19 @@ def parseConfig(config_file_path, version_check_flag=True):
                 "clamp",
             ]
 
+            resize_requested = False
+            for key in params["data_preprocessing"]:
+                if key in ["resize", "resize_image", "resize_images", "resize_patch"]:
+                    resize_requested = True
             if (
-                "resize" in params["data_preprocessing"]
+                resize_requested
                 and "resample" in params["data_preprocessing"]
             ):
-                print(
-                    "WARNING: 'resize' is ignored as 'resample' is defined under 'data_processing'",
-                    file=sys.stderr,
+                for key in ["resize", "resize_image", "resize_images", "resize_patch"]:
+                    params["data_preprocessing"].pop(key)
+                
+                warnings.warn(
+                    "WARNING: Different 'resize' operations are ignored as 'resample' is defined under 'data_processing'"
                 )
 
             # iterate through all keys
