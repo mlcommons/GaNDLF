77eaa1fe2f22eb82f7e53b180a0b27fc4c9353bd
Junwen Wu <junwen.wu@intel.com>
2022-03-09 17:25:39 -0800

    Resolve conflicts

--- GANDLF/parseConfig.py
+++ GANDLF/parseConfig.py
@@ -123,10 +123,10 @@ def parseConfig(config_file_path, version_check_flag=True):
             # ensuring same size during torchio processing
             params["patch_size"].append(1)
             if "dimension" not in params["model"]:
-                params["model"]["dimension"] == "2D"
+                params["model"]["dimension"] = 2
         elif len(params["patch_size"]) == 3:  # 2d check
             if "dimension" not in params["model"]:
-                params["model"]["dimension"] == "3D"            
+                params["model"]["dimension"] = 3
     else:
         sys.exit(
             "The 'patch_size' parameter needs to be present in the configuration file"
