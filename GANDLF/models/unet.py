84a71d177163e87aa116ca18ba08258e0f1831d0
Sofia-Mouchtaris <58452387+Sofia-Mouchtaris@users.noreply.github.com>
2022-03-17 08:38:17 -0400

    patch

--- GANDLF/models/unet.py
+++ GANDLF/models/unet.py
@@ -70,14 +70,14 @@ class unet(ModelBase):
             parameters["patch_size"], numlay=parameters["model"]["depth"]
         )
 
-        if patch_check != parameters["model"]["depth"] and patch_check > 1:
+        if patch_check != parameters["model"]["depth"] and patch_check >= 4:
             print(
-                "The patch size is not large enough for desired number of layers. It is expected that each dimension of the patch size is divisible by 2^i, where i is in a integer greater than or equal to 2. Only the first %d layers will run."
+                "The patch size is not large enough for desired depth. It is expected that each dimension of the patch size is divisible by 2^i, where i is in a integer greater than or equal to 4. Only the first %d layers will run."
                 % patch_check
             )
-        elif patch_check != parameters["model"]["depth"] and patch_check <= 1:
+        elif patch_check != parameters["model"]["depth"] and patch_check < 4:
             sys.exit(
-                "The patch size is not large enough for desired number of layers. It is expected that each dimension of the patch size is divisible by 2^i, where i is in a integer greater than or equal to 2."
+                "The patch size is not large enough for desired depth. It is expected that each dimension of the patch size is divisible by 2^i, where i is in a integer greater than or equal to 4."
             )
 
         self.num_layers = patch_check
