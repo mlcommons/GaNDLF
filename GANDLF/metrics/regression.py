fix for classification accuracy calculation during training

GANDLF/metrics/regression.py
@@ -11,7 +11,9 @@ def classification_accuracy(output, label, params):
         predicted_classes = torch.argmax(output, 1)
     else:
         predicted_classes = output
-    acc = torch.sum(predicted_classes == label) / len(label)
+        
+    
+    acc = torch.sum(predicted_classes == label.squeeze()) / len(label)
     return acc
 
 