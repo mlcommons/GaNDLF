1fd1eb2f355b93ca18144495b6d070ed1752c2f5
Siddhesh Thakur <sid.cre8er@gmail.com>
2022-03-22 01:27:25 +0530

    Added imagenet for vgg

--- GANDLF/models/__init__.py
+++ GANDLF/models/__init__.py
@@ -16,6 +16,12 @@ from .efficientnet import (
     efficientnetB6,
     efficientnetB7,
 )
+from .imagenet import (
+    imagenet_vgg16,
+    imagenet_vgg19,
+    imagenet_vgg16_bn,
+    imagenet_vgg19_bn,
+)
 from .sdnet import SDNet
 from .MSDNet import MSDNet
 from .brain_age import brainage
@@ -40,6 +46,10 @@ global_models_dict = {
     "vgg13": vgg13,
     "vgg16": vgg16,
     "vgg19": vgg19,
+    "imagenet_vgg16": imagenet_vgg16,
+    "imagenet_vgg19": imagenet_vgg19,
+    "imagenet_vgg16_bn": imagenet_vgg16_bn,
+    "imagenet_vgg19_bn": imagenet_vgg19_bn,
     "densenet": densenet264,
     "densenet121": densenet121,
     "densenet169": densenet169,
---
+++ GANDLF/models/imagenet.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Modified from https://github.com/pytorch/vision.git
+"""
+
+import torchvision
+import torch.nn as nn
+
+from .modelBase import ModelBase
+from GANDLF.models import modelBase
+
+def create_torchvision_model(modelname, pretrained=True, num_classes=2):
+    if modelname == "vgg16":
+        model = torchvision.models.vgg16(pretrained=pretrained)
+    if modelname == "vgg16_bn":
+        model = torchvision.models.vgg16_bn(pretrained=pretrained)
+    if modelname == "vgg19":
+        model = torchvision.models.vgg19(pretrained=pretrained)
+    if modelname == "vgg19_bn":
+        model = torchvision.models.vgg19_bn(pretrained=pretrained)
+    prev_out_features = model.classifier[3].out_features
+    model.classifier[6] = nn.Linear(
+        in_features=prev_out_features, out_features=num_classes
+    )
+    return model
+    
+class imagenet_vgg16(modelBase):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = create_torchvision_model("vgg16", pretrained=True, num_classes=self.n_classes)
+
+    def forward(self, x):
+        return self.model(x)
+
+class imagenet_vgg19(modelBase):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = create_torchvision_model("vgg19", pretrained=True, num_classes=self.n_classes)
+
+    def forward(self, x):
+        return self.model(x)
+
+class imagenet_vgg16_bn(modelBase):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = create_torchvision_model("vgg16_bn", pretrained=True, num_classes=self.n_classes)
+
+    def forward(self, x):
+        return self.model(x)
+
+class imagenet_vgg19_bn(modelBase):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = create_torchvision_model("vgg19_bn", pretrained=True, num_classes=self.n_classes)
+
+    def forward(self, x):
+        return self.model(x)
