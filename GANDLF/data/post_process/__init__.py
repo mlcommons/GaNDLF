c68bd9fb1951b18a57bf423a2ff32d35eb3f4654
sarthakpati <sarthak.pati@hotmail.com>
2022-03-23 08:50:54 -0400

    added global post-processing dict

--- GANDLF/data/post_process/__init__.py
+++ GANDLF/data/post_process/__init__.py
@@ -1,3 +1,6 @@
 from .morphology import torch_morphological, fill_holes
 
 from .tensor import get_mapped_label
+
+
+global_postprocessing_dict = {"fill_holes": fill_holes, "mapping": get_mapped_label}
