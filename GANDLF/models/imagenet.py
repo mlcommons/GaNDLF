bcd4f7114aeba7bd9e0ec072c106211e6f5c530e
sarthakpati <sarthak.pati@hotmail.com>
2022-03-29 18:02:39 -0400

    added shallower vggs

--- GANDLF/models/imagenet.py
+++ GANDLF/models/imagenet.py
@@ -10,6 +10,14 @@ from .modelBase import ModelBase
 
 
 def create_torchvision_model(modelname, pretrained=True, num_classes=2):
+    if modelname == "vgg11":
+        model = torchvision.models.vgg11(pretrained=pretrained)
+    if modelname == "vgg11_bn":
+        model = torchvision.models.vgg11_bn(pretrained=pretrained)
+    if modelname == "vgg13":
+        model = torchvision.models.vgg13(pretrained=pretrained)
+    if modelname == "vgg13_bn":
+        model = torchvision.models.vgg13_bn(pretrained=pretrained)
     if modelname == "vgg16":
         model = torchvision.models.vgg16(pretrained=pretrained)
     if modelname == "vgg16_bn":
@@ -23,6 +31,62 @@ def create_torchvision_model(modelname, pretrained=True, num_classes=2):
         in_features=prev_out_features, out_features=num_classes
     )
     return model
+
+
+class imagenet_vgg11(ModelBase):
+    def __init__(
+        self,
+        parameters,
+    ) -> None:
+        super(imagenet_vgg11, self).__init__(parameters)
+        self.model = create_torchvision_model(
+            "vgg11", pretrained=True, num_classes=self.n_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class imagenet_vgg11_bn(ModelBase):
+    def __init__(
+        self,
+        parameters,
+    ) -> None:
+        super(imagenet_vgg11_bn, self).__init__(parameters)
+        self.model = create_torchvision_model(
+            "vgg11_bn", pretrained=True, num_classes=self.n_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class imagenet_vgg13(ModelBase):
+    def __init__(
+        self,
+        parameters,
+    ) -> None:
+        super(imagenet_vgg13, self).__init__(parameters)
+        self.model = create_torchvision_model(
+            "vgg13", pretrained=True, num_classes=self.n_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class imagenet_vgg13_bn(ModelBase):
+    def __init__(
+        self,
+        parameters,
+    ) -> None:
+        super(imagenet_vgg13_bn, self).__init__(parameters)
+        self.model = create_torchvision_model(
+            "vgg13_bn", pretrained=True, num_classes=self.n_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
 
 
 class imagenet_vgg16(ModelBase):
@@ -39,20 +103,6 @@ class imagenet_vgg16(ModelBase):
         return self.model(x)
 
 
-class imagenet_vgg19(ModelBase):
-    def __init__(
-        self,
-        parameters,
-    ) -> None:
-        super(imagenet_vgg19, self).__init__(parameters)
-        self.model = create_torchvision_model(
-            "vgg19", pretrained=True, num_classes=self.n_classes
-        )
-
-    def forward(self, x):
-        return self.model(x)
-
-
 class imagenet_vgg16_bn(ModelBase):
     def __init__(
         self,
@@ -61,6 +111,20 @@ class imagenet_vgg16_bn(ModelBase):
         super(imagenet_vgg16_bn, self).__init__(parameters)
         self.model = create_torchvision_model(
             "vgg16_bn", pretrained=True, num_classes=self.n_classes
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class imagenet_vgg19(ModelBase):
+    def __init__(
+        self,
+        parameters,
+    ) -> None:
+        super(imagenet_vgg19, self).__init__(parameters)
+        self.model = create_torchvision_model(
+            "vgg19", pretrained=True, num_classes=self.n_classes
         )
 
     def forward(self, x):
