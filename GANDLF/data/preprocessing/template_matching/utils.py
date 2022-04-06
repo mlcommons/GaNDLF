784ac784286072b234fb0d0b10a86ac690aee61b
sarthakpati <sarthak.pati@hotmail.com>
2022-04-06 13:39:20 -0400

    added general-purpose utils

---
+++ GANDLF/data/preprocessing/template_matching/utils.py
@@ -0,0 +1,107 @@
+""" adapted from https://github.com/TissueImageAnalytics/tiatoolbox/blob/master/tiatoolbox/tools/stainextract.py """
+import numpy as np
+from skimage import exposure
+import cv2
+
+
+def contrast_enhancer(img, low_p=2, high_p=98):
+    """Enhancing contrast of the input image using intensity adjustment.
+       This method uses both image low and high percentiles.
+
+    Args:
+        img (:class:`numpy.ndarray`): input image used to obtain tissue mask.
+            Image should be uint8.
+        low_p (scalar): low percentile of image values to be saturated to 0.
+        high_p (scalar): high percentile of image values to be saturated to 255.
+            high_p should always be greater than low_p.
+
+    Returns:
+        img (:class:`numpy.ndarray`): Image (uint8) with contrast enhanced.
+
+    Raises:
+        AssertionError: Internal errors due to invalid img type.
+
+    Examples:
+        >>> from tiatoolbox import utils
+        >>> img = utils.misc.contrast_enhancer(img, low_p=2, high_p=98)
+
+    """
+    # check if image is not uint8
+    if not img.dtype == np.uint8:
+        raise AssertionError("Image should be uint8.")
+    img_out = img.copy()
+    p_low, p_high = np.percentile(img_out, (low_p, high_p))
+    if p_low >= p_high:
+        p_low, p_high = np.min(img_out), np.max(img_out)
+    if p_high > p_low:
+        img_out = exposure.rescale_intensity(
+            img_out, in_range=(p_low, p_high), out_range=(0.0, 255.0)
+        )
+    return np.uint8(img_out)
+
+
+def get_luminosity_tissue_mask(img, threshold):
+    """Get tissue mask based on the luminosity of the input image.
+
+    Args:
+        img (:class:`numpy.ndarray`): input image used to obtain tissue mask.
+        threshold (float): luminosity threshold used to determine tissue area.
+
+    Returns:
+        tissue_mask (:class:`numpy.ndarray`): binary tissue mask.
+
+    Examples:
+        >>> from tiatoolbox import utils
+        >>> tissue_mask = utils.misc.get_luminosity_tissue_mask(img, threshold=0.8)
+
+    """
+    img = img.astype("uint8")  # ensure input image is uint8
+    img = contrast_enhancer(img, low_p=2, high_p=98)  # Contrast  enhancement
+    img_lab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+    l_lab = img_lab[:, :, 0] / 255.0  # Convert to range [0,1].
+    tissue_mask = l_lab < threshold
+
+    # check it's not empty
+    if tissue_mask.sum() == 0:
+        raise ValueError("Empty tissue mask computed.")
+
+    return tissue_mask
+
+
+def rgb2od(img):
+    """Convert from RGB to optical density (OD_RGB) space.
+    RGB = 255 * exp(-1*OD_RGB).
+
+    Args:
+        img (:class:`numpy.ndarray` of type :class:`numpy.uint8`): Image RGB
+
+    Returns:
+        :class:`numpy.ndarray`: Optical denisty RGB image.
+
+    Examples:
+        >>> from tiatoolbox.utils import transforms, misc
+        >>> rgb_img = misc.imread('path/to/image')
+        >>> od_img = transforms.rgb2od(rgb_img)
+
+    """
+    mask = img == 0
+    img[mask] = 1
+    return np.maximum(-1 * np.log(img / 255), 1e-6)
+
+
+def dl_output_for_h_and_e(dictionary):
+    """Return correct value for H and E from dictionary learning output.
+
+    Args:
+        dictionary (:class:`numpy.ndarray`):
+            :class:`sklearn.decomposition.DictionaryLearning` output
+
+    Returns:
+        :class:`numpy.ndarray`:
+            With correct values for H and E.
+
+    """
+    if dictionary[0, 0] < dictionary[1, 0]:
+        return dictionary[[1, 0], :]
+
+    return dictionary
