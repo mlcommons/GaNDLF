7327711370f4d928679fb4e02f904684e4779fd3
sarthakpati <sarthak.pati@hotmail.com>
2022-04-06 14:28:04 -0400

    added tests

--- testing/test_full.py
+++ testing/test_full.py
@@ -1254,6 +1254,24 @@ def test_preprocess_functions():
     input_tensor = torch.rand(4, 256, 256, 1)
     input_transformed = global_preprocessing_dict["rgba2rgb"]()(input_tensor)
     assert input_transformed.shape[1] == 3, "Number of channels is not 3"
+    ## stain_normalization checks
+    input_tensor = 2 * torch.rand(3, 256, 256, 1) - 1
+    training_data, _ = parseTrainingCSV(inputDir + "/train_2d_rad_segmentation.csv")
+    parameters_temp = {}
+    parameters_temp["data_preprocessing"] = {}
+    parameters_temp["data_preprocessing"]["stain_normalizer"] = {
+        "target": training_data["Channel_0"][0]
+    }
+    for extractor in ["vahadane", "ruifrok", "macenko"]:
+        parameters_temp["data_preprocessing"]["stain_normalizer"]["extractor"] = extractor
+        non_zero_normalizer = global_preprocessing_dict["stain_normalizer"](
+            parameters_temp["data_preprocessing"]["stain_normalizer"]
+        )
+        input_transformed = non_zero_normalizer(input_tensor)
+        test = 1
+
+
+
     input_tensor = 2 * torch.rand(3, 256, 256, 1) - 1
     input_transformed = global_preprocessing_dict["normalize_div_by_255"](input_tensor)
     input_tensor = 2 * torch.rand(1, 3, 256, 256) - 1
@@ -1296,7 +1314,7 @@ def test_preprocess_functions():
     parameters_temp["data_preprocessing"] = {}
     parameters_temp["data_preprocessing"]["histogram_matching"] = {}
     non_zero_normalizer = global_preprocessing_dict["histogram_matching"](
-        parameters_temp
+        parameters_temp["data_preprocessing"]
     )
     input_transformed = non_zero_normalizer(input_tensor)
     # adaptive histogram equalization
@@ -1304,7 +1322,7 @@ def test_preprocess_functions():
     parameters_temp["data_preprocessing"] = {}
     parameters_temp["data_preprocessing"]["histogram_matching"] = {"target": "adaptive"}
     non_zero_normalizer = global_preprocessing_dict["histogram_matching"](
-        parameters_temp
+        parameters_temp["data_preprocessing"]
     )
     input_transformed = non_zero_normalizer(input_tensor)
     # histogram matching
@@ -1315,7 +1333,7 @@ def test_preprocess_functions():
         "target": training_data["Channel_0"][0]
     }
     non_zero_normalizer = global_preprocessing_dict["histogram_matching"](
-        parameters_temp
+        parameters_temp["data_preprocessing"]
     )
     input_transformed = non_zero_normalizer(input_tensor)
 
