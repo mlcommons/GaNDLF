863276952fb5a05d95c668996dbdc152c20444b6
Edwards <edwardsb@spr-gpu01.jf.intel.com>
2022-04-19 16:44:28 -0700

    moving get_final_layer method functionality to GANDLF.utils to avoid circular import issue.

--- GANDLF/models/__init__.py
+++ GANDLF/models/__init__.py
@@ -91,3 +91,35 @@ global_models_dict = {
     "efficientnetb6": efficientnetB6,
     "efficientnetb7": efficientnetB7,
 }
+
+def get_modelbase_final_layer(final_convolution_layer):
+    """
+    This function gets the final layer of the model.
+
+    Args:
+        final_convolution_layer (str): The final layer of the model as a string.
+
+    Returns:
+        Functional: sigmoid, softmax, or None
+    """
+    none_list = [
+        "none",
+        None,
+        "None",
+        "regression",
+        "classification_but_not_softmax",
+        "logits",
+        "classification_without_softmax",
+    ]
+
+    if final_convolution_layer in ["sigmoid", "sig"]:
+        final_convolution_layer = torch.sigmoid
+
+    elif final_convolution_layer in ["softmax", "soft"]:
+        final_convolution_layer = F.softmax
+
+    elif final_convolution_layer in none_list:
+        final_convolution_layer = None
+
+    return final_convolution_layer
+
