9e314ccf7e9a6c4a214ad475b50d71a4a6081b9b
sarthakpati <sarthak.pati@hotmail.com>
2022-04-25 17:24:26 -0400

    added unit test for this case

--- testing/test_full.py
+++ testing/test_full.py
@@ -1513,7 +1513,7 @@ def test_one_hot_logic():
     comparison = random_array == img_tensor_oh_rev_array
     assert comparison.all(), "Arrays are not equal"
 
-    class_list = [0, "1||2||3", np.max(random_array)]
+    class_list = ["0", "1||2||3", np.max(random_array)]
     img_tensor_oh = one_hot(img_tensor, class_list)
     img_tensor_oh_rev_array = reverse_one_hot(img_tensor_oh[0], class_list)
 
@@ -1542,6 +1542,23 @@ def test_one_hot_logic():
     for key, value in parameters["data_postprocessing"]["mapping"].items():
         comparison = (img_tensor_oh_rev_array == key) == (mapped_output == value)
         assert comparison.all(), "Arrays at {}:{} are not equal".format(key, value)
+
+    class_list = ["1||2||3", np.max(random_array)]
+    img_tensor_oh = one_hot(img_tensor, class_list)
+    img_tensor_oh_rev_array = reverse_one_hot(img_tensor_oh[0], class_list)
+
+    # check last foreground
+    comparison = (random_array == np.max(random_array)) == (
+        img_tensor_oh_rev_array == len(class_list)
+    )
+    assert comparison.all(), "Arrays at final foreground are not equal"
+
+    # check combined foreground
+    combined_array = np.logical_or(
+        np.logical_or((random_array == 1), (random_array == 2)), (random_array == 3)
+    )
+    comparison = combined_array == (img_tensor_oh_rev_array == 1)
+    assert comparison.all(), "Arrays at the combined foreground are not equal"
 
     print("passed")
 
