f1c890633c0ba321cf08954f456127b37a3c84d9
sarthakpati <sarthak.pati@hotmail.com>
2022-05-19 19:17:15 -0400

    removed imagenet_unet

--- testing/test_full.py
+++ testing/test_full.py
@@ -196,9 +196,8 @@ def test_train_segmentation_rad_2d(device):
     parameters["model"]["onnx_export"] = False
     parameters["data_preprocessing"]["resize_image"] = [224, 224]
     parameters = populate_header_in_parameters(parameters, parameters["headers"])
-    # read and initialize parameters for specific data dimension
     all_models_segmentation.extend(["imagenet_unet"])
-
+    # read and initialize parameters for specific data dimension
     for model in all_models_segmentation:
         if model == "imagenet_unet":
             # imagenet_unet can only handle a specific patch size
@@ -215,6 +214,7 @@ def test_train_segmentation_rad_2d(device):
             resume=False,
             reset=True,
         )
+    all_models_segmentation.remove("imagenet_unet")
 
     print("passed")
 
