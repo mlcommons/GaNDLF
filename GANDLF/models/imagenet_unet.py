cd6db22f595c5c9472ad871c67d13f26026ff069
sarthakpati <sarthak.pati@hotmail.com>
2022-05-19 16:00:18 -0400

    added new class for imagenet_unet

--- GANDLF/models/imagenet_unet.py
+++ GANDLF/models/imagenet_unet.py
@@ -4,19 +4,26 @@ import segmentation_models_pytorch as smp
 from .modelBase import ModelBase
 
 
-class imagenet_unet(ModelBase):
+class ImageNet_UNet(ModelBase):
     def __init__(
         self,
         parameters,
     ) -> None:
-        super(imagenet_unet, self).__init__(parameters)
+        super(ImageNet_UNet, self).__init__(parameters)
 
         # define a default encoder
         # all encoders: https://github.com/qubvel/segmentation_models.pytorch/blob/master/segmentation_models_pytorch/encoders/__init__.py
         encoder_name = parameters.get("encoder_name", "resnet34")
-        return smp.Unet(
+        self.model = smp.Unet(
             encoder_name=encoder_name,
             encoder_weights="imagenet",
             in_channels=self.n_channels,
             classes=self.n_classes,
         )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+def imagenet_unet_wrapper(parameters):
+    return ImageNet_UNet(parameters)
