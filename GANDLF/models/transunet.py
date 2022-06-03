e12b81d2d865ee66e3f371ea315a6c9b2269239a
geeks-sid <sid.cre8er@gmail.com>
2022-06-03 11:43:27 -0400

    sorted imports in gandlf.models.transunet

--- GANDLF/models/transunet.py
+++ GANDLF/models/transunet.py
@@ -2,23 +2,20 @@
 """
 Implementation of TransUNet
 """
-from torch.nn import ModuleList
-
-from GANDLF.models.seg_modules.DownsamplingModule import DownsamplingModule
-from GANDLF.models.seg_modules.EncodingModule import EncodingModule
-from GANDLF.models.seg_modules.DecodingModule import DecodingModule
-from GANDLF.models.seg_modules.UpsamplingModule import UpsamplingModule
-from GANDLF.models.seg_modules.in_conv import in_conv
-from GANDLF.models.seg_modules.out_conv import out_conv
-from .modelBase import ModelBase
 import sys
-from GANDLF.utils.generic import checkPatchDimensions
-from GANDLF.models.seg_modules.Interpolate import Interpolate
+
+import numpy as np
 import torch
 import torch.nn as nn
+from GANDLF.models.seg_modules.DecodingModule import DecodingModule
+from GANDLF.models.seg_modules.DownsamplingModule import DownsamplingModule
+from GANDLF.models.seg_modules.EncodingModule import EncodingModule
+from GANDLF.models.seg_modules.in_conv import in_conv
+from GANDLF.models.seg_modules.out_conv import out_conv
+from GANDLF.utils.generic import checkPatchDimensions
 from torch.nn import ModuleList
-import numpy as np
-import math
+
+from .modelBase import ModelBase
 from .unetr import _Transformer
 
 
