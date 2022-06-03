0d80b856335b9b5943043ca5889dc2e18c9276c3
Siddhesh Thakur <sid.cre8er@gmail.com>
2022-06-03 12:38:02 -0400

    added unreachable code

--- GANDLF/data/post_process/__init__.py
+++ GANDLF/data/post_process/__init__.py
@@ -1,4 +1,4 @@
-from .morphology import fill_holes
+from .morphology import fill_holes, torch_morphological
 from .tensor import get_mapped_label
 
 global_postprocessing_dict = {"fill_holes": fill_holes, "mapping": get_mapped_label}
