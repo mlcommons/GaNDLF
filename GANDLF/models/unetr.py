c1123f994bc79c13953fc516e40973812456370a
sarthakpati <sarthak.pati@hotmail.com>
2022-08-30 10:47:05 -0400

    fixes for E501

--- GANDLF/models/unetr.py
+++ GANDLF/models/unetr.py
@@ -294,8 +294,9 @@ def checkImgSize(img_size, number=4):
 
 class unetr(ModelBase):
     """
-    This is the standard U-Net architecture : https://arxiv.org/pdf/1606.06650.pdf. The 'residualConnections' flag controls residual connections The Downsampling, Encoding, Decoding modules
-    are defined in the seg_modules file. These smaller modules are basically defined by 2 parameters, the input channels (filters) and the output channels (filters),
+    This is the U-NetR architecture : https://arxiv.org/abs/2103.10504. 
+    The 'residualConnections' flag controls residual connections The Downsampling, Encoding, Decoding modules are defined in the seg_modules file.
+    These smaller modules are basically defined by 2 parameters, the input channels (filters) and the output channels (filters),
     and some other hyperparameters, which remain constant all the modules. For more details on the smaller modules please have a look at the seg_modules file.
     """
 
