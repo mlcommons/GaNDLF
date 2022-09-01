ce9dceb8db94ad8bda7fc7a99e56510057aa42ef
Siddhesh Thakur <sid.cre8er@gmail.com>
2022-09-01 14:22:34 -0400

    Update GANDLF/data/post_process/__init__.py
    Co-authored-by: Sarthak Pati <sarthak.pati@pennmedicine.upenn.edu>

--- GANDLF/data/post_process/__init__.py
+++ GANDLF/data/post_process/__init__.py
@@ -6,5 +6,6 @@ from .tensor import get_mapped_label
 global_postprocessing_dict = {
     "fill_holes": fill_holes,
     "mapping": get_mapped_label,
+    "morphology": torch_morphological,
     "cca": cca,
 }
