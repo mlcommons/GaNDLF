6c7f4c4b791240d37b543c42bcd34854b5a21378
sarthakpati <sarthak.pati@hotmail.com>
2022-09-11 12:42:32 -0400

    added variable to store post_processors that need to be applied after rev one-hot

--- GANDLF/data/post_process/__init__.py
+++ GANDLF/data/post_process/__init__.py
@@ -3,4 +3,11 @@ from .morphology import torch_morphological, fill_holes
 from .tensor import get_mapped_label
 
 
-global_postprocessing_dict = {"fill_holes": fill_holes, "mapping": get_mapped_label}
+global_postprocessing_dict = {
+    "fill_holes": fill_holes,
+    "mapping": get_mapped_label,
+    "morphology": torch_morphological,
+}
+
+# append post_processing functions that are to be be applied after reverse one-hot encoding
+postprocessing_after_reverse_one_hot_encoding = ["mapping"]
