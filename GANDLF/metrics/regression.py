02b803780cd0e09df0597422d4e457f1af2662c2
sarthakpati <sarthak.pati@hotmail.com>
2022-09-16 10:11:22 -0400

    added auc

--- GANDLF/metrics/regression.py
+++ GANDLF/metrics/regression.py
@@ -102,6 +102,7 @@ def overall_stats(predictions, ground_truth, params):
         "per_class_average": "macro",
         "per_class_weighted": "weighted",
     }
+    # metrics that need the "average" parameter
     for average_type, average_type_key in average_types_keys.items():
         calculators = {
             "accuracy": tm.Accuracy(
@@ -116,6 +117,10 @@ def overall_stats(predictions, ground_truth, params):
             "f1": tm.F1(
                 num_classes=params["model"]["num_classes"], average=average_type_key
             ),
+            ## weird error for multi-class problem, where pos_label is not getting set
+            # "aucroc": tm.AUROC(
+            #     num_classes=params["model"]["num_classes"], average=average_type_key
+            # ),
         }
         for metric_name, calculator in calculators.items():
             temp_output = calculator(predictions, ground_truth)
@@ -124,5 +129,18 @@ def overall_stats(predictions, ground_truth, params):
             else:
                 temp_output = temp_output.cpu().item()
             output_metrics[f"{metric_name}_{average_type}"] = temp_output
+    # metrics that do not have any "average" parameter
+    calculators = {
+        "auc": tm.AUC(reorder=True),
+        ## weird error for multi-class problem, where pos_label is not getting set
+        # "roc": tm.ROC(num_classes=params["model"]["num_classes"]),
+    }
+    for metric_name, calculator in calculators.items():
+        temp_output = calculator(predictions, ground_truth)
+        if temp_output.dim() > 0:
+            temp_output = temp_output.cpu().tolist()
+        else:
+            temp_output = temp_output.cpu().item()
+        output_metrics[metric_name] = temp_output
 
     return output_metrics
