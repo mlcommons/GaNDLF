664dfa125f33a586382a809a42ee326caec6f9eb
sarthakpati <sarthak.pati@hotmail.com>
2022-09-16 14:51:57 -0400

    commenting out extra `predict` interface

--- GANDLF/models/imagenet_unet.py
+++ GANDLF/models/imagenet_unet.py
@@ -40,23 +40,24 @@ class SegmentationModel(torch.nn.Module):
             labels = self.classification_head(features[-1])
             return labels
 
-    def predict(self, x):
-        """Inference method. Switch model to `eval` mode, call `.forward(x)` with `torch.no_grad()`
+    ## commented out because we are not using this interface
+    # def predict(self, x):
+    #     """Inference method. Switch model to `eval` mode, call `.forward(x)` with `torch.no_grad()`
 
-        Args:
-            x: 4D torch tensor with shape (batch_size, channels, height, width)
+    #     Args:
+    #         x: 4D torch tensor with shape (batch_size, channels, height, width)
 
-        Return:
-            prediction: 4D torch tensor with shape (batch_size, classes, height, width)
+    #     Return:
+    #         prediction: 4D torch tensor with shape (batch_size, classes, height, width)
 
-        """
-        if self.training:
-            self.eval()
+    #     """
+    #     if self.training:
+    #         self.eval()
 
-        with torch.no_grad():
-            x = self.forward(x)
+    #     with torch.no_grad():
+    #         x = self.forward(x)
 
-        return x
+    #     return x
 
 
 class Unet(SegmentationModel):
