5518deccfef839917f61e83a268b07f29b30684c
Sarthak Pati <sarthak.pati@pennmedicine.upenn.edu>
2022-12-20 14:06:25 -0500

    added a requirement for SimpleITK

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ requirements = [
     "black",
     "numpy==1.22.0",
     "scipy",
-    "SimpleITK!=2.0.*",
+    "SimpleITK!=2.0.*",    
+    "SimpleITK!=2.2.1",  # https://github.com/mlcommons/GaNDLF/issues/536
     "torchvision",
     "tqdm",
     "torchio==0.18.75",
