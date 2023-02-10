d79a50e64a51ffaa7e85b4b3d745d9e315ce9aba
Siddhesh Thakur <sid.cre8er@gmail.com>
2023-02-10 10:20:20 -0500

    Update handle_collisions.py

--- GANDLF/utils/handle_collisions.py
+++ GANDLF/utils/handle_collisions.py
@@ -7,6 +7,17 @@ def handle_collisions(df, output_path):
     # Create a dictionary to store the count of each subjectid
     subjectid_counts = {}
 
+    # Find the subjectid header
+    # Find the subjectID header
+    subject_id_column_name = None
+    for col in df.columns:
+        currentHeaderLoc = df.columns.get_loc(col)
+        col_lower = col.lower()
+        if ("subject" in col_lower) or ("patient" in col_lower) or ("pid" in col_lower):
+            subject_id_column_name = currentHeaderLoc
+        else:
+            raise ValueError("SubjectID not found in the header of the csv file")
+
     # Create the path to the collision.csv file and mapping.csv file
     collision_path = os.path.join(output_path, "collision.csv")
     mapping_path = os.path.join(output_path, "updated_test_mapping.csv")
@@ -19,7 +30,7 @@ def handle_collisions(df, output_path):
 
     # Loop through each row in the original dataframe
     for i, row in df.iterrows():
-        subjectid = row["SubjectID"]
+        subjectid = row[subject_id_column_name]
 
         # If the subjectid has not been seen before, add it to the dictionary
         if subjectid not in subjectid_counts:
@@ -33,7 +44,9 @@ def handle_collisions(df, output_path):
             new_df.at[i, subject_id_column_name] = new_subjectid
 
     # Write the colliding subjectids to the collision.csv file
-    pd.DataFrame({"SubjectID": collisions}).to_csv(collision_path, index=False)
+    pd.DataFrame({subject_id_column_name: collisions}).to_csv(
+        collision_path, index=False
+    )
 
     # Write the updated dataframe to the new_test_mapping.csv file
     new_df.to_csv(mapping_path, index=False)
@@ -41,3 +54,4 @@ def handle_collisions(df, output_path):
     collisions_found = len(collisions) > 0
 
     return collisions_found, new_df
+
