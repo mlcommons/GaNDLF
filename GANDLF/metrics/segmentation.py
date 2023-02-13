388afa133b43246286391f2d17ed6a3b2495e004
sarthakpati <sarthak.pati@hotmail.com>
2023-02-13 10:36:09 -0500

    improved comments

--- GANDLF/metrics/segmentation.py
+++ GANDLF/metrics/segmentation.py
@@ -1,7 +1,9 @@
 """
 All the segmentation metrics are to be called from here
 """
-import torch, numpy
+import sys
+import torch
+import numpy as np
 from GANDLF.losses.segmentation import dice
 from scipy.ndimage import _ni_support
 from scipy.ndimage.morphology import (
@@ -13,26 +15,16 @@ from scipy.ndimage.morphology import (
 
 def multi_class_dice(output, label, params, per_label=False):
     """
-    This function computes a multi-class dice
+    This function computes a multi-class dice.
 
-    Parameters
-    ----------
-    output : torch.Tensor
-        Input data containing objects. Can be any type but will be converted
-        into binary: background where 0, object everywhere else.
-    label : torch.Tensor
-        Input data containing objects. Can be any type but will be converted
-        into binary: background where 0, object everywhere else.
-    params : dict
-        The parameter dictionary containing training and data information.
-    per_label : bool
-        Whether the dice needs to be calculated per label or not.
+    Args:
+        output (torch.Tensor): Input data containing objects. Can be any type but will be converted into binary: background where 0, object everywhere else.
+        label (torch.Tensor): Input data containing objects. Can be any type but will be converted into binary: background where 0, object everywhere else.
+        params (dict): The parameter dictionary containing training and data information.
+        per_label (bool, optional): Whether the dice needs to be calculated per label or not. Defaults to False.
 
-    Returns
-    -------
-    total_dice : float or list
-        The average dice for all labels or a list of per-label dice scores.
-
+    Returns:
+        float or list: The average dice for all labels or a list of per-label dice scores.
     """
     total_dice = 0
     avg_counter = 0
@@ -55,23 +47,15 @@ def multi_class_dice(output, label, params, per_label=False):
 
 def multi_class_dice_per_label(output, label, params):
     """
-    This function computes a multi-class dice
+    This function computes a multi-class dice.
 
-    Parameters
-    ----------
-    output : torch.Tensor
-        Input data containing objects. Can be any type but will be converted
-        into binary: background where 0, object everywhere else.
-    label : torch.Tensor
-        Input data containing objects. Can be any type but will be converted
-        into binary: background where 0, object everywhere else.
-    params : dict
-        The parameter dictionary containing training and data information.
+    Args:
+        output (torch.Tensor): Input data containing objects. Can be any type but will be converted into binary: background where 0, object everywhere else.
+        label (torch.Tensor): Input data containing objects. Can be any type but will be converted into binary: background where 0, object everywhere else.
+        params (dict): The parameter dictionary containing training and data information.
 
-    Returns
-    -------
-    total_dice : list
-        The list of per-label dice scores.
+    Returns:
+        list: The list of per-label dice scores.
     """
     return multi_class_dice(output, label, params, per_label=True)
 
@@ -79,15 +63,22 @@ def multi_class_dice_per_label(output, label, params):
 def __surface_distances(result, reference, voxelspacing=None, connectivity=1):
     """
     The distances between the surface voxel of binary objects in result and their
-    nearest partner surface voxel of a binary object in reference.
+    nearest partner surface voxel of a binary object in reference. Adapted from https://github.com/loli/medpy/blob/39131b94f0ab5328ab14a874229320efc2f74d98/medpy/metric/binary.py#L1195.
 
-    Adopted from https://github.com/loli/medpy/blob/39131b94f0ab5328ab14a874229320efc2f74d98/medpy/metric/binary.py#L1195
+    Args:
+        result (torch.Tensor): Input prediction containing objects. Can be any type but will be converted into binary: background where 0, object everywhere else.
+        reference (torch.Tensor): Input ground truth containing objects. Can be any type but will be converted into binary: background where 0, object everywhere else.
+        voxelspacing (tuple): The size of each voxel, defaults to isotropic spacing of 1mm.
+        connectivity (int): The connectivity of regions. See scipy.ndimage.generate_binary_structure for more information.
+
+    Returns:
+        float: The symmetric Hausdorff Distance between the object(s) in ```result``` and the object(s) in ```reference```. The distance unit is the same as for the spacing of elements along each dimension, which is usually given in mm.
     """
-    result = numpy.atleast_1d(result.astype(numpy.bool))
-    reference = numpy.atleast_1d(reference.astype(numpy.bool))
+    result = np.atleast_1d(result.astype(np.bool))
+    reference = np.atleast_1d(reference.astype(np.bool))
     if voxelspacing is not None:
         voxelspacing = _ni_support._normalize_sequence(voxelspacing, result.ndim)
-        voxelspacing = numpy.asarray(voxelspacing, dtype=numpy.float64)
+        voxelspacing = np.asarray(voxelspacing, dtype=np.float64)
         if not voxelspacing.flags.contiguous:
             voxelspacing = voxelspacing.copy()
 
@@ -95,17 +86,9 @@ def __surface_distances(result, reference, voxelspacing=None, connectivity=1):
     footprint = generate_binary_structure(result.ndim, connectivity)
 
     # test for emptiness
-    if 0 == numpy.count_nonzero(result):
-        # print(
-        #     "The first supplied array does not contain any binary object.",
-        #     file=sys.stderr,
-        # )
+    if 0 == np.count_nonzero(result):
         return 0
-    if 0 == numpy.count_nonzero(reference):
-        # print(
-        #     "The second supplied array does not contain any binary object.",
-        #     file=sys.stderr,
-        # )
+    if 0 == np.count_nonzero(reference):
         return 0
 
     # extract only 1-pixel border line of objects
@@ -129,32 +112,19 @@ def hd_generic(inp, target, params, percentile=95, per_label=False):
     Computes the Hausdorff Distance (HD) between the binary objects in two
     images. Compared to the Hausdorff Distance, this metric is slightly more stable to small outliers and is
     commonly used in Biomedical Segmentation challenges.
-    Parameters
-    ----------
-    result : torch.tensor
-        Input data containing objects. Can be any type but will be converted
-        into binary: background where 0, object everywhere else.
-    reference : torch.tensor
-        Input data containing objects. Can be any type but will be converted
-        into binary: background where 0, object everywhere else.
-    params : dict
-        The parameter dictionary containing training and data information.
-    percentile : int
-        The percentile of surface distances to include during Hausdorff calculation.
-    per_label : bool
-        Whether the hausdorff needs to be calculated per label or not.
-    Returns
-    -------
-    hd : float
-        The symmetric Hausdorff Distance between the object(s) in ```result``` and the
-        object(s) in ```reference```. The distance unit is the same as for the spacing of
-        elements along each dimension, which is usually given in mm.
-    See also
-    --------
-    :func:`hd`
-    Notes
-    -----
-    This is a real metric. The binary images can therefore be supplied in any order.
+
+    Args:
+        inp (torch.Tensor): Input prediction containing objects. Can be any type but will be converted into binary: background where 0, object everywhere else.
+        target (torch.Tensor): Input ground truth containing objects. Can be any type but will be converted into binary: binary: background where 0, object everywhere else.
+        params (dict): The parameter dictionary containing training and data information.
+        percentile (int, optional): The percentile of surface distances to include during Hausdorff calculation. Defaults to 95.
+        per_label (bool, optional): Whether the hausdorff needs to be calculated per label or not. Defaults to False.
+
+    Returns:
+        float or list: The symmetric Hausdorff Distance between the object(s) in ```result``` and the object(s) in ```reference```. The distance unit is the same as for the spacing of elements along each dimension, which is usually given in mm.
+
+    See also:
+        :func:`hd`
     """
     result_array = inp.detach().cpu().numpy()
     target_array = target.detach().cpu().numpy()
@@ -182,7 +152,7 @@ def hd_generic(inp, target, params, percentile=95, per_label=False):
                     result_array[b, i, ...],
                     params["subject_spacing"][b],
                 )
-                current_hd = numpy.percentile(numpy.hstack((hd1, hd2)), percentile)
+                current_hd = np.percentile(np.hstack((hd1, hd2)), percentile)
                 hd += current_hd
                 hd_per_label.append(current_hd)
                 avg_counter += 1
