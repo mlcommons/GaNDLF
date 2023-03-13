5fe286e6b8a405ce2c3208a64f4df566a1099e95
Cory Gwin <gwincr11@github.com>
2023-03-13 20:07:01 +0000

    adding a devcontainer based on install instructions https://mlcommons.github.io/GaNDLF/setup this container checks for the presence of a gpu to determine how to install"

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# if there is a gpu present in the container, install the gpu version of pytorch
+# we will check for the presence of the nvidia-smi command as that signifies the presence of a gpu
+
+if command -v nvidia-smi &> /dev/null
+then
+	pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 -f https://download.pytorch.org/whl/torch_stable.html
+else
+	pip install torch==1.11.0+cpu torchvision==0.12.0+cpu torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cpu
+fi
+
+pip install openvino-dev==2022.1.0 # [OPTIONAL] to generate optimized models for inference
+pip install mlcube_docker # [OPTIONAL] to deploy GaNDLF models as MLCube-compliant Docker containers
+pip install -e .
