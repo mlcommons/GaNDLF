2c1f52e32940beaf514d92a09c3b00f3d9f4b91d
Cory Gwin <gwincr11@github.com>
2023-03-13 20:20:44 +0000

    add verify step

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -13,3 +13,4 @@ fi
 pip install openvino-dev==2022.1.0 # [OPTIONAL] to generate optimized models for inference
 pip install mlcube_docker # [OPTIONAL] to deploy GaNDLF models as MLCube-compliant Docker containers
 pip install -e .
+python ./gandlf_verifyInstall
