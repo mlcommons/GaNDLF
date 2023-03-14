b99239626f23da2f8ec5262c793deebe63203138
Siddhesh Thakur <sid.cre8er@gmail.com>
2023-03-14 16:11:36 -0400

    Added additional checks and docs for CEL loss

--- GANDLF/losses/regression.py
+++ GANDLF/losses/regression.py
@@ -4,11 +4,27 @@ from GANDLF.utils import one_hot
 
 
 def CEL(out, target, params):
+    """
+    Cross entropy loss with optional class weights.
+
+    Args:
+        out (torch.Tensor): Output tensor from the model.
+        target (torch.Tensor): Target tensor of class labels.
+        params (dict): Dictionary of parameters including weights.
+
+    Returns:
+        torch.Tensor: Cross entropy loss tensor.
+    """
     if len(target.shape) > 1 and target.shape[-1] == 1:
         target = torch.squeeze(target, -1)
 
     weights = None
-    if params["weights"]:
+    if params.get("weights") is not None:
+        # Check that the number of classes matches the number of weights
+        num_classes = len(params["weights"])
+        if out.shape[-1] != num_classes:
+            raise ValueError(f"Number of classes {num_classes} does not match output shape {out.shape[-1]}")
+        
         weights = torch.FloatTensor(list(params["weights"].values()))
         weights = weights.float().to(target.device)
 
