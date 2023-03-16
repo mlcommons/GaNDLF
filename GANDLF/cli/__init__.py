ff5966579549ddbcfcc044a8f7381c4bb9b797bb
sarthakpati <sarthak.pati@hotmail.com>
2023-03-16 19:55:01 -0400

    added new functions to respective inits

--- GANDLF/cli/__init__.py
+++ GANDLF/cli/__init__.py
@@ -4,6 +4,7 @@ from .preprocess_and_save import preprocess_and_save
 from .config_generator import config_generator
 from .deploy import deploy_targets, run_deployment
 from .recover_config import recover_config
+from .post_training_model_optimization import post_training_model_optimization
 
 from datetime import date
 
