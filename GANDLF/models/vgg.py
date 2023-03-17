30cb58dee569f989bbbeaea27b947ce13a7b8b56
Geeks-sid <sid.cre8er@gmail.com>
2023-03-17 12:37:13 -0400

    updated documentation for VGG

--- GANDLF/models/vgg.py
+++ GANDLF/models/vgg.py
@@ -16,9 +16,7 @@ class VGG(ModelBase):
     """
 
     def __init__(
-        self,
-        parameters: dict,
-        configuration,
+        self, parameters: dict, configuration,
     ):
         """
         Initializer function for the VGG model
@@ -29,23 +27,22 @@ class VGG(ModelBase):
         """
         super(VGG, self).__init__(parameters)
 
-        self.features = self.make_layers(
-            configuration,
-            self.n_channels,
-        )
-
         # amp is not supported for vgg
         parameters["model"]["amp"] = False
+
+        self.features = self.make_layers(configuration, self.n_channels)
 
         self.classifier = nn.Sequential(
             nn.Dropout(),
             self.GlobalAvgPool(),
             nn.ReLU(True),
             nn.Dropout(),
-            # number of input features should be changed later, but works for all vgg right now
-            nn.Linear(512, self.n_classes),
+            nn.Linear(
+                512, self.n_classes
+            ),  # Note from devs: number of input features should be changed later, but works for all vgg right now
         )
-        # Initialize weights
+
+        # Initialize weights, if convolutional use He initialization, if linear use Xavier initialization
         for m in self.modules():
             if isinstance(m, self.Conv):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -57,34 +54,62 @@ class VGG(ModelBase):
                 pass
 
     def forward(self, x):
-        out = self.features(x)
-        out = self.classifier(out)
+        """
+        Forward pass function of the VGG model.
+
+        Args:
+            x (tensor): Input tensor of shape (batch_size, n_channels, height, width).
+
+        Returns:
+            tensor: Output tensor of shape (batch_size, n_classes).
+        """
+        features_output = self.features(x)
+        classifier_output = self.classifier(features_output)
         if not self.final_convolution_layer is None:
             if self.final_convolution_layer == F.softmax:
-                out = self.final_convolution_layer(out, dim=1)
+                # Apply softmax activation to the output tensor if specified in the configuration
+                classifier_output = self.final_convolution_layer(
+                    classifier_output, dim=1
+                )
             else:
-                out = self.final_convolution_layer(out)
-        return out
+                # Apply whatever final layer specified in the configuration to the output tensor
+                classifier_output = self.final_convolution_layer(classifier_output)
+        return classifier_output
 
-    def make_layers(self, cfg, in_channels):
+    def make_layers(self, layer_config, input_channels):
+        """
+        Function to create convolutional layers for the VGG model based on the given layer configuration
+        for VGG based models including VGG11, VGG13, VGG16, VGG19.
+
+        Args:
+            layer_config (list): A list containing the configuration of convolutional layers and max pooling layers
+            in_channels (int): The number of input channels
+
+        Returns:
+            nn.Sequential: A sequential module containing the convolutional layers
+        """
         layers = []
-        for v in cfg:
-            if v == "M":
+        for layer in layer_config:
+            if layer == "M":
+                # If you found M, then add a maxpool layer
                 layers += [self.MaxPool(kernel_size=2, stride=2)]
             else:
-                conv = self.Conv(in_channels, v, kernel_size=3, padding=1)
+                # Otherwise, add a convolutional layer with the number of channels
+                conv = self.Conv(input_channels, layer_config, kernel_size=3, padding=1)
                 if self.norm_type in ["batch", "instance"]:
-                    layers += [conv, self.Norm(v), nn.ReLU(inplace=True)]
+                    layers += [conv, self.Norm(layer_config), nn.ReLU(inplace=True)]
                 else:
                     layers += [conv, nn.ReLU(inplace=True)]
-                in_channels = v
+                input_channels = layer_config
         return nn.Sequential(*layers)
 
 
+# Layer configuration for VGG models, as per the paper and M represents maxpool
+# and the integers represent the number of channels in the convolutional layers
 cfg = {
-    "A": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
-    "B": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
-    "D": [
+    "VGG11": [64, "M", 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "VGG13": [64, 64, "M", 128, 128, "M", 256, 256, "M", 512, 512, "M", 512, 512, "M"],
+    "VGG16": [
         64,
         64,
         "M",
@@ -104,7 +129,7 @@ cfg = {
         512,
         "M",
     ],
-    "E": [
+    "VGG19": [
         64,
         64,
         "M",
@@ -132,47 +157,75 @@ cfg = {
 
 class vgg11(VGG):
     """
-    VGG model
+    A class representing the VGG11 model, which is a variant of the VGG model architecture.
+
+    Inherits from the VGG class and specifies the configuration for the VGG11 architecture.
     """
 
     def __init__(
-        self,
-        parameters,
+        self, parameters,
     ):
-        super(vgg11, self).__init__(parameters=parameters, configuration=cfg["A"])
+        """
+        Initializes the VGG11 model with the given parameters.
+
+        Args:
+            parameters (dict): A dictionary containing the parameters for the VGG11 model.
+        """
+        super(vgg11, self).__init__(parameters=parameters, configuration=cfg["VGG11"])
 
 
 class vgg13(VGG):
     """
-    VGG model
+    A class representing the VGG13 model, which is a variant of the VGG model architecture.
+
+    Inherits from the VGG class and specifies the configuration for the VGG13 architecture.
     """
 
     def __init__(
-        self,
-        parameters,
+        self, parameters,
     ):
-        super(vgg13, self).__init__(parameters=parameters, configuration=cfg["B"])
+        """
+        Initializes the VGG13 model with the given parameters.
+
+        Args:
+            parameters (dict): A dictionary containing the parameters for the VGG13 model.
+        """
+        super(vgg13, self).__init__(parameters=parameters, configuration=cfg["VGG13"])
 
 
 class vgg16(VGG):
     """
-    VGG model
+    A class representing the VGG16 model, which is a variant of the VGG model architecture.
+
+    Inherits from the VGG class and specifies the configuration for the VGG16 architecture.
     """
 
     def __init__(
-        self,
-        parameters,
+        self, parameters,
     ):
-        super(vgg16, self).__init__(parameters=parameters, configuration=cfg["D"])
+        """
+        Initializes the VGG16 model with the given parameters.
+
+        Args:
+            parameters (dict): A dictionary containing the parameters for the VGG16 model.
+        """
+        super(vgg16, self).__init__(parameters=parameters, configuration=cfg["VGG16"])
 
 
 class vgg19(VGG):
     """
-    VGG model
+    A class representing the VGG19 model, which is a variant of the VGG model architecture.
+
+    Inherits from the VGG class and specifies the configuration for the VGG19 architecture.
     """
 
     def __init__(
-        self,
-        parameters,
+        self, parameters,
     ):
-        super(vgg19, self).__init__(parameters=parameters, configuration=cfg["E"])
+        """
+        Initializes the VGG19 model with the given parameters.
+
+        Args:
+            parameters (dict): A dictionary containing the parameters for the VGG19 model.
+        """
+        super(vgg19, self).__init__(parameters=parameters, configuration=cfg["VGG19"])
