15c255c6beaf2f198e1d4f910f80de836a73a822
sarthakpati <sarthak.pati@hotmail.com>
2023-03-24 09:36:36 -0400

    moving gandlf installation after torch

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,4 +10,5 @@ else
 	pip install torch==1.13.1+cpu torchvision==0.14.1+cpu torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cpu
 fi
 
+pip install -e .
 python ./gandlf_verifyInstall
