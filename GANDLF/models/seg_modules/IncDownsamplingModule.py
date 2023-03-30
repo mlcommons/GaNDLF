d305c63ae2a74945a2d683ed4ad69e2eb026aa9e
Geeks-sid <sid.cre8er@gmail.com>
2023-03-30 11:11:47 -0400

    updates to IncDownsamplingModule

--- GANDLF/models/seg_modules/IncDownsamplingModule.py
+++ GANDLF/models/seg_modules/IncDownsamplingModule.py
@@ -21,26 +21,19 @@ class IncDownsamplingModule(nn.Module):
         The output tensor is then passed through an instance normalization layer and a leaky ReLU activation function.
 
         Args:
-            input_channels (int):
-                The number of input channels in the tensor.
-            output_channels (int):
-                The number of output channels in the tensor.
-            Conv (torch.nn.Module):
-                The type of convolution layer to use.
-            Dropout (torch.nn.Module):
-                The type of dropout layer to use.
-            InstanceNorm (torch.nn.Module):
-                The type of instance normalization layer to use.
-            leakiness (float, optional):
-                The negative slope of the leaky ReLU activation function. Defaults to 1e-2.
-            kernel_size (int, optional):
-                The kernel size of the 1x1 convolution layer. Defaults to 1.
-            conv_bias (bool, optional):
-                Whether to include a bias term in the 1x1 convolution layer. Defaults to True.
-            inst_norm_affine (bool, optional):
-                Whether to include an affine transform in the instance normalization layer. Defaults to True.
-            lrelu_inplace (bool, optional):
-                Whether to use the input tensor for the output of the leaky ReLU activation function. Defaults to True.
+            input_channels (int): The number of input channels in the tensor.
+            output_channels (int): The number of output channels in the tensor.
+            Conv (torch.nn.Module): The type of convolution layer to use.
+            Dropout (torch.nn.Module): The type of dropout layer to use.
+            InstanceNorm (torch.nn.Module): The type of instance normalization layer to use.
+            leakiness (float, optional): The negative slope of the leaky ReLU activation function. Defaults to 1e-2.
+            kernel_size (int, optional): The kernel size of the 1x1 convolution layer. Defaults to 1.
+            conv_bias (bool, optional): Whether to include a bias term in the 1x1 convolution layer.
+                                        Defaults to True.
+            inst_norm_affine (bool, optional): Whether to include an affine transform in the instance normalization layer.
+                                               Defaults to True.
+            lrelu_inplace (bool, optional): Whether to use the input tensor for the output of the leaky ReLU activation function.
+                                            Defaults to True.
         """
 
         nn.Module.__init__(self)
@@ -51,10 +44,14 @@ class IncDownsamplingModule(nn.Module):
         self.inst_norm = InstanceNorm(
             output_channels, affine=inst_norm_affine, track_running_stats=True
         )
+
+        # Instantiate 1x1 convolution layer
+        # Dev Note: This currently makes little sense if kernel size is 1
+        # and stride is 2 as information is lost with this
         self.down = Conv(
             input_channels,
             output_channels,
-            kernel_size=kernel_size,
+            kernel_size=1,
             stride=2,
             padding=0,
             bias=conv_bias,
