408f0bc5e646e5e24209f85b008fe67bca2f26b9
Sylwia Malec Sylwia.Malec@Pennmedicine.upenn.edu <sylwiaa.malec@gmail.com>
2023-04-05 19:03:15 -0400

    Improving determinism functionnality

--- GANDLF/utils/set_determinism.py
+++ GANDLF/utils/set_determinism.py
@@ -2,12 +2,15 @@ import os, torch, random
 import numpy as np
 
 def set_determinism(seed=42):
-    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
     torch.use_deterministic_algorithms(True, warn_only = True)
+
+    if torch.cuda.is_available():
+        os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        
--- testing/test_full.py
+++ testing/test_full.py
@@ -2933,3 +2933,4 @@ def test_random_numbers_are_deterministic_on_cpu():
     assert f == h
 
     print("passed")
+    
