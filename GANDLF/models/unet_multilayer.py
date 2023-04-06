f9e55cd470ed4a7b72232f038c7a1fcf8ae15920
Geeks-sid <sid.cre8er@gmail.com>
2023-04-06 11:04:52 -0400

    updated unet_multilyaer

--- GANDLF/models/unet_multilayer.py
+++ GANDLF/models/unet_multilayer.py
@@ -121,13 +121,11 @@ class unet_multilayer(ModelBase):
         """
         Forward pass of the UNet model.
 
-        Parameters
-        ----------
-        x (Tensor): Should be a 5D Tensor as [batch_size, channels, x_dims, y_dims, z_dims].
+        Args:
+            x (Tensor): Should be a 5D Tensor as [batch_size, channels, x_dims, y_dims, z_dims].
 
-        Returns
-        -------
-        x (Tensor): Returns a 5D Output Tensor as [batch_size, n_classes, x_dims, y_dims, z_dims].
+        Returns:
+            x (Tensor): Returns a 5D Output Tensor as [batch_size, n_classes, x_dims, y_dims, z_dims].
 
         """
         # Store intermediate feature maps
