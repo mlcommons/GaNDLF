2542e6ddeb44ec6806b97ed940f25d408fd711bb
Siddhesh Thakur <sid.cre8er@gmail.com>
2023-04-24 15:09:27 -0400

    Update __init__.py with documentation for get_optimizer

--- GANDLF/optimizers/__init__.py
+++ GANDLF/optimizers/__init__.py
@@ -27,12 +27,21 @@ global_optimizer_dict = {
 
 def get_optimizer(params):
     """
-    Function to get the optimizer definition.
+    Returns an instance of the specified optimizer from the PyTorch `torch.optim` module.
 
     Args:
-        params (dict): The parameters' dictionary.
+        params (dict): A dictionary containing the input parameters for the optimizer.
 
     Returns:
-        model (Optimizer): The optimizer definition.
+        optimizer (torch.optim.Optimizer): An instance of the specified optimizer.
+
     """
-    return global_optimizer_dict[params["optimizer"]["type"]](params)
+    # Retrieve the optimizer type from the input parameters
+    optimizer_type = params["optimizer"]["type"]
+    
+    # Create the optimizer instance using the specified type and input parameters
+    if optimizer_type in global_optimizer_dict:
+        optimizer_function = global_optimizer_dict[optimizer_type]
+        return optimizer_function(params)
+    else:
+        raise ValueError("Optimizer type %s not found" % optimizer_type)
