afbb584a1c92b48fa3de984050be0a27c86a3c2d
Craig Peters <craiglpeters@github.com>
2023-04-26 21:35:06 +0000

    separate GPU install and welcome

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,10 +3,10 @@
 # if there is a gpu present in the container, install the gpu version of pytorch
 # we will check for the presence of the nvidia-smi command as that signifies the presence of a gpu
 
-if command -v nvidia-smi &>/dev/null; then
+# if runnning on a GPU machine, install the GPU version of pytorch
+if command -v nvidia-smi &> /dev/null
+then
 	pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
-else
-	pip install torch==1.13.1+cpu torchvision==0.14.1+cpu torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cpu
 fi
 
 pip install -e .
