932fb84206c2f91a57317585afd2c2e8d67787e9
Cory Gwin <gwincr11@github.com>
2023-04-26 17:37:19 +0000

    gzip csv files and decompress when opening a new codespace

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,8 +3,7 @@
 # if there is a gpu present in the container, install the gpu version of pytorch
 # we will check for the presence of the nvidia-smi command as that signifies the presence of a gpu
 
-if command -v nvidia-smi &> /dev/null
-then
+if command -v nvidia-smi &>/dev/null; then
 	pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
 else
 	pip install torch==1.13.1+cpu torchvision==0.14.1+cpu torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cpu
@@ -12,3 +11,4 @@ fi
 
 pip install -e .
 python ./gandlf_verifyInstall
+gzip -dk -r tutorial/medmnist/dataset
