4586a0ad4ee749e698ef6e43abe902b7973ae300
Sarthak Pati <sarthak.pati@pennmedicine.upenn.edu>
2023-04-28 13:45:50 -0400

    Update GANDLF/optimizers/wrap_torch.py

--- GANDLF/optimizers/wrap_torch.py
+++ GANDLF/optimizers/wrap_torch.py
@@ -82,7 +82,7 @@ def adam(parameters, opt_type="normal"):
     return optimizer_fn(
         parameters["model_parameters"],
         lr=parameters.get("learning_rate"),
-        betas=optimizer_params.get("betas", (0.9, 0.999)),
+        betas=parameters["optimizer"].get("betas", (0.9, 0.999)),
         weight_decay=parameters["optimizer"].get("weight_decay", 0.00005),
         eps=parameters["optimizer"].get("eps", 1e-8),
         amsgrad=parameters["optimizer"].get("amsgrad", False),
