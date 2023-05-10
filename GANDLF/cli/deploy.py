e1bfee2894d9d291b5c7a01289edb9739c69c1ba
alexandergetka <GetkaA@pennmedicine.upenn.edu>
2023-05-09 21:07:13 -0400

    Update deploy.py

--- GANDLF/cli/deploy.py
+++ GANDLF/cli/deploy.py
@@ -168,6 +168,8 @@ def deploy_docker_mlcube(modeldir, config, outputdir, mlcubedir, requires_gpu):
         if not os.path.exists(os.path.join(gandlf_root, "GANDLF", "GANDLF")):
             os.symlink(os.path.join(gandlf_root, "GANDLF"), os.path.join(gandlf_root, "GANDLF", "GANDLF"))
         gandlf_root = os.path.join(gandlf_root, "GANDLF")
+        
+    print(os.listdir(gandlf_root))
             
     # Requires mlcube_docker python package to be installed with scripts available
     # command_to_run = "mlcube_docker configure --platform=docker  -Pdocker.build_strategy=always" + " --mlcube=" + os.path.realpath(mlcubedir) + " -Pdocker.build_context=" + gandlf_root
