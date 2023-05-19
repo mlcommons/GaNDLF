1141a4b41b6cca44ba227fe789e06f71ed26de35
Sarthak Pati <sarthak.pati@pennmedicine.upenn.edu>
2023-05-19 15:24:20 -0400

    Update GANDLF/optimizers/wrap_torch.py

--- GANDLF/optimizers/wrap_torch.py
+++ GANDLF/optimizers/wrap_torch.py
@@ -236,7 +236,7 @@ def radam(parameters):
     """
     # Create the optimizer using the input parameters
     return RAdam(
-        parameters["model_parameters"], 
+        parameters["model_parameters"],
         lr=parameters.get("learning_rate"),
         betas=parameters["optimizer"].get("betas", (0.9, 0.999)),
         eps=parameters["optimizer"].get("eps", 1e-8),
