e50faf5e79ddd5e0a3175ba74c290524ce59e0d7
sarthakpati <sarthak.pati@hotmail.com>
2023-05-25 14:26:20 -0400

    added test

--- testing/test_full.py
+++ testing/test_full.py
@@ -30,6 +30,7 @@ from GANDLF.cli import (
     run_deployment,
     recover_config,
     post_training_model_optimization,
+    generate_metrics_dict,
 )
 from GANDLF.schedulers import global_schedulers_dict
 from GANDLF.optimizers import global_optimizer_dict
@@ -2965,3 +2966,44 @@ def test_generic_random_numbers_are_deterministic_on_cpu():
     assert f == h
 
     print("passed")
+
+
+def test_metrics_cli_rad_2d():
+    print("49: Starting metric calculation tests")
+    for problem_type in ["segmentation", "classification"]:
+        # read and parse csv
+        training_data, _ = parseTrainingCSV(
+            inputDir + f"/train_2d_rad_{problem_type}.csv"
+        )
+        if problem_type == "segmentation":
+            labels_array = training_data["Label"]
+        else:
+            labels_array = training_data["ValueToPredict"]        
+        training_data["target"] = labels_array
+        training_data["prediction"] = labels_array
+
+        # read and initialize parameters for specific data dimension
+        parameters = parseConfig(
+            testingDir + f"/config_{problem_type}.yaml", version_check_flag=False
+        )
+        parameters["modality"] = "rad"
+        parameters["patch_size"] = patch_size["2D"]
+        parameters["model"]["dimension"] = 2
+        parameters["verbose"] = False
+
+        temp_infer_csv = os.path.join(outputDir, "temp_csv.csv")
+        training_data.to_csv(temp_infer_csv, index=False)
+
+        output_file = os.path.join(outputDir, "output.yaml")
+        training_data.to_csv(temp_infer_csv, index=False)
+
+        temp_config = get_temp_config_path()
+        with open(temp_config, "w") as file:
+            yaml.dump(parameters, file)
+        
+        # run the metrics calculation
+        generate_metrics_dict(temp_infer_csv, temp_config, output_file)
+
+        assert os.path.isfile(output_file), "Metrics output file was not generated"
+
+        sanitize_outputDir()
