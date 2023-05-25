068ba1e90a7296162d33debea582324c3c9c45df
sarthakpati <sarthak.pati@hotmail.com>
2023-05-25 14:26:13 -0400

    better initialization of variables

--- GANDLF/cli/generate_metrics.py
+++ GANDLF/cli/generate_metrics.py
@@ -6,16 +6,13 @@ import torch
 
 from GANDLF.parseConfig import parseConfig
 from GANDLF.utils import find_problem_type_from_parameters, one_hot
-from GANDLF.metrics import overall_stats, multi_class_dice
+from GANDLF.metrics import overall_stats
+from GANDLF.losses.segmentation import dice
 from GANDLF.metrics.segmentation import (
     _calculator_generic_all_surface_distances,
     _calculator_sensitivity_specificity,
     _calculator_jaccard,
 )
-
-# input: 1 csv with 3 columns: subjectid, prediction, target
-# input: gandlf config
-# output: metrics.csv (based on the config)
 
 
 def generate_metrics_dict(input_csv: str, config: str, outputfile: str = None) -> dict:
@@ -39,10 +36,11 @@ def generate_metrics_dict(input_csv: str, config: str, outputfile: str = None) -
         col_lower = col.lower()
         for column_to_check in required_columns:
             if column_to_check == col_lower:
-                headers[column_to_check] = column_to_check
+                headers[column_to_check] = col
     for column in required_columns:
         assert column in headers, f"The input csv should have a column named {column}"
 
+    overall_stats_dict = {}
     parameters = parseConfig(config)
     problem_type = find_problem_type_from_parameters(parameters)
     parameters["problem_type"] = problem_type
@@ -54,56 +52,71 @@ def generate_metrics_dict(input_csv: str, config: str, outputfile: str = None) -
         # read images and then calculate metrics
         class_list = parameters["model"]["class_list"]
         for _, row in input_df.iterrows():
-            current_subject_id = row[headers["subject_id"]]
+            current_subject_id = row[headers["subjectid"]]
             overall_stats_dict[current_subject_id] = {}
             label_image = sitk.ReadImage(row["target"])
             pred_image = sitk.ReadImage(row["prediction"])
-            label_tensor = torch.from_numpy(sitk.GetArrayFromImage(label_image))
-            pred_tensor = torch.from_numpy(sitk.GetArrayFromImage(pred_image))
+            label_array = sitk.GetArrayFromImage(label_image)
+            pred_array = sitk.GetArrayFromImage(pred_image)
+            label_tensor = torch.from_numpy(label_array).reshape(
+                1, 1, *label_array.shape
+            )
+            pred_tensor = torch.from_numpy(pred_array).reshape(1, 1, *label_array.shape)
+
+            if len(label_tensor.shape) == 6:
+                label_tensor = label_tensor.squeeze(0)
+                pred_tensor = pred_tensor.squeeze(0)
 
             # one hot encode with batch_size = 1
             label_image_one_hot = one_hot(label_tensor, class_list).unsqueeze(0)
             pred_image_one_hot = one_hot(pred_tensor, class_list).unsqueeze(0)
 
             for class_index, _ in enumerate(class_list):
+                overall_stats_dict[current_subject_id][str(class_index)] = {}
                 # this is inconsequential, since one_hot will ensure that the classes are present
                 parameters["model"]["class_list"] = [0, 1]
+                parameters["model"]["num_classes"] = 2
                 parameters["model"]["ignore_label_validation"] = 0
-                overall_stats_dict[current_subject_id][
-                    "dice_" + str(class_index)
-                ] = multi_class_dice(
+                overall_stats_dict[current_subject_id][str(class_index)]["dice"] = dice(
                     pred_image_one_hot,
                     label_image_one_hot,
-                    parameters,
                 ).item()
-                overall_stats_dict[current_subject_id]["nsd_" + str(class_index)],
-                overall_stats_dict[current_subject_id]["hd100_" + str(class_index)],
-                overall_stats_dict[current_subject_id][
-                    "hd95_" + str(class_index)
-                ] = _calculator_generic_all_surface_distances(
+                nsd, hd100, hd95 = _calculator_generic_all_surface_distances(
                     pred_image_one_hot,
                     label_image_one_hot,
                     parameters,
                 )
+                overall_stats_dict[current_subject_id][str(class_index)][
+                    "nsd"
+                ] = nsd.item()
+                overall_stats_dict[current_subject_id][str(class_index)][
+                    "hd100"
+                ] = hd100.item()
+                overall_stats_dict[current_subject_id][str(class_index)][
+                    "hd95"
+                ] = hd95.item()
+
                 (
-                    overall_stats_dict[current_subject_id][
-                        "sensitivity_" + str(class_index)
-                    ],
-                    overall_stats_dict[current_subject_id][
-                        "specificity_" + str(class_index)
-                    ],
+                    s,
+                    p,
                 ) = _calculator_sensitivity_specificity(
                     pred_image_one_hot,
                     label_image_one_hot,
                     parameters,
                 )
+                overall_stats_dict[current_subject_id][str(class_index)][
+                    "sensitivity"
+                ] = s.item()
+                overall_stats_dict[current_subject_id][str(class_index)][
+                    "specificity"
+                ] = p.item()
                 overall_stats_dict[current_subject_id][
                     "jaccard_" + str(class_index)
                 ] = _calculator_jaccard(
                     pred_image_one_hot,
                     label_image_one_hot,
                     parameters,
-                )
+                ).item()
 
     pprint(overall_stats_dict)
     if outputfile is not None:
