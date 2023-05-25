173965400502b8e010192c10ade51633b0c1459c
sarthakpati <sarthak.pati@hotmail.com>
2023-05-25 15:01:10 -0400

    added fix for labels

--- GANDLF/cli/generate_metrics.py
+++ GANDLF/cli/generate_metrics.py
@@ -3,6 +3,7 @@ from pprint import pprint
 import pandas as pd
 import SimpleITK as sitk
 import torch
+import torchio
 
 from GANDLF.parseConfig import parseConfig
 from GANDLF.utils import find_problem_type_from_parameters, one_hot
@@ -44,32 +45,37 @@ def generate_metrics_dict(input_csv: str, config: str, outputfile: str = None) -
     parameters = parseConfig(config)
     problem_type = find_problem_type_from_parameters(parameters)
     parameters["problem_type"] = problem_type
+
     if problem_type == "regression" or problem_type == "classification":
-        predictions_array = input_df[headers["prediction"]].to_numpy().ravel()
-        labels_array = input_df[headers["target"]].to_numpy().ravel()
-        overall_stats_dict = overall_stats(predictions_array, labels_array, parameters)
+        parameters["model"]["num_classes"] = len(parameters["model"]["class_list"])
+        predictions_tensor = torch.from_numpy(
+            input_df[headers["prediction"]].to_numpy().ravel()
+        )
+        labels_tensor = torch.from_numpy(input_df[headers["target"]].to_numpy().ravel())
+        overall_stats_dict = overall_stats(
+            predictions_tensor, labels_tensor, parameters
+        )
     elif problem_type == "segmentation":
         # read images and then calculate metrics
         class_list = parameters["model"]["class_list"]
         for _, row in input_df.iterrows():
             current_subject_id = row[headers["subjectid"]]
             overall_stats_dict[current_subject_id] = {}
-            label_image = sitk.ReadImage(row["target"])
-            pred_image = sitk.ReadImage(row["prediction"])
-            label_array = sitk.GetArrayFromImage(label_image)
-            pred_array = sitk.GetArrayFromImage(pred_image)
-            label_tensor = torch.from_numpy(label_array).reshape(
-                1, 1, *label_array.shape
-            )
-            pred_tensor = torch.from_numpy(pred_array).reshape(1, 1, *label_array.shape)
-
-            if len(label_tensor.shape) == 6:
-                label_tensor = label_tensor.squeeze(0)
-                pred_tensor = pred_tensor.squeeze(0)
+            label_image = torchio.LabelMap(row["target"])
+            pred_image = torchio.LabelMap(row["prediction"])
+            label_tensor = label_image.data
+            pred_tensor = pred_image.data
+            spacing = label_image.spacing
+            if label_tensor.data.shape[-1] == 1:
+                spacing = spacing[0:2]
+            # add dimension for batch
+            parameters["subject_spacing"] = torch.Tensor(spacing).unsqueeze(0)
+            label_tensor = label_tensor.unsqueeze(0)
+            pred_tensor = pred_tensor.unsqueeze(0)
 
             # one hot encode with batch_size = 1
-            label_image_one_hot = one_hot(label_tensor, class_list).unsqueeze(0)
-            pred_image_one_hot = one_hot(pred_tensor, class_list).unsqueeze(0)
+            label_image_one_hot = one_hot(label_tensor, class_list)
+            pred_image_one_hot = one_hot(pred_tensor, class_list)
 
             for class_index, _ in enumerate(class_list):
                 overall_stats_dict[current_subject_id][str(class_index)] = {}
