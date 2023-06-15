011aaa8356dacc6dc5ab36aa9e55453ba18b7d71
sarthakpati <sarthak.pati@hotmail.com>
2023-06-15 16:23:25 -0400

    using main_run to test new functionality

--- testing/test_full.py
+++ testing/test_full.py
@@ -1478,20 +1478,19 @@ def test_generic_cli_function_mainrun(device):
         resume=False,
         reset=True,
     )
-    sanitize_outputDir()
 
     with open(file_config_temp, "w") as file:
         yaml.dump(parameters, file)
 
-    # testing train/valid/test split
+    # testing train/valid/test split with resume
     main_run(
         file_data + "," + file_data + "," + file_data,
         file_config_temp,
         outputDir,
         True,
         device,
-        resume=False,
-        reset=True,
+        resume=True,
+        reset=False,
     )
     sanitize_outputDir()
 
