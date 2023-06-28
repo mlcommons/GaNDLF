cd224bdc95584e36db0c253023eed0f695f9abe2
hasan7n <hasankassim7@hotmail.com>
2023-06-28 13:58:22 +0200

    write example custom entrypoint

---
+++ mlcube/metrics_mlcube/example_custom_entrypoint/getting_started_3d_rad_seg.py
@@ -0,0 +1,65 @@
+import os
+import argparse
+import sys
+import pandas as pd
+
+
+def create_csv(predictions, labels):
+    """This function expects `predictions` to be structured in a certain way:
+    it should contain the following paths: `testing/<subID>/<subID>_seg.nii.gz`
+    `labels` is expected to have a `data.csv` file containing relative paths of
+    labels"""
+
+    # read and parse expected labels dict
+    labels_data_csv = os.path.join(labels, "data.csv")
+    labels_df = pd.read_csv(labels_data_csv)
+    for col in labels_df:
+        if col.lower() == "subjectid":
+            subjectid_header = col
+        if col.lower() == "label":
+            label_header = col
+    labels_records = labels_df.to_dict("records")
+    labels_dict = {
+        int(rec[subjectid_header]): rec[label_header] for rec in labels_records
+    }
+
+    # generate data input dict
+    input_data = []
+    for subjectID in os.listdir(os.path.join(predictions, "testing")):
+        pred_path = os.path.join(
+            predictions, "testing", subjectID, f"{subjectID}_seg.nii.gz"
+        )
+        pred_path = os.path.abspath(pred_path)
+        label_path = labels_dict[int(subjectID)]
+        label_path = os.path.join(labels, label_path)
+        label_path = os.path.abspath(label_path)
+        prediction_record = {
+            "SubjectID": subjectID,
+            "Prediction": pred_path,
+            "Target": label_path,
+        }
+        input_data.append(prediction_record)
+
+    input_data_df = pd.DataFrame(input_data)
+    input_data_df.to_csv("./data.csv")
+
+
+def run_gandlf(output_path, parameters_file):
+    exit_status = os.system(
+        f"python3.8 gandlf_generateMetrics -c {parameters_file} -i ./data.csv -o {output_path}"
+    )
+    exit_code = os.WEXITSTATUS(exit_status)
+    sys.exit(exit_code)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--parameters_file", metavar="", type=str, required=True)
+    parser.add_argument("--predictions", metavar="", type=str, required=True)
+    parser.add_argument("--output_path", metavar="", type=str, default=None)
+    parser.add_argument("--labels", metavar="", type=str, required=True)
+
+    args = parser.parse_args()
+
+    create_csv(args.predictions, args.labels)
+    run_gandlf(args.output_path, args.parameters_file)
---
+++ mlcube/metrics_mlcube/example_custom_entrypoint/template.py
@@ -0,0 +1,35 @@
+"""If the predictions and labels inputs to the MLCube should come from different
+mount points and can't be referred to in a single csv, a custom entrypoint is
+needed to create a temporary csv file before calling GaNDLF's generate metrics command.
+This script should expect the same arguments passed to the command `mlcube run --task evaluate`,
+i.e. it should expect the inputs and outputs defined in `mlcube.yaml` in the `evaluate` task"""
+
+import os
+import argparse
+import sys
+
+
+def create_csv(predictions, labels):
+    """A function that creates a ./data.csv file from input folders."""
+
+
+def run_gandlf(output_path, parameters_file):
+    """A function that calls GaNDLF's generate metrics command with the previously created csv"""
+    exit_status = os.system(
+        f"python3.8 gandlf_generateMetrics -c {parameters_file} -i ./data.csv -o {output_path}"
+    )
+    exit_code = os.WEXITSTATUS(exit_status)
+    sys.exit(exit_code)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--parameters_file", metavar="", type=str, required=True)
+    parser.add_argument("--predictions", metavar="", type=str, required=True)
+    parser.add_argument("--output_path", metavar="", type=str, default=None)
+    parser.add_argument("--labels", metavar="", type=str, required=True)
+
+    args = parser.parse_args()
+
+    create_csv(args.predictions, args.labels)
+    run_gandlf(args.output_path, args.parameters_file)
