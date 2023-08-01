d8b1f8d33c6be488d3a70eea1fe572d04dace53a
FelixSteinbauer <43598998+FelixSteinbauer@users.noreply.github.com>
2023-08-01 21:16:33 +0200

    Returned to current state of code
    I do not know why the pipeline fails To me it seems unrelated to the changes that were made since the last successful run.

--- GANDLF/metrics/synthesis.py
+++ GANDLF/metrics/synthesis.py
@@ -62,14 +62,13 @@ def peak_signal_noise_ratio(target, prediction, data_range=None, epsilon=None) -
     if epsilon == None:
         psnr = PeakSignalNoiseRatio(data_range=data_range)
         return psnr(preds=prediction, target=target)
-    else:
+    else: # implementation of PSNR that does not give 'inf'/'nan' when 'mse==0'
         mse = mean_squared_error(target, prediction)
         if data_range == None: #compute data_range like torchmetrics if not given
             min_v = 0 if torch.min(target) > 0 else torch.min(target) #look at this line
             max_v = torch.max(target)
             data_range = max_v - min_v
-        return 10.0 * (( torch.log10(data_range) ** 2) / (mse + epsilon) )
-        #return 10.0 * torch.log10((data_range ** 2) / (mse + epsilon))
+        return 10.0 * torch.log10((data_range ** 2) / (mse + epsilon))
 
 
 def mean_squared_log_error(target, prediction) -> torch.Tensor:
