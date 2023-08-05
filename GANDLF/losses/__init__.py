2649ce1e854c181df2cf849e357bc58417402c53
sarthakpati <sarthak.pati@hotmail.com>
2023-08-05 13:02:45 -0400

    updated global dict

--- GANDLF/losses/__init__.py
+++ GANDLF/losses/__init__.py
@@ -7,6 +7,8 @@ from .segmentation import (
     MCT_loss,
     KullbackLeiblerDivergence,
     FocalLoss,
+    MCC_loss,
+    MCC_log_loss,
 )
 from .regression import CE, CEL, MSE_loss, L1_loss
 from .hybrid import DCCE, DCCE_Logits, DC_Focal
@@ -17,7 +19,14 @@ global_losses_dict = {
     "dc": MCD_loss,
     "dice": MCD_loss,
     "dc_log": MCD_log_loss,
+    "dclog": MCD_log_loss,
     "dice_log": MCD_log_loss,
+    "dicelog": MCD_log_loss,
+    "mcc": MCC_loss,
+    "mcc_log": MCC_log_loss,
+    "mcclog": MCC_log_loss,
+    "mathews": MCC_loss,
+    "mathews_log": MCC_log_loss,
     "dcce": DCCE,
     "dcce_logits": DCCE_Logits,
     "ce": CE,
