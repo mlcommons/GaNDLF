023185580c90c935086f699c09a50693b51efdf9
rachitsaluja <116708758+rachitsaluja@users.noreply.github.com>
2023-08-16 20:21:45 +0530

    Update __init__.py

--- GANDLF/cli/__init__.py
+++ GANDLF/cli/__init__.py
@@ -6,6 +6,7 @@ from .deploy import deploy_targets, mlcube_types, run_deployment
 from .recover_config import recover_config
 from .post_training_model_optimization import post_training_model_optimization
 from .generate_metrics import generate_metrics_dict
+from .generate_BraTS_metrics import generate_metrics_dict
 
 from datetime import date
 
