6777b08b75025e45b1d36f3d6c114b8f28bb696b
Szymon <szymonmazurek57@gmail.com>
2023-11-03 14:03:47 +0100

    Testing git CI/CD

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
         version=__version__,
         author="MLCommons",
         author_email="gandlf@mlcommons.org",
-        python_requires=">=3.8",
+        python_requires="==3.10",
         packages=find_packages(
             where=os.path.dirname(os.path.abspath(__file__)),
             exclude=toplevel_package_excludes,
