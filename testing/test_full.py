a4fddf71b1eff159934af1c7d96d0fd70973d513
Ainesh Nanda <aineshnanda@gmail.com>
2023-11-20 21:55:40 +0530

    test commit

--- testing/test_full.py
+++ testing/test_full.py
@@ -2259,7 +2259,7 @@ def test_train_inference_classification_histology_large_2d(device):
         ]:
             _ = get_patch_size_in_microns(row["Channel_0"], patch_size)
 
-        # try to break resizer
+        # try to break resizer - test
         new_filename = resize_for_ci(row["Channel_0"], scale=10)
         row["Channel_0"] = new_filename
         files_to_delete.append(new_filename)
