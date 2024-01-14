dbd89e130c3efa5ccb45bb25c3a8455564d7fc49
Szymon Mazurek <szymonmazurek57@gmail.com>
2024-01-14 11:00:54 +0100

    Hotfix
    - added proper parameter creation for gan models
    - modified the function to create optimizers so now it properly uses parameters for generator and discriminator

--- GANDLF/compute/generic.py
+++ GANDLF/compute/generic.py
@@ -179,7 +179,8 @@ def create_pytorch_objects_gan(
 
     # get the model
     model = get_model(parameters)
-    parameters["model_parameters"] = model.parameters()
+    parameters["model_parameters_gen"] = model.generator.parameters()
+    parameters["model_parameters_disc"] = model.discriminator.parameters()
     optimizer_gen, optimizer_disc = get_optimizers_gan(parameters)
     parameters["optimizer_gen_object"] = optimizer_gen
     parameters["optimizer_disc_object"] = optimizer_disc
@@ -222,6 +223,8 @@ def create_pytorch_objects_gan(
     generator_keys_to_remove = [
         "optimizer_gen_object",
         "optimizer_disc_object",
+        "model_parameters_gen",
+        "model_parameters_disc",
         "model_parameters",
     ]
     for key in generator_keys_to_remove:
