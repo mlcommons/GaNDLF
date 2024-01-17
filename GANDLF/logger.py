d7a5fad03cd60ce638f28137b176aa315193531d
Szymon Mazurek <szymonmazurek57@gmail.com>
2024-01-17 10:51:18 +0100

    New logger and refactoring old one
    - added new logger for GAN type networks
    - refactored the old logger class -  both logger are now typed and properly docstringed
    - added new class method to elegantly parse the numeric values into strings

--- GANDLF/logger.py
+++ GANDLF/logger.py
@@ -8,34 +8,42 @@ Created on Thu Mar 11 14:55:44 2021
 
 import os
 import torch
+from typing import List, Union, Dict
+from pathlib import Path
 
 
 class Logger:
-    def __init__(self, logger_csv_filename, metrics):
+    """
+    This class is used to log the metrics to a csv file.
+    """
+
+    def __init__(
+        self, logger_csv_filename: Union[str, Path], metrics: Dict[str, str]
+    ):
         """
-
-        Parameters
-        ----------
-        logger_csv_filename : String
-            Path to a filename where the csv has to be stored
-        metric : list
-            Should be a list of the metrics
-
-        Returns
-        -------
-        None.
-
+        Create a logger object.
+        Parameters:
+            logger_csv_filename (str or Path): The filename of the csv file.
+            metrics (dict): The metrics that will be logged during the experiment.
         """
         self.filename = logger_csv_filename
         self.metrics = metrics
 
-    def write_header(self, mode="train"):
+    def write_header(self, mode: str = "train"):
+        """Create a header for the csv file.
+        Parameters:
+            mode (str): "train" or "valid" or "test"
+
+        """
         self.csv = open(self.filename, "a")
         if os.stat(self.filename).st_size == 0:
             mode_lower = mode.lower()
             row = "epoch_no," + mode_lower + "_loss,"
             row += (
-                ",".join([mode_lower + "_" + metric for metric in self.metrics]) + ","
+                ",".join(
+                    [mode_lower + "_" + metric for metric in self.metrics]
+                )
+                + ","
             )
             row = row[:-1]
             row += "\n"
@@ -44,38 +52,39 @@ class Logger:
         #     print("Found a pre-existing file for logging, now appending logs to that file!")
         self.csv.close()
 
-    def write(self, epoch_number, loss, epoch_metrics):
+    @staticmethod
+    def _parse_input_value(numeric_input: Union[float, torch.Tensor]) -> str:
+        """Parse the input value to a string
+        Parameters:
+            numeric_input (float or torch.Tensor): The input value
+        Returns:
+            str: The parsed input value
         """
+        if torch.is_tensor(numeric_input):
+            return str(numeric_input.cpu().item())
+        else:
+            return str(numeric_input)
 
-        Parameters
-        ----------
-        epoch_number : TYPE
-            DESCRIPTION.
-        loss : TYPE
-            DESCRIPTION.
-        metrics : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
+    def write(
+        self,
+        epoch_number: int,
+        loss: Union[float, torch.Tensor],
+        epoch_metrics: Dict[str, Union[float, torch.Tensor]],
+    ):
+        """
+        Write the metrics to the csv file.
+        Parameters:
+            epoch_number (int): The epoch number
+            loss (float or torch.Tensor): The loss
+            epoch_metrics (dict): The epoch metrics
 
         """
         self.csv = open(self.filename, "a")
         row = ""
-        row += str(epoch_number) + ","
-        if torch.is_tensor(loss):
-            row += str(loss.cpu().item())
-        else:
-            row += str(loss)
-        row += ","
-
+        row += self._parse_input_value(epoch_number) + ","
+        row += self._parse_input_value(loss) + ","
         for metric in epoch_metrics:
-            if torch.is_tensor(epoch_metrics[metric]):
-                row += str(epoch_metrics[metric].cpu().item())
-            else:
-                row += str(epoch_metrics[metric])
-            row += ","
+            row += self._parse_input_value(epoch_metrics[metric]) + ","
         row = row[:-1]
         self.csv.write(row)
         self.csv.write("\n")
@@ -83,3 +92,69 @@ class Logger:
 
     def close(self):
         self.csv.close()
+
+
+class LoggerGAN:
+    def __init__(self, logger_csv_filename, metrics):
+        self.filename = logger_csv_filename
+        self.metrics = metrics
+
+    def write_header(self, mode: str = "train") -> None:
+        """Create a header for the csv file when training GAN networks
+        Parameters:
+            mode (str): "train" or "valid" or "test"
+        """
+        with open(self.filename, "a") as log_file:
+            if os.stat(self.filename).st_size == 0:
+                mode_lower = mode.lower()
+                row = "epoch_no," + mode_lower + "_disc_loss,"
+                row += mode_lower + "_gen_loss,"
+                row += (
+                    ",".join(
+                        [mode_lower + "_" + metric for metric in self.metrics]
+                    )
+                    + ","
+                )
+                row = row[:-1]
+                row += "\n"
+                log_file.write(row)
+
+    @staticmethod
+    def _parse_input_value(numeric_input: Union[float, torch.Tensor]) -> str:
+        """Parse the input value to a string
+        Parameters:
+            numeric_input (float or torch.Tensor): The input value
+        Returns:
+            str: The parsed input value
+        """
+        if torch.is_tensor(numeric_input):
+            return str(numeric_input.cpu().item())
+        else:
+            return str(numeric_input)
+
+    def write(
+        self,
+        epoch_number: int,
+        disc_loss: Union[float, torch.Tensor],
+        gen_loss: Union[float, torch.Tensor],
+        epoch_metrics: Dict[str, Union[float, torch.Tensor]],
+    ) -> None:
+        """
+        Write the metrics to the csv file.
+        Parameters:
+            epoch_number (int): The epoch number
+            disc_loss (float or torch.Tensor): The discriminator loss
+            gen_loss (float or torch.Tensor): The generator loss
+            epoch_metrics (dict): The epoch metrics
+        """
+        with open(self.filename, "a") as log_file:
+            row = ""
+            row += self._parse_input_value(epoch_number) + ","
+            row += self._parse_input_value(disc_loss) + ","
+            row += self._parse_input_value(gen_loss) + ","
+
+            for metric in epoch_metrics:
+                row += self._parse_input_value(epoch_metrics[metric]) + ","
+            row = row[:-1]  # ??
+            log_file.write(row)
+            log_file.write("\n")
