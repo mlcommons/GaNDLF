741365a33197ef52f4966208784a08481b9c38fe
Ainesh Nanda <aineshnanda@gmail.com>
2024-01-22 21:37:37 +0530

    wrote function for Novograd optimizer

--- GANDLF/optimizers/wrap_monai.py
+++ GANDLF/optimizers/wrap_monai.py
@@ -1,7 +1,19 @@
 from monai.optimizers import (
-    novograd
+    novograd,
+    WarmupCosineSchedule as WCS
 )
 
 def novograd(parameters):
-    print("currently here")
-#test push to check git push
+    return novograd(
+        parameters["model_parameters"],
+        lr=parameters.get("learning_rate"),
+        betas=parameters["optimizer"].get("betas", (0.9, 0.999)),
+        eps=parameters["optimizer"].get("eps", 1e-8),
+        weight_decay=parameters["optimizer"].get("weight_decay", 3e-05),
+        amsgrad=parameters["optimizer"].get("amsgrad", False),
+    )
+
+def WCS(parameters):
+    return WCS(
+        
+    )
