59841ad5442944acc48020ea5a2de8939b53d9f4
Geeks-sid <sid.cre8er@gmail.com>
2024-02-08 12:05:56 -0500

    Add losses and metrics to respective modules

---
+++ GANDLF/metrics/metrics_dict.py
@@ -0,0 +1,131 @@
+"""
+All the metrics are to be called from here
+"""
+import GANDLF.metrics.classification as classification
+import GANDLF.metrics.regression as regression
+from GANDLF.losses.regression import CEL, MSE_loss
+
+from .generic import (
+    accuracy,
+    f1_score,
+    iou_score,
+    precision_score,
+    recall_score,
+    specificity_score,
+)
+from .regression import balanced_acc_score, classification_accuracy, per_label_accuracy
+from .segmentation import (
+    hd95,
+    hd95_per_label,
+    hd100,
+    hd100_per_label,
+    jaccard,
+    jaccard_per_label,
+    multi_class_dice,
+    multi_class_dice_per_label,
+    nsd,
+    nsd_per_label,
+    sensitivity,
+    sensitivity_per_label,
+    specificity_segmentation,
+    specificity_segmentation_per_label,
+)
+from .synthesis import (
+    mean_absolute_error,
+    mean_squared_error,
+    mean_squared_log_error,
+    ncc_max,
+    ncc_mean,
+    ncc_min,
+    ncc_std,
+    peak_signal_noise_ratio,
+    structural_similarity_index,
+)
+
+# global defines for the metrics
+global_metrics_dict = {
+    "dice": multi_class_dice,
+    "dice_per_label": multi_class_dice_per_label,
+    "accuracy": accuracy,
+    "mse": MSE_loss,
+    "hd95": hd95,
+    "hd95_per_label": hd95_per_label,
+    "hausdorff95_per_label": hd95_per_label,
+    "hausdorff95": hd95,
+    "hd100": hd100,
+    "hd100_per_label": hd100_per_label,
+    "hausdorff": hd100,
+    "hausdorff100": hd100,
+    "nsd": nsd,
+    "nsd_per_label": nsd_per_label,
+    "normalized_surface_dice": nsd,
+    "normalized_surface_dice_per_label": nsd_per_label,
+    "normalized_sd": nsd,
+    "normalized_sd_per_label": nsd_per_label,
+    "cel": CEL,
+    "f1_score": f1_score,
+    "f1": f1_score,
+    "classification_accuracy": classification_accuracy,
+    "precision": precision_score,
+    "recall": recall_score,
+    "specificity": specificity_score,
+    "iou": iou_score,
+    "balanced_accuracy": balanced_acc_score,
+    "per_label_one_hot_accuracy": per_label_accuracy,
+    "sensitivity": sensitivity,
+    "sensitivity_per_label": sensitivity_per_label,
+    "specificity_segmentation": specificity_segmentation,
+    "specificity_segmentation_per_label": specificity_segmentation_per_label,
+    "jaccard": jaccard,
+    "jaccard_per_label": jaccard_per_label,
+    "mean_squared_error": mean_squared_error,
+    "mean_absolute_error": mean_absolute_error,
+    "mean_squared_log_error": mean_squared_log_error,
+    "peak_signal_noise_ratio": peak_signal_noise_ratio,
+    "structural_similarity_index": structural_similarity_index,
+    "ncc_max": ncc_max,
+    "ncc_mean": ncc_mean,
+    "ncc_min": ncc_min,
+    "ncc_std": ncc_std,
+}
+
+
+# global define for the metrics that use surface distances, and hence require "connectivity" to be defined
+surface_distance_ids = [
+    "hd95",
+    "hd95_per_label",
+    "hausdorff95_per_label",
+    "hausdorff95",
+    "hd100",
+    "hd100_per_label",
+    "hausdorff",
+    "hausdorff100",
+    "nsd",
+    "nsd_per_label",
+    "normalized_surface_dice",
+    "normalized_surface_dice_per_label",
+    "normalized_sd",
+    "normalized_sd_per_label",
+]
+
+
+def overall_stats(predictions, ground_truth, params):
+    """
+    Generates a dictionary of metrics calculated on the overall predictions and ground truths.
+
+    Args:
+        predictions (torch.Tensor): The output of the model.
+        ground_truth (torch.Tensor): The ground truth labels.
+        params (dict): The parameter dictionary containing training and data information.
+
+    Returns:
+        dict: A dictionary of metrics.
+    """
+    assert len(predictions) == len(
+        ground_truth
+    ), "Predictions and ground truth must be of same length"
+
+    if params["problem_type"] == "classification":
+        return classification.overall_stats(predictions, ground_truth, params)
+    elif params["problem_type"] == "regression":
+        return regression.overall_stats(predictions, ground_truth, params)
