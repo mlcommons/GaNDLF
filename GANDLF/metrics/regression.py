b79aacde76c38e0685d8d2278e2757d8f9e3daca
scap3yvt <149599669+scap3yvt@users.noreply.github.com>
2024-02-14 03:18:43 +0000

    updated regression comments

--- GANDLF/metrics/regression.py
+++ GANDLF/metrics/regression.py
@@ -1,6 +1,7 @@
 """
 All the metrics are to be called from here
 """
+
 import torch
 from sklearn.metrics import balanced_accuracy_score
 import numpy as np
@@ -8,56 +9,62 @@ import torchmetrics as tm
 from ..utils import get_output_from_calculator
 
 
-def classification_accuracy(output, label, params):
+def classification_accuracy(
+    prediction: torch.Tensor, target: torch.Tensor, params: dict
+) -> torch.Tensor:
     """
     This function computes the classification accuracy.
 
     Args:
-        output (torch.Tensor): The output of the model.
-        label (torch.Tensor): The ground truth labels.
+        prediction (torch.Tensor): The prediction of the model.
+        target (torch.Tensor): The ground truth labels.
         params (dict): The parameter dictionary containing training and data information.
 
     Returns:
         torch.Tensor: The classification accuracy.
     """
     if params["problem_type"] == "classification":
-        predicted_classes = torch.argmax(output, 1)
+        predicted_classes = torch.argmax(prediction, 1)
     else:
-        predicted_classes = output
+        predicted_classes = prediction
 
-    acc = torch.sum(predicted_classes == label.squeeze()) / len(label)
+    acc = torch.sum(predicted_classes == target.squeeze()) / len(target)
     return acc
 
 
-def balanced_acc_score(output, label, params):
+def balanced_acc_score(
+    prediction: torch.Tensor, target: torch.Tensor, params: dict
+) -> torch.Tensor:
     """
     This function computes the balanced accuracy.
 
     Args:
-        output (torch.Tensor): The output of the model.
-        label (torch.Tensor): The ground truth labels.
+        prediction (torch.Tensor): The prediction of the model.
+        target (torch.Tensor): The ground truth labels.
         params (dict): The parameter dictionary containing training and data information.
 
     Returns:
         torch.Tensor: The balanced accuracy.
     """
     if params["problem_type"] == "classification":
-        predicted_classes = torch.argmax(output, 1)
+        predicted_classes = torch.argmax(prediction, 1)
     else:
-        predicted_classes = output
+        predicted_classes = prediction
 
     return torch.from_numpy(
-        np.array(balanced_accuracy_score(predicted_classes.cpu(), label.cpu()))
+        np.array(balanced_accuracy_score(predicted_classes.cpu(), target.cpu()))
     )
 
 
-def per_label_accuracy(output, label, params):
+def per_label_accuracy(
+    prediction: torch.Tensor, target: torch.Tensor, params: dict
+) -> torch.Tensor:
     """
     This function computes the per class accuracy.
 
     Args:
-        output (torch.Tensor): The output of the model.
-        label (torch.Tensor): The ground truth labels.
+        prediction (torch.Tensor): The prediction of the model.
+        target (torch.Tensor): The ground truth labels.
         params (dict): The parameter dictionary containing training and data information.
 
     Returns:
@@ -66,31 +73,33 @@ def per_label_accuracy(output, label, params):
     if params["problem_type"] == "classification":
         # ensure this works for multiple batches
         output_accuracy = torch.zeros(len(params["model"]["class_list"]))
-        for output_batch, label_batch in zip(output, label):
+        for output_batch, label_batch in zip(prediction, target):
             predicted_classes = torch.Tensor([0] * len(params["model"]["class_list"]))
             label_cpu = torch.Tensor([0] * len(params["model"]["class_list"]))
             predicted_classes[torch.argmax(output_batch, 0).cpu().item()] = 1
             label_cpu[label_batch.cpu().item()] = 1
             output_accuracy += (predicted_classes == label_cpu).type(torch.float)
-        return output_accuracy / len(output)
+        return output_accuracy / len(prediction)
     else:
-        return balanced_acc_score(output, label, params)
+        return balanced_acc_score(prediction, target, params)
 
 
-def overall_stats(predictions, ground_truth, params):
+def overall_stats(
+    prediction: torch.Tensor, target: torch.Tensor, params: dict
+) -> torch.Tensor:
     """
     Generates a dictionary of metrics calculated on the overall predictions and ground truths.
 
     Args:
-        predictions (torch.Tensor): The output of the model.
-        ground_truth (torch.Tensor): The ground truth labels.
+        predictions (torch.Tensor): The prediction of the model.
+        target (torch.Tensor): The ground truth labels.
         params (dict): The parameter dictionary containing training and data information.
 
     Returns:
         dict: A dictionary of metrics.
     """
     predictions = predictions.type(torch.float)
-    ground_truth = ground_truth.type(torch.float) * params["scaling_factor"]
+    target = target.type(torch.float) * params["scaling_factor"]
     assert (
         params["problem_type"] == "regression"
     ), "Only regression is supported for these stats"
@@ -108,9 +117,9 @@ def overall_stats(predictions, ground_truth, params):
             "cosinesimilarity": tm.CosineSimilarity(reduction=reduction_type_key),
         }
         for metric_name, calculator in calculators.items():
-            output_metrics[
-                f"{metric_name}_{reduction_type}"
-            ] = get_output_from_calculator(predictions, ground_truth, calculator)
+            output_metrics[f"{metric_name}_{reduction_type}"] = (
+                get_output_from_calculator(predictions, target, calculator)
+            )
     # metrics that do not have any "reduction" parameter
     calculators = {
         "mse": tm.MeanSquaredError(),
@@ -120,7 +129,7 @@ def overall_stats(predictions, ground_truth, params):
     }
     for metric_name, calculator in calculators.items():
         output_metrics[metric_name] = get_output_from_calculator(
-            predictions, ground_truth, calculator
+            predictions, target, calculator
         )
 
     return output_metrics
