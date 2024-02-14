39af8e90b85af3bf5d3153e21385b4d52ab640f1
Geeks-sid <sid.cre8er@gmail.com>
2024-02-14 11:11:03 -0500

    Refactor inference_loop.py

--- GANDLF/compute/inference_loop.py
+++ GANDLF/compute/inference_loop.py
@@ -1,29 +1,30 @@
-from .forward_pass import validate_network
-from .generic import create_pytorch_objects
-import os, sys
+import os
+import sys
 from pathlib import Path
 
-# hides torchio citation request, see https://github.com/fepegar/torchio/issues/235
-os.environ["TORCHIO_HIDE_CITATION_PROMPT"] = "1"
-
-import torch
 import cv2
 import numpy as np
-from torch.utils.data import DataLoader
-from skimage.io import imsave
-from tqdm import tqdm
-from torch.cuda.amp import autocast
 import tiffslide as openslide
+import torch
 from GANDLF.data import get_testing_loader
+from GANDLF.data.inference_dataloader_histopath import InferTumorSegDataset
+from GANDLF.data.preprocessing import get_transforms_for_preprocessing
 from GANDLF.utils import (
     best_model_path_end,
     latest_model_path_end,
     load_ov_model,
     print_model_summary,
 )
+from skimage.io import imsave
+from torch.cuda.amp import autocast
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-from GANDLF.data.inference_dataloader_histopath import InferTumorSegDataset
-from GANDLF.data.preprocessing import get_transforms_for_preprocessing
+from .forward_pass import validate_network
+from .generic import create_pytorch_objects
+
+# hides torchio citation request, see https://github.com/fepegar/torchio/issues/235
+os.environ["TORCHIO_HIDE_CITATION_PROMPT"] = "1"
 
 
 def applyCustomColorMap(im_gray):
@@ -125,7 +126,7 @@ def inference_loop(
             model, input_blob, output_blob = load_ov_model(xml_to_check, device.upper())
             parameters["model"]["IO"] = [input_blob, output_blob]
 
-    if not (os.environ.get("HOSTNAME") is None):
+    if os.environ.get("HOSTNAME") is not None:
         print("\nHostname     :" + str(os.environ.get("HOSTNAME")), flush=True)
 
     # radiology inference
