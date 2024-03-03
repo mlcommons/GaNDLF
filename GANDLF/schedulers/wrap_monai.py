1e9372b8c876e16734491063c33d27595afb42ce
Ainesh Nanda <aineshnanda@gmail.com>
2024-03-03 19:52:28 +0530

    added wcs scheduler

--- GANDLF/schedulers/wrap_monai.py
+++ GANDLF/schedulers/wrap_monai.py
@@ -1,7 +1,11 @@
 from monai.optimizers import WarmupCosineSchedule as WCS
 
 def warmupcosineschedule(optimizers):
+    # if not ("warmup_steps" in parameters["scheduler"]):
+    #     parameters["scheduler"]["warmup_steps"] = 10
+    
     return WCS(
         optimizers["model_optimizers"],
-        
+        t_total= 100,
+        warmup_steps= 0.1*100
     )
