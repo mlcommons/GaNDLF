0ae4c68f97df32f5adad5e03148d7ae3407a7757
Ainesh Nanda <aineshnanda@gmail.com>
2024-03-07 13:45:52 +0530

    removed unwanted whitespace

--- GANDLF/schedulers/wrap_monai.py
+++ GANDLF/schedulers/wrap_monai.py
@@ -1,9 +1,6 @@
 from monai.optimizers import WarmupCosineSchedule as WCS
 
 def warmupcosineschedule(optimizers):
-    # if not ("warmup_steps" in parameters["scheduler"]):
-    #     parameters["scheduler"]["warmup_steps"] = 10
-    
     return WCS(
         optimizers["model_optimizers"],
         t_total= 100,
