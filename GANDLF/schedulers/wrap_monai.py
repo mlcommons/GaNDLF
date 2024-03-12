d8a6cef46259efeec6b0e8f33646dca492954d80
Ainesh Nanda <aineshnanda@gmail.com>
2024-03-12 17:44:56 +0530

    corrected key related error

--- GANDLF/schedulers/wrap_monai.py
+++ GANDLF/schedulers/wrap_monai.py
@@ -1,8 +1,8 @@
 from monai.optimizers import WarmupCosineSchedule as WCS
 
-def warmupcosineschedule(optimizers):
+def warmupcosineschedule(parameters):
     return WCS(
-        optimizers["model_optimizers"],
+        parameters["optimizer_object"], #optimizers["model_optimizers"],
         t_total= 100,
         warmup_steps= 0.1*100
     )
