44dbdaebcbcd4eef5693e1d05a964968553aebcb
Ainesh Nanda <67788476+Ainesh06102004@users.noreply.github.com>
2024-03-12 19:20:48 +0530

    Update GANDLF/schedulers/wrap_monai.py
    Co-authored-by: Sarthak Pati <patis@iu.edu>

--- GANDLF/schedulers/wrap_monai.py
+++ GANDLF/schedulers/wrap_monai.py
@@ -4,5 +4,5 @@ def warmupcosineschedule(parameters):
     return WCS(
         parameters["optimizer_object"], #optimizers["model_optimizers"],
         t_total = parameters["num_epochs"],
-        warmup_steps= 0.1*100
+        warmup_steps = 0.1 * parameters["num_epochs"],
     )
