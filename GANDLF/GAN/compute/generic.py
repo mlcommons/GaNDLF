319ba6a7d1af0ae32561d6b63bd9683d2a055c0f
Szymon Mazurek <szymonmazurek57@gmail.com>
2024-03-14 11:38:22 +0100

    Docs and typing updateds
    - updated comment styles in functions generating latent vectors
    - updated typing in function to create pytorch objects

--- GANDLF/GAN/compute/generic.py
+++ GANDLF/GAN/compute/generic.py
@@ -13,23 +13,27 @@ from GANDLF.utils import (
     send_model_to_device,
     get_class_imbalance_weights,
 )
+from typing import Optional
 
 
 def generate_latent_vector(
     batch_size: int, latent_vector_size: int, dimension: int, device: str
 ) -> torch.Tensor:
-    """Creates a latent vector of given size and adjusts the dimensions
+    """
+    Creates a latent vector of given size and adjusts the dimensions
     according to the dimension parameter (for 2D or 3D).
+
     Args:
         batch_size (int): The batch size.
         latent_vector_size (int): The latent vector size.
         dimension (int): The dimension of the images in a given problem.
-    can be 2 for 2D or 3 for 3D.
+    Can be `2` for 2D or `3` for 3D.
         device (str): The device to perform computations on.
+
     Returns:
         latent_vector (torch.Tensor): The latent vector.
     """
-    assert dimension in [2, 3], "Dimension should be 2 (2D) or 3 (3D)"
+    assert dimension in [2, 3], "Dimension should be `2` (2D) or `3` (3D)"
     latent_vector = torch.randn(
         (batch_size, latent_vector_size, 1, 1), device=device
     )
@@ -49,12 +53,15 @@ def get_fixed_latent_vector(
     Function to get the fixed latent vector for inference or validation.
     It always starts with the seed given by user and then re-sets the
     previous RNG state.
+
     Args:
         batch_size (int): The batch size.
         latent_vector_size (int): The latent vector size.
-        dimension (int): The dimension of the images in a given problem. Can be `2` for 2D or `3` for 3D.
+        dimension (int): The dimension of the images in a given problem.
+    Can be `2` for 2D or `3` for 3D.
         device (str): The device to perform computations on.
         seed (int): The seed to use for reproducibility.
+
     Returns:
         latent_vector (torch.Tensor): The fixed latent vector.
     """
@@ -73,16 +80,19 @@ def get_fixed_latent_vector(
 
 
 def create_pytorch_objects_gan(
-    parameters, train_csv=None, val_csv=None, device="cpu"
+    parameters: dict,
+    train_csv=Optional[str],
+    val_csv=Optional[str],
+    device: str = "cpu",
 ):
     """
     This function creates all the PyTorch objects needed for training.
 
     Args:
         parameters (dict): The parameters dictionary.
-        train_csv (str): The path to the training CSV file.
-        val_csv (str): The path to the validation CSV file.
-        device (str): The device to perform computations on.
+        train_csv (optional, str): The path to the training CSV file.
+        val_csv (optional, str): The path to the validation CSV file.
+        device (str): The device to perform computations on. Defaults to "cpu".
 
     Returns:
         model (torch.nn.Module): The model to use for training.
