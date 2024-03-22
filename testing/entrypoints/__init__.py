ac12ed50d95fe3245ef254887e848f9126fb4016
Viacheslav Kukushkin <vy.kukushkin@gmail.com>
2024-03-22 16:39:45 +0300

    Covered update_version with tests

--- testing/entrypoints/__init__.py
+++ testing/entrypoints/__init__.py
@@ -218,11 +218,11 @@ def assert_called_properly(mock_func, expected_args: dict, args_normalizer):
 
 
 def run_test_case(
-    cli_runner: CliRunner,
+    cli_runner: Optional[CliRunner],
     file_system_config: List[_TmpPath],
     case: CliCase,
     real_code_function_path: str,
-    new_way: BaseCommand,
+    new_way: Optional[BaseCommand],
     old_way: Callable,
     old_script_name: str,
     patched_return_value: Any = None,
--- testing/test_update_version.py
+++ testing/test_update_version.py
@@ -1,14 +1,18 @@
 import pytest
-from click.testing import CliRunner
+import sys
+from pathlib import Path
+from entrypoints import CliCase, run_test_case
 
-from GANDLF.entrypoints.update_version import new_way, old_way
-from . import cli_runner, CliCase, run_test_case
+parent = str(Path(__file__).parent.parent.absolute())
+sys.path.append(parent)
+
+from update_version import main
 
 # This function is a place where a real logic is executed.
 # For tests, we replace it with mock up, and check if this function is called
 # with proper args for different cli commands
-MOCK_PATH = "GANDLF.entrypoints.update_version._update_version"
-OLD_SCRIPT_NAME = "gandlf_updateVersion"
+MOCK_PATH = "update_version._update_version"
+OLD_SCRIPT_NAME = "update_version.py"
 
 # these files would be either created temporarily for test execution,
 # or we ensure they do not exist
@@ -16,25 +20,14 @@ test_file_system = []
 test_cases = [
     CliCase(
         should_succeed=True,
-        new_way_lines=[
-            # full command
-            "--old-version 0.18 --new-version 0.19",
-            # short aliases
-            "-ov 0.18 -nv 0.19",
-        ],
         old_way_lines=[
-            "--old_version 0.18 --new_version 0.19",
+            "--old-version 0.18 --new-version 0.19",
             "-ov 0.18 -nv 0.19",
         ],
         expected_args={"old_version": "0.18", "new_version": "0.19"},
     ),
     CliCase(
         should_succeed=False,
-        new_way_lines=[
-            # both old-version and new-version are required
-            "-ov 0.18",
-            "-nv 0.19",
-        ],
         old_way_lines=[
             "-ov 0.18",
             "-nv 0.19",
@@ -44,13 +37,13 @@ test_cases = [
 
 
 @pytest.mark.parametrize("case", test_cases)
-def test_case(cli_runner: CliRunner, case: CliCase):
+def test_case(case: CliCase):
     run_test_case(
-        cli_runner=cli_runner,
+        cli_runner=None,
         file_system_config=test_file_system,
         case=case,
         real_code_function_path=MOCK_PATH,
-        new_way=new_way,
-        old_way=old_way,
+        new_way=None,
+        old_way=main,
         old_script_name=OLD_SCRIPT_NAME,
     )
