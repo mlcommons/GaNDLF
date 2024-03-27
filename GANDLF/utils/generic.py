baad9567a33e4ee6e6d9c519b0f6fc6f0a4ef006
scap3yvt <149599669+scap3yvt@users.noreply.github.com>
2024-03-27 10:38:09 -0400

    removed accidental double print

--- GANDLF/utils/generic.py
+++ GANDLF/utils/generic.py
@@ -1,5 +1,6 @@
 import os, datetime, subprocess, sys
 from copy import deepcopy
+from pprint import pprint
 import random
 import numpy as np
 import torch
@@ -253,7 +254,6 @@ def print_and_format_metrics(
         Returns:
             dict: The output metrics dictionary.
         """
-        print(input_metrics_dict)
         output_metrics_dict = deepcopy(input_metrics_dict)
         for metric in input_metrics_dict.keys():
             if isinstance(input_metrics_dict[metric], list):
@@ -265,7 +265,7 @@ def print_and_format_metrics(
                     .split(",")
                 )
 
-        print(output_metrics_dict)
+        pprint(output_metrics_dict)
         return output_metrics_dict
 
     output_metrics_dict = deepcopy(cohort_level_metrics)
