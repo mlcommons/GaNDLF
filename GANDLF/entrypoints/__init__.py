d98f004e995a02531d130057b182a434df620fd7
Viacheslav Kukushkin <vy.kukushkin@gmail.com>
2024-04-03 20:24:44 +0300

    replaced if-else to oneliner for better coverage

--- GANDLF/entrypoints/__init__.py
+++ GANDLF/entrypoints/__init__.py
@@ -2,8 +2,9 @@ from GANDLF.cli import copyrightMessage
 
 
 def append_copyright_to_help(command_func):
-    if command_func.__doc__ is None:
-        command_func.__doc__ = copyrightMessage
-    else:
-        command_func.__doc__ += "\n\n" + copyrightMessage
+    command_func.__doc__ = (
+        copyrightMessage
+        if command_func.__doc__ is None
+        else (command_func.__doc__ + "\n\n" + copyrightMessage)
+    )
     return command_func
