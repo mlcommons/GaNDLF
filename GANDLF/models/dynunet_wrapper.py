1dda4eda2562bacdfd63b7f586d100c50d06775e
V.Malefioudakis <benmalef@hotmail.com>
2024-06-05 09:51:04 +0300

    made the proposed changes

--- GANDLF/models/dynunet_wrapper.py
+++ GANDLF/models/dynunet_wrapper.py
@@ -35,54 +35,59 @@ class dynunet_wrapper(ModelBase):
         ) == True, "\033[0;31m`strides` key missing in parameters"
 
         # defining some defaults
-        if not ("upsample_kernel_size" in parameters["model"]):
-            parameters["model"]["upsample_kernel_size"] = parameters["model"][
-                "strides"
-            ][1:]
+        # if not ("upsample_kernel_size" in parameters["model"]):
+        #     parameters["model"]["upsample_kernel_size"] = parameters["model"][
+        #         "strides"
+        #     ][1:]
 
         if not ("filters" in parameters["model"]):
-            parameters["model"]["filters"] = None
+            parameters["model"]["filters"] = parameters["model"].get("filters", None)
 
         if not ("act_name" in parameters["model"]):
-            parameters["model"]["act_name"] = (
-                "leakyrelu",
-                {"inplace": True, "negative_slope": 0.01},
+            parameters["model"]["act_name"] = parameters["model"].get(
+                "act_name", ("leakyrelu", {"inplace": True, "negative_slope": 0.01})
             )
 
         if not ("deep_supervision" in parameters["model"]):
-            parameters["model"]["deep_supervision"] = True
+            parameters["model"]["deep_supervision"] = parameters["model"].get(
+                "deep_supervision", True
+            )
 
         if not ("deep_supr_num" in parameters["model"]):
-            parameters["model"]["deep_supr_num"] = 1
+            parameters["model"]["deep_supr_num"] = parameters["model"].get(
+                "deep_supr_num", 1
+            )
 
         if not ("res_block" in parameters["model"]):
-            parameters["model"]["res_block"] = True
+            parameters["model"]["res_block"] = parameters["model"].get(
+                "res_block", True
+            )
 
         if not ("trans_bias" in parameters["model"]):
-            parameters["model"]["trans_bias"] = False
+            parameters["model"]["trans_bias"] = parameters["model"].get(
+                "trans_bias", False
+            )
 
         if not ("norm_type" in parameters["model"]):
             self.norm_type = "INSTANCE"
 
         if not ("dropout" in parameters):
-            parameters["model"]["dropout"] = None
+            parameters["model"]["dropout"] = parameters["model"].get("dropout", None)
 
         self.model = dynunet.DynUNet(
             spatial_dims=self.n_dimensions,
             in_channels=self.n_channels,
-            out_channels=self.base_filters,  # ? Is it correct?
+            out_channels=self.n_classes,
             kernel_size=parameters["model"]["kernel_size"],
             strides=parameters["model"]["strides"],
-            upsample_kernel_size=parameters["model"][
-                "upsample_kernel_size"
-            ],  # The values should equal to strides[1:]
+            upsample_kernel_size=parameters["model"]["strides"][1:],
             filters=parameters["model"][
                 "filters"
-            ],  # ? self.base_filter??? , number of output channels for each blocks
+            ],  # number of output channels for each blocks
             dropout=parameters["model"][
                 "dropout"
             ],  # dropout ratio. Defaults to no dropout
-            norm_name=self.norm_type,  # ? Is it correct??
+            norm_name=self.norm_type,
             act_name=parameters["model"]["act_name"],
             deep_supervision=parameters["model"]["deep_supervision"],
             deep_supr_num=parameters["model"][
