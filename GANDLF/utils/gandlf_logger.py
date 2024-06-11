778f46aa71c94939497663d8d9f63eec43a69141
V.Malefioudakis <benmalef@hotmail.com>
2024-06-11 21:16:15 +0300

    fixed gandlf_logger config.

--- GANDLF/utils/gandlf_logger.py
+++ GANDLF/utils/gandlf_logger.py
@@ -3,9 +3,60 @@ from logging import config
 import yaml
 from pathlib import Path
 
+logging_config  = {
+  "version": 1,
+  "formatters": {
+    "detailed": {
+      "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    }
+  },
+  "filters": {
+    "warnings_filter": {
+      "()": "logging.Filter",
+      "name": "py.warnings"
+    },
+    "info_only_filter": {
+      "()": "gandlf_logger.InfoOnlyFilter"
+    }
+  },
+  "handlers": {
+    "stdoutHandler": {
+      "class": "logging.StreamHandler",
+      "level": "INFO",
+      "formatter": "detailed",
+      "filters": ["info_only_filter"],
+      "stream": "ext://sys.stdout"
+    },
+    "stderrHandler": {
+      "class": "logging.StreamHandler",
+      "level": "WARNING",
+      "formatter": "detailed",
+      "stream": "ext://sys.stderr"
+    },
+    "debugHandler": {
+      "class": "logging.FileHandler",
+      "filename": "gandlf.log",
+      "formatter": "detailed",
+      "level": "DEBUG"
+    }
+  },
+  "loggers": {
+    "debug_logger": {
+      "level": "DEBUG",
+      "handlers": ["stdoutHandler", "debugHandler", "stderrHandler"],
+      "propagate": False
+    }
+  },
+  "root": {
+    "level": "DEBUG",
+    "handlers": ["stdoutHandler", "debugHandler", "stderrHandler"]
+  }
+}
+
+
 
 def gandlf_logger_setup(
-    logger_name, config_filename="logging_config.yaml"
+    logger_name
 ) -> logging.Logger:
     """
     It sets up the logger. Read from logging_config.
@@ -17,12 +68,7 @@ def gandlf_logger_setup(
     Returns:
         logging.Logger
     """
-    current_dir = Path.cwd()
-    file_path = current_dir.parent.parent / config_filename
-
-    with open(file_path, "r") as file:
-        config1 = yaml.safe_load(file)
-        logging.config.dictConfig(config1)
+    logging.config.dictConfig(logging_config)
 
     logging.captureWarnings(True)
 
