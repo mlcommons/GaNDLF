1995143e0144e8141dc8fa118605cb875caeeb7c
V.Malefioudakis <benmalef@hotmail.com>
2024-06-25 16:57:41 +0300

    some code changes

--- GANDLF/utils/gandlf_logger.py
+++ GANDLF/utils/gandlf_logger.py
@@ -3,49 +3,6 @@ from logging import config
 import yaml
 from pathlib import Path
 
-logging_config = {
-    "version": 1,
-    "formatters": {
-        "detailed": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}
-    },
-    "filters": {
-        "warnings_filter": {"()": "logging.Filter", "name": "py.warnings"},
-        "info_only_filter": {"()": "gandlf_logger.InfoOnlyFilter"},
-    },
-    "handlers": {
-        "stdoutHandler": {
-            "class": "logging.StreamHandler",
-            "level": "INFO",
-            "formatter": "detailed",
-            "filters": ["info_only_filter"],
-            "stream": "ext://sys.stdout",
-        },
-        "stderrHandler": {
-            "class": "logging.StreamHandler",
-            "level": "WARNING",
-            "formatter": "detailed",
-            "stream": "ext://sys.stderr",
-        },
-        "debugHandler": {
-            "class": "logging.FileHandler",
-            "filename": "gandlf.log",
-            "formatter": "detailed",
-            "level": "DEBUG",
-        },
-    },
-    "loggers": {
-        "debug_logger": {
-            "level": "DEBUG",
-            "handlers": ["stdoutHandler", "debugHandler", "stderrHandler"],
-            "propagate": False,
-        }
-    },
-    "root": {
-        "level": "DEBUG",
-        "handlers": ["stdoutHandler", "debugHandler", "stderrHandler"],
-    },
-}
-
 
 def gandlf_logger_setup(
     logger_name, config_path="logging_config.yaml"
