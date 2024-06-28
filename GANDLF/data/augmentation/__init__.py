56973678fd8fa6bb52b305156e12c7271f47eb91
Szymon Mazurek <szymonmazurek57@gmail.com>
2024-06-28 09:26:01 +0200

    Adding function to get augmentation transforms

--- GANDLF/data/augmentation/__init__.py
+++ GANDLF/data/augmentation/__init__.py
@@ -1,3 +1,7 @@
+from warnings import warn
+from typing import List, Union, Dict, Callable
+
+
 from .wrap_torchio import (
     mri_artifact,
     motion,
@@ -35,3 +39,40 @@ global_augs_dict = {
     "colorjitter": colorjitter_transform,
     "hed_transform": hed_transform,
 }
+
+
+def get_augmentation_transforms(
+    augmentation_params_dict: Union[Dict[str, object], List[str]]
+) -> List[Callable]:
+    """
+    This function gets the augmentation transformations from the parameters.
+
+    Args:
+        augmentation_params_dict (dict): The dictionary containing the parameters for the augmentation.
+
+    Returns:
+        List[Callable]: The list of augmentation to be applied.
+    """
+    current_augmentations = []
+
+    # Check if user specified some augmentations without extra params
+    if isinstance(augmentation_params_dict, list):
+        for n, augmentation_type in enumerate(augmentation_params_dict):
+            if isinstance(augmentation_type, dict):
+                continue
+            else:
+                augmentation_params_dict[n] = {augmentation_type: {}}
+
+    for augmentation_type, augmentation_params in augmentation_params_dict.items():
+        augmentation_type_lower = augmentation_type.lower()
+
+        if augmentation_type_lower in global_augs_dict:
+            current_augmentations.append(
+                global_augs_dict[augmentation_type_lower](**augmentation_params)
+            )
+        else:
+            warn(
+                f"Augmentation {augmentation_type} not found in the global augmentation dictionary.",
+                UserWarning,
+            )
+    return current_augmentations
