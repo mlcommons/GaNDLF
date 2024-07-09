8efe7f19c71c070589451ea8fc2e423624afaf52
scap3yvt <149599669+scap3yvt@users.noreply.github.com>
2024-07-09 13:40:46 -0400

    added output_dir for model optimization

--- GANDLF/cli/post_training_model_optimization.py
+++ GANDLF/cli/post_training_model_optimization.py
@@ -1,16 +1,21 @@
 import os
+from pathlib import Path
+from typing import Optional
 from GANDLF.compute import create_pytorch_objects
 from GANDLF.config_manager import ConfigManager
 from GANDLF.utils import version_check, load_model, optimize_and_save_model
 
 
-def post_training_model_optimization(model_path: str, config_path: str) -> bool:
+def post_training_model_optimization(
+    model_path: str, config_path: Optional[str] = None, output_dir: Optional[str] = None
+) -> bool:
     """
     CLI function to optimize a model for deployment.
 
     Args:
         model_path (str): Path to the model file.
-        config_path (str): Path to the config file.
+        config_path (str, optional): Path to the configuration file.
+        output_dir (str, optional): Output directory to save the optimized model.
 
     Returns:
         bool: True if successful, False otherwise.
@@ -26,6 +31,12 @@ def post_training_model_optimization(model_path: str, config_path: str) -> bool:
         else parameters
     )
 
+    output_dir = os.path.dirname(model_path) if output_dir is None else output_dir
+    Path.mkdir(output_dir, parents=True, exist_ok=True)
+    optimized_model_path = os.path.join(
+        output_dir, os.path.basename(model_path).replace("pth.tar", "onnx")
+    )
+
     # Create PyTorch objects and set onnx_export to True for optimization
     model, _, _, _, _, parameters = create_pytorch_objects(parameters, device="cpu")
     parameters["model"]["onnx_export"] = True
@@ -35,10 +46,9 @@ def post_training_model_optimization(model_path: str, config_path: str) -> bool:
     model.load_state_dict(main_dict["model_state_dict"])
 
     # Optimize the model and save it to an ONNX file
-    optimize_and_save_model(model, parameters, model_path, onnx_export=True)
+    optimize_and_save_model(model, parameters, optimized_model_path, onnx_export=True)
 
     # Check if the optimized model file exists
-    optimized_model_path = model_path.replace("pth.tar", "onnx")
     if not os.path.exists(optimized_model_path):
         print("Error while optimizing the model.")
         return False
