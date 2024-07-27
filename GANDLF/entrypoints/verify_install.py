02d39b1fde78e996598454db57f617713b0bdad3
Sarthak Pati <patis@iu.edu>
2024-07-26 21:52:53 -0400

    automatic gandlf installation removed

--- GANDLF/entrypoints/verify_install.py
+++ GANDLF/entrypoints/verify_install.py
@@ -19,13 +19,6 @@ def _verify_install():
             "GaNDLF not properly installed, please see https://mlcommons.github.io/GaNDLF/setup"
         ) from e
 
-    # we always want to do submodule update to ensure any hash updates are ingested correctly
-    try:
-        os.system(f"{sys.executable} -m pip install -e .")
-    # TODO: how does this work? why do we trigger on git? what if gandlf is installed as pypi package?
-    except Exception as e:
-        raise Exception("Git was not found, please try again.") from e
-
     print("GaNDLF is ready. See https://mlcommons.github.io/GaNDLF/usage")
 
 
