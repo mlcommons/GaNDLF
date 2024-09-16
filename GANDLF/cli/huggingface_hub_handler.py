38984d0aac921e86eb57177dfc4a7259ae2ba5b4
pranayasinghcsmpl <ritesh.khatua@corp.csmpl.com>
2024-09-16 14:15:51 +0530

    resolved conflit

--- GANDLF/cli/huggingface_hub_handler.py
+++ GANDLF/cli/huggingface_hub_handler.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from GANDLF.utils import get_git_hash
 import re
 
+
 def validate_model_card(file_path: str):
     """
     Validate that the required fields in the model card are not null, empty, or set to 'REQUIRED_FOR_GANDLF'.
@@ -16,54 +17,70 @@ def validate_model_card(file_path: str):
     Raises:
         AssertionError: If any required field is missing, empty, null, or contains 'REQUIRED_FOR_GANDLF'.
     """
-    #Read the Markdown file
+    # Read the Markdown file
     path = Path(file_path)
-    with path.open('r') as file:
-
+    with path.open("r") as file:
         template_str = file.read()
-
 
     # Define required fields and their regex patterns to capture the values
     patterns = {
-        'Developed by': re.compile(r'\*\*Developed by:\*\*\s*\{\{\s*developers\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}', re.MULTILINE),
-        'License': re.compile(r'\*\*License:\*\*\s*\{\{\s*license\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}', re.MULTILINE),
-        'Primary Organization': re.compile(r'\*\*Primary Organization:\*\*\s*\{\{\s*primary_organization\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}', re.MULTILINE),
-        'Commercial use policy': re.compile(r'\*\*Commercial use policy:\*\*\s*\{\{\s*commercial_use\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}', re.MULTILINE)
+        "Developed by": re.compile(
+            r'\*\*Developed by:\*\*\s*\{\{\s*developers\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}',
+            re.MULTILINE,
+        ),
+        "License": re.compile(
+            r'\*\*License:\*\*\s*\{\{\s*license\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}',
+            re.MULTILINE,
+        ),
+        "Primary Organization": re.compile(
+            r'\*\*Primary Organization:\*\*\s*\{\{\s*primary_organization\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}',
+            re.MULTILINE,
+        ),
+        "Commercial use policy": re.compile(
+            r'\*\*Commercial use policy:\*\*\s*\{\{\s*commercial_use\s*\|\s*default\("(.+?)",\s*true\)\s*\}\}',
+            re.MULTILINE,
+        ),
     }
-     
-
 
     # Iterate through the required fields and validate
     for field, pattern in patterns.items():
         match = pattern.search(template_str)
-        
+
         # Ensure the field is present and does not contain 'REQUIRED_FOR_GANDLF'
         assert match, f"Field '{field}' is missing or not found in the file."
-        
 
         extract_value = match.group(1)
 
         # Get the field value
-        value = re.search(r'\[([^\]]+)\]', extract_value).group(1) if re.search(r'\[([^\]]+)\]', extract_value) else None
+        value = (
+            re.search(r"\[([^\]]+)\]", extract_value).group(1)
+            if re.search(r"\[([^\]]+)\]", extract_value)
+            else None
+        )
 
-
-        
         # Ensure the field is not set to 'REQUIRED_FOR_GANDLF' or empty
-        assert value != 'REQUIRED_FOR_GANDLF', f"The value for '{field}' is set to the default placeholder '[REQUIRED_FOR_GANDLF]'. It must be a valid value."
+        assert (
+            value != "REQUIRED_FOR_GANDLF"
+        ), f"The value for '{field}' is set to the default placeholder '[REQUIRED_FOR_GANDLF]'. It must be a valid value."
         assert value, f"The value for '{field}' is empty or null."
 
         # Ensure the value contains only alphabetic or alphanumeric characters
-        assert re.match(r'^[a-zA-Z0-9]+$', value), f"The value for '{field}' must be alphabetic or alphanumeric, but got: '{value}'"
+        assert re.match(
+            r"^[a-zA-Z0-9]+$", value
+        ), f"The value for '{field}' must be alphabetic or alphanumeric, but got: '{value}'"
 
-    print("All required fields are valid, non-empty, properly filled, and do not contain '[REQUIRED_FOR_GANDLF]'.")
+    print(
+        "All required fields are valid, non-empty, properly filled, and do not contain '[REQUIRED_FOR_GANDLF]'."
+    )
 
     # Example usage
     return template_str
 
+
 def push_to_model_hub(
     repo_id: str,
     folder_path: str,
-    hf_template:str,
+    hf_template: str,
     path_in_repo: Union[str, None] = None,
     commit_message: Union[str, None] = None,
     commit_description: Union[str, None] = None,
@@ -71,7 +88,7 @@ def push_to_model_hub(
     revision: Union[str, None] = None,
     allow_patterns: Union[List[str], str, None] = None,
     ignore_patterns: Union[List[str], str, None] = None,
-    delete_patterns: Union[List[str], str, None] = None, 
+    delete_patterns: Union[List[str], str, None] = None,
 ):
     api = HfApi(token=token)
 
@@ -79,8 +96,6 @@ def push_to_model_hub(
         api.create_repo(repo_id)
     except Exception as e:
         print(f"Error: {e}")
-
-
 
     api = HfApi(token=token)
 
@@ -94,7 +109,7 @@ def push_to_model_hub(
         tags += [git_hash]
 
     readme_template = validate_model_card(hf_template)
-        
+
     card_data = ModelCardData(library_name="GaNDLF", tags=tags)
     card = ModelCard.from_template(card_data, template_str=readme_template)
 
@@ -112,6 +127,7 @@ def push_to_model_hub(
     )
     print("Model Sucessfully Uploded")
 
+
 def download_from_hub(
     repo_id: str,
     revision: Union[str, None] = None,
@@ -128,4 +144,3 @@ def download_from_hub(
         force_download=force_download,
         token=token,
     )
-    
