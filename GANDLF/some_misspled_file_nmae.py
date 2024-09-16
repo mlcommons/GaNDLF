d453ca07774cb3598d89d7c41d04b800a8a58da2
Szymon Mazurek <szymonmazurek57@gmail.com>
2024-09-16 15:40:03 +0200

    Testing spellchekcer

---
+++ GANDLF/some_misspled_file_nmae.py
@@ -0,0 +1,13 @@
+# Testing the spellckecker workflow
+
+
+def some_function():
+    print("Hello World!")
+    print("This is a test of the spellchecker workflow")
+    print("Here it is properly spelled")
+
+
+def some_misspled_function():
+    print("This is a test of the spellchecker workflow")
+    print("Here it is not properly spelled")
+    print("misspled!")
