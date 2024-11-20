2aeed0be619c936f74c751b052a1eb430ee97a72
scap3yvt <149599669+scap3yvt@users.noreply.github.com>
2024-11-20 17:23:25 -0500

    fixed lint

--- GANDLF/optimizers/thirdparty/lion.py
+++ GANDLF/optimizers/thirdparty/lion.py
@@ -17,6 +17,8 @@ def lion_wrapper(parameters: dict) -> Optimizer:
         lr=parameters.get("learning_rate", 1e-4),
         betas=parameters["optimizer"].get("betas", (0.9, 0.999)),
         weight_decay=parameters["optimizer"].get("weight_decay", 0.0),
-        decoupled_weight_decay=parameters["optimizer"].get("decoupled_weight_decay", False),
+        decoupled_weight_decay=parameters["optimizer"].get(
+            "decoupled_weight_decay", False
+        ),
         use_triton=False,  # as of 20241120, triton is not generally available for all platforms
     )
