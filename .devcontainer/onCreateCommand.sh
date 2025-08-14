498dd454124a8dc22b55aac932eacf2ae3da95e2
Sarthak Pati <sarthak.pati@hotmail.com>
2025-08-14 16:50:38 -0400

    checking if this helps the dev container build

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -2,8 +2,6 @@
 
 python -m ensurepip # ensures pip is installed in the current environment
 pip install --upgrade pip==24.0
-pip install wheel
 pip install openvino-dev==2023.0.1 # [OPTIONAL] to generate optimized models for inference
-pip install mlcube_docker          # [OPTIONAL] to deploy GaNDLF models as MLCube-compliant Docker containers
 pip install medmnist==2.1.0
 pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
