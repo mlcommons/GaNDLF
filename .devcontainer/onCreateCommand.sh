da6cc3d6a9441203688e7d6c34792551fbec881b
Sarthak Pati <sarthak.pati@hotmail.com>
2025-08-15 15:15:45 -0400

    reverting uv change

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -2,10 +2,10 @@
 
 python -m ensurepip # ensures pip is installed in the current environment
 pip install --upgrade pip==24.0
-pip install uv
-uv pip install openvino-dev==2023.0.1 --system # [OPTIONAL] to generate optimized models for inference
-uv pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu --system
-uv pip install medmnist==2.1.0 --system
-# pip install openvino-dev==2023.0.1 # [OPTIONAL] to generate optimized models for inference
-# pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
-# pip install medmnist==2.1.0
+# pip install uv ## this is giving `Permission denied (os error 13)``
+# uv pip install openvino-dev==2023.0.1 --system # [OPTIONAL] to generate optimized models for inference
+# uv pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu --system
+# uv pip install medmnist==2.1.0 --system
+pip install openvino-dev==2023.0.1 # [OPTIONAL] to generate optimized models for inference
+pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
+pip install medmnist==2.1.0
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,15 +3,15 @@
 # if there is a gpu present in the container, install the gpu version of pytorch
 # we will check for the presence of the nvidia-smi command as that signifies the presence of a gpu
 
-pip install uv  ## potentially creating permission issues
+# pip install uv  ## potentially creating permission issues
 # if runnning on a GPU machine, install the GPU version of pytorch
 if command -v nvidia-smi &> /dev/null
 then
-	# pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
-	uv pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu --system
+	pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
+	# uv pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu --system
 fi
 
-# pip install -e .
-uv pip install -e . --system
+pip install -e .
+# uv pip install -e . --system
 gandlf verify-install
 gzip -dk -r tutorials/classification_medmnist_notebook/medmnist/dataset
