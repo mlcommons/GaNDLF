caa49b9c548a6a817576246e9e59534b843e25ef
Sarthak Pati <sarthak.pati@hotmail.com>
2025-08-15 13:59:08 -0400

    missed uv installation

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -2,7 +2,7 @@
 
 python -m ensurepip # ensures pip is installed in the current environment
 pip install --upgrade pip==24.0
-# pip install uv ## potentially creating permission issues
+pip install uv ## potentially creating permission issues
 uv pip install openvino-dev==2023.0.1 --system # [OPTIONAL] to generate optimized models for inference
 uv pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu --system
 uv pip install medmnist==2.1.0 --system
