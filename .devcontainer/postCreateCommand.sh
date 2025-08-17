3183ff268d9799e871a72f5ced8606b1aff92c9f
Sarthak Pati <sarthak.pati@hotmail.com>
2025-08-17 16:46:53 -0400

    trying something else

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,14 +12,14 @@ then
 fi
 
 ### temp fix for monai to prevent it from pullin all nvidia stuff
-pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
+python3 -m pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
 git clone https://github.com/Project-MONAI/MONAI.git
 cd MONAI
 git checkout 8ee3f89b1db3c28f7056235dfd1b1b8bf435bf67
-pip install -e .
+python3 -m pip install -e .
 cd ..
 ### temp fix for monai to prevent it from pullin all nvidia stuff
-pip install -e .
+python3 -m pip install -e .
 # uv pip install -e . --system
 gandlf verify-install
 gzip -dk -r tutorials/classification_medmnist_notebook/medmnist/dataset
