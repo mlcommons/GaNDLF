d18f795d33cad354afcc07fb0f221377a4c3f43a
Sarthak Pati <sarthak.pati@hotmail.com>
2025-08-17 21:57:30 -0400

    letting pip solve for this

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 python -m ensurepip # ensures pip is installed in the current environment
-which python
-which pip
 python -m pip install --upgrade pip==24.0
 # pip install uv ## this is giving `Permission denied (os error 13)``
 # uv pip install openvino-dev==2023.0.1 --system # [OPTIONAL] to generate optimized models for inference
@@ -11,14 +9,10 @@ python -m pip install --upgrade pip==24.0
 # pip install openvino-dev==2023.0.1 # [OPTIONAL] to generate optimized models for inference
 python -m pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cpu
 python -m pip install medmnist==2.1.0
-### temp fix for monai to prevent it from pullin all nvidia stuff
-git clone https://github.com/Project-MONAI/MONAI.git
-cd MONAI
-git checkout 8ee3f89b1db3c28f7056235dfd1b1b8bf435bf67
-which python
-which pip
-python -m pip install -e .
-which python
-which pip
-cd ..
-### temp fix for monai to prevent it from pullin all nvidia stuff
+# ### temp fix for monai to prevent it from pullin all nvidia stuff
+# git clone https://github.com/Project-MONAI/MONAI.git
+# cd MONAI
+# git checkout 8ee3f89b1db3c28f7056235dfd1b1b8bf435bf67
+# python -m pip install -e .
+# cd ..
+# ### temp fix for monai to prevent it from pullin all nvidia stuff
